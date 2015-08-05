37ead5fcfbbd2e4b092510c48d93124b7dadcc91
Barthelemy <barthelemy.von.haller@cern.ch>
2015-08-05 16:44:21 +0200

    Qc infologger specialization
    We don't want every class to configure and instantiate its own InfoLogger instance.

---
+++ libs/Core/QcInfoLogger.h
@@ -0,0 +1,53 @@
+//
+// Created by flpprotodev on 7/10/15.
+//
+
+#ifndef QUALITY_CONTROL_QCINFOLOGGER_H
+#define QUALITY_CONTROL_QCINFOLOGGER_H
+
+#include "QCTask.h"
+#include <iostream>
+#include <InfoLogger.hxx>
+
+namespace AliceO2 {
+namespace QualityControl {
+namespace Core {
+
+using namespace std;
+
+class QcInfoLogger : public AliceO2::InfoLogger::InfoLogger
+{
+
+  public:
+
+    static QcInfoLogger &GetInstance()
+    {
+      // Guaranteed to be destroyed. Instantiated on first use
+      static QcInfoLogger foo;// *foo = new QcInfoLogger;
+      return foo;
+    }
+
+  private:
+
+    QcInfoLogger()
+    {
+      // TODO configure the QC infologger, e.g. proper facility
+      *this << "QC infologger initialized" << AliceO2::InfoLogger::InfoLogger::endm;
+    }
+
+    virtual ~QcInfoLogger()
+    {
+
+    }
+
+    // Disallow copying
+    QcInfoLogger &operator=(const QcInfoLogger &) = delete;
+    QcInfoLogger(const QcInfoLogger &) = delete;
+
+};
+
+}
+}
+}
+
+#endif //QUALITY_CONTROL_BASICTASK_H
---
+++ test/testQcInfoLogger.cxx
@@ -0,0 +1,31 @@
+///
+/// \file   Publisher_test.cpp
+/// \author Barthelemy von Haller
+///
+
+#include "../libs/Core/QcInfoLogger.h"
+
+#define BOOST_TEST_MODULE Quality test
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#include <assert.h>
+#include <iostream>
+
+using namespace std;
+
+namespace AliceO2 {
+namespace QualityControl {
+namespace Core {
+
+BOOST_AUTO_TEST_CASE(qc_info_logger)
+{
+  QcInfoLogger &qc1 = QcInfoLogger::GetInstance();
+  QcInfoLogger &qc2 = QcInfoLogger::GetInstance();
+  BOOST_CHECK_EQUAL(&qc1, &qc2);
+  qc1 << "test" << AliceO2::InfoLogger::InfoLogger::endm;
+}
+
+} /* namespace Publisher */
+} /* namespace QualityControl */
+} /* namespace AliceO2 */
