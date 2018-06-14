50c181fe255482228e70863df4d08d1da56b37db
Barthelemy von Haller <barthelemy.von.haller@cern.ch>
2018-06-14 11:52:03 +0200

    QC-68 workaround to be able to compile

--- Framework/src/TaskDPL.cxx
+++ Framework/src/TaskDPL.cxx
@@ -26,6 +26,7 @@
 #include <TH1F.h>
 
 #include "Framework/DataSampling.h"
+#include "Framework/InputRecord.h"
 #include "Framework/runDataProcessing.h"
 #include "QualityControl/TaskDataProcessorFactory.h"
 #include "QualityControl/TaskDataProcessor.h"
@@ -84,16 +85,16 @@ WorkflowSpec defineDataProcessing(ConfigContext const&)
 
         return (AlgorithmSpec::ProcessCallback) [](ProcessingContext& processingContext) mutable {
           LOG(INFO) << "checker invoked";
-          auto mo = processingContext.inputs().get<MonitorObject>("aaa");
-
-          if (mo->getName() == "example") {
-            auto* g = dynamic_cast<TH1F*>(mo->getObject());
-            std::string bins = "BINS:";
-            for (int i = 0; i < g->GetNbinsX(); i++) {
-              bins += " " + std::to_string((int) g->GetBinContent(i));
-            }
-            LOG(INFO) << bins;
-          }
+//          auto mo = processingContext.inputs().get<o2::quality_control::core::MonitorObject>("aaa");
+//
+//          if (mo->getName() == "example") {
+//            auto* g = dynamic_cast<TH1F*>(mo->getObject());
+//            std::string bins = "BINS:";
+//            for (int i = 0; i < g->GetNbinsX(); i++) {
+//              bins += " " + std::to_string((int) g->GetBinContent(i));
+//            }
+//            LOG(INFO) << bins;
+//          }
 
         };
       }
