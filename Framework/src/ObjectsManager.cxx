7f8864d3d655778b0e01959cc4b739d636ada502
Barthelemy <barthelemy.von.haller@cern.ch>
2019-03-08 09:32:07 +0100

    Remove the special object that contained the list of objects published by the tasks.

--- Framework/src/ObjectsManager.cxx
+++ Framework/src/ObjectsManager.cxx
@@ -30,7 +30,7 @@ namespace core
 
 ObjectsManager::ObjectsManager(TaskConfig& taskConfig) : mTaskName(taskConfig.taskName)
 {
-  startPublishing(&mObjectsList, MonitorObject::SYSTEM_OBJECT_PUBLICATION_LIST);
+//  startPublishing(&mObjectsList, MonitorObject::SYSTEM_OBJECT_PUBLICATION_LIST);
   mMonitorObjects.SetOwner(true);
 }
 
@@ -43,19 +43,6 @@ void ObjectsManager::startPublishing(TObject* object, std::string objectName)
   auto* newObject = new MonitorObject(object, mTaskName);
   newObject->setIsOwner(false);
   mMonitorObjects.Add(newObject);
-
-  // update index
-  if (objectName != MonitorObject::SYSTEM_OBJECT_PUBLICATION_LIST) {
-    UpdateIndex(object->GetName());
-  }
-}
-
-void ObjectsManager::UpdateIndex(const string& nonEmptyName)
-{
-  string newString = this->mObjectsList.GetString().Data();
-  newString += nonEmptyName;
-  newString += ",";
-  this->mObjectsList.SetString(newString.c_str());
 }
 
 Quality ObjectsManager::getQuality(std::string objectName)
