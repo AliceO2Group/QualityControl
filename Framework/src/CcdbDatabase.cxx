68ca6a4f040013f0cc9070647d87f59bb43e621f
Barthelemy von Haller <barthelemy.von.haller@cern.ch>
2019-05-10 11:18:56 +0200

    remove extraneous print

--- Framework/src/CcdbDatabase.cxx
+++ Framework/src/CcdbDatabase.cxx
@@ -59,15 +59,9 @@ void CcdbDatabase::store(std::shared_ptr<o2::quality_control::core::MonitorObjec
   // metadata
   map<string, string> metadata;
   metadata["quality"] = std::to_string(mo->getQuality().getLevel());
-  cout << "metadata of " << mo->getName() << "(" << mo->getMetadataMap().size() << ")" << endl;
   map<string, string> userMetadata = mo->getMetadataMap();
-  for(auto& kv : userMetadata) {
-    cout << "   - " << kv.first << " : " << kv.second << endl;
-  }
   if(!userMetadata.empty()) {
-    cout << "adding elements " << endl;
     metadata.insert(userMetadata.begin(), userMetadata.end());
-    cout << "   after : " << metadata.size() << endl;
   }
 
   // other attributes
