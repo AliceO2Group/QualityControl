495042e5f8c64d6cb262497ae19aac1e11a00afb
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2019-06-26 17:23:10 +0200

    Added unit test for TOF

---
+++ Modules/TOF/test/testTOF.cxx
@@ -0,0 +1,29 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file   testTOF.cxx
+/// \author
+///
+
+#include "QualityControl/TaskFactory.h"
+
+#define BOOST_TEST_MODULE Publisher test
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+
+namespace o2::quality_control_modules::tof
+{
+
+BOOST_AUTO_TEST_CASE(instantiate_task) { BOOST_CHECK(true); }
+
+} // namespace o2::quality_control_modules::tof
