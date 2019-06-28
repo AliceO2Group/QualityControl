1d71ff515a8a842911d341831aedb45934e12df9
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2019-06-28 14:12:49 +0200

    Updated include for TOF runner

--- Modules/TOF/src/runTOF.cxx
+++ Modules/TOF/src/runTOF.cxx
@@ -67,8 +67,8 @@ void customize(std::vector<ConfigParamSpec>& workflowOptions)
 
 #include "QualityControl/Checker.h"
 #include "QualityControl/InfrastructureGenerator.h"
-#include "../../Framework/src/runnerUtils.h"
-#include "../../Framework/src/ExamplePrinterSpec.h"
+#include "runnerUtils.h"
+#include "ExamplePrinterSpec.h"
 
 std::string getConfigPath(const ConfigContext& config);
 
