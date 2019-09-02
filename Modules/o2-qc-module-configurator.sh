f244d4953b522a1d706325105c063011ee39a259
Barthélémy von Haller <barthelemy.von.haller@cern.ch>
2019-09-02 13:33:59 +0200

    Fix newline in new module's script (QC-229) (#223)

--- Modules/o2-qc-module-configurator.sh
+++ Modules/o2-qc-module-configurator.sh
@@ -6,6 +6,8 @@ set -u # exit when using undeclared variable
 DONOR=Skeleton
 DONOR_LC=skeleton
 
+OS=`uname`
+
 function inplace_sed() {
   sed -ibck "$1" $2 && rm $2bck
 }
@@ -55,7 +57,7 @@ function create_module() {
 
 function cmake_format() {
   if command cmake-format >/dev/null 2>&1; then
-    cmake-format -i $1
+    cmake-format -i $1 -c ../.cmake-format.py
   fi
 }
 
@@ -107,14 +109,19 @@ function create_class() {
   ' $DONOR_INCLUDE_FILENAME >$INCLUDE_FILENAME
 
   # add LinkDef.h
-  inplace_sed '/#endif/ i\ 
-    #pragma link C++ class o2::quality_control_modules::'${MODULE_LC}'::'$classname'+;
-
-    ' $modulename/include/$modulename/LinkDef.h
-
-  inplace_sed '/LINKDEF include/ i\ 
-    include/'$modulename'/'$classname'.h\
-    ' $modulename/CMakeLists.txt
+  if [[ $OS == Linux ]] ; then
+    sed -i '/#endif/ i #pragma link C++ class o2::quality_control_modules::'${MODULE_LC}'::'$classname'+;' \
+      $modulename/include/$modulename/LinkDef.h
+    sed -i '/HEADERS/ a \ \ include/'$modulename'/'$classname'.h' $modulename/CMakeLists.txt
+  else #Darwin/BSD
+    inplace_sed '/#endif/ i\
+      #pragma link C++ class o2::quality_control_modules::'${MODULE_LC}'::'$classname'+;\
+      \
+      ' $modulename/include/$modulename/LinkDef.h
+    inplace_sed '/LINKDEF include/ i\
+      include/'$modulename'/'$classname'.h\
+      ' $modulename/CMakeLists.txt
+  fi
 
   # add src
   sed '
