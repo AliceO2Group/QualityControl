179a02c6dd25b2bde490af18ba7d450a7534fd96
Barthelemy <barthelemy.von.haller@cern.ch>
2019-09-18 13:35:50 +0200

    trigger new build

--- Framework/src/InfrastructureGenerator.cxx
+++ Framework/src/InfrastructureGenerator.cxx
@@ -127,7 +127,7 @@ void InfrastructureGenerator::customizeInfrastructure(std::vector<framework::Com
 
 void InfrastructureGenerator::printVersion()
 {
-  // Log the version
+  // Log the version number
   QcInfoLogger::GetInstance() << "QC version " << o2::quality_control::core::Version::getString() << infologger::endm;
 }
 
