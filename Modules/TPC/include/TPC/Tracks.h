fb9a0a537cdf1aa9660bf9a6950923972808ca0a
stheckel <sheckel@cern.ch>
2019-11-19 19:24:11 +0100

    TPC QC Tracks workflow including json configs
    o This is the QualityControl part of the TPC QC task for the tracks
    o It implements the workflow for the O2 TPC QC Tracks task
    o It can be run with the also provided json configs, either:
      - using the availble TPC track reader and the sampled json
      - piped to a TPC reco workflow using the direct json

---
+++ Modules/TPC/include/TPC/Tracks.h
@@ -0,0 +1,57 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file   Tracks.h
+/// \author Stefan Heckel, sheckel@cern.ch
+///
+
+#ifndef QC_MODULE_TPC_Tracks_H
+#define QC_MODULE_TPC_Tracks_H
+
+// O2 includes
+#include "TPCQC/Tracks.h"
+
+// QC includes
+#include "QualityControl/TaskInterface.h"
+
+using namespace o2::quality_control::core;
+
+namespace o2::quality_control_modules::tpc
+{
+
+/// \brief Quality Control DPL Task for QC Module TPC for track related observables
+/// It is final because there is no reason to derive from it. Just remove it if needed.
+/// \author Barthelemy von Haller
+/// \author Piotr Konopka
+class Tracks /*final*/ : public TaskInterface // todo add back the "final" when doxygen is fixed
+{
+ public:
+  /// \brief Constructor
+  Tracks();
+  /// Destructor
+  ~Tracks() override;
+
+  // Definition of the methods for the template method pattern
+  void initialize(o2::framework::InitContext& ctx) override;
+  void startOfActivity(Activity& activity) override;
+  void startOfCycle() override;
+  void monitorData(o2::framework::ProcessingContext& ctx) override;
+  void endOfCycle() override;
+  void endOfActivity(Activity& activity) override;
+  void reset() override;
+
+ private:
+  o2::tpc::qc::Tracks mQCTracks{};
+};
+
+} // namespace o2::quality_control_modules::tpc
+
+#endif // QC_MODULE_TPC_Tracks_H
