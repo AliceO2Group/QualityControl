44052811eca609562debbf8cda60071d0840a577
Piotr Konopka <piotr.jan.konopka@cern.ch>
2020-01-09 10:05:19 +0100

    improvements in testTrendingTask

--- Framework/test/testTrendingTask.cxx
+++ Framework/test/testTrendingTask.cxx
@@ -45,49 +45,65 @@ BOOST_AUTO_TEST_CASE(test_task)
   const std::string taskName = "TestTrendingTask";
   const size_t trendTimes = 5;
 
-  // Putting the objects to trend into the database
   std::shared_ptr<DatabaseInterface> repository = DatabaseFactory::create("CCDB");
   repository->connect(CCDB_ENDPOINT, "", "", "");
 
-  TH1I* histo = new TH1I("testHistoTrending", "testHistoTrending", 10, 0, 10.0);
-  histo->Fill(4);
-  histo->Fill(5);
-  histo->Fill(6);
-  std::shared_ptr<MonitorObject> mo = std::make_shared<MonitorObject>(histo, taskName, "TST");
-  repository->storeMO(mo);
+  // Putting the objects to trend into the database
+  {
+    TH1I* histo = new TH1I("testHistoTrending", "testHistoTrending", 10, 0, 10.0);
+    histo->Fill(4);
+    histo->Fill(5);
+    histo->Fill(6);
+    std::shared_ptr<MonitorObject> mo = std::make_shared<MonitorObject>(histo, taskName, "TST");
+    repository->storeMO(mo);
 
-  std::shared_ptr<QualityObject> qo = std::make_shared<QualityObject>("testTrendingTaskCheck");
-  qo->updateQuality(Quality::Bad);
-  repository->storeQO(qo);
+    std::shared_ptr<QualityObject> qo = std::make_shared<QualityObject>("testTrendingTaskCheck");
+    qo->updateQuality(Quality::Bad);
+    repository->storeQO(qo);
+  }
 
-  ServiceRegistry services;
-  services.registerService<DatabaseInterface>(repository.get());
+  // We make sure, that destroy the previous, possibly correct test result
+  {
+    TTree* dummyTree = new TTree(taskName.c_str(), taskName.c_str());
+    repository->storeMO(std::make_shared<MonitorObject>(dummyTree, taskName, "TST"));
+    auto treeMO = repository->retrieveMO("qc/TST/" + taskName, taskName);
+    TTree* treeFromRepo = dynamic_cast<TTree*>(treeMO->getObject());
+    BOOST_REQUIRE(treeFromRepo != nullptr);
+    BOOST_REQUIRE_EQUAL(treeFromRepo->GetEntries(), 0);
+  }
 
   // Running the task
-  TrendingTask task;
-  task.setName(taskName);
-  task.configure(taskName, *ConfigurationFactory::getConfiguration(configFilePath));
-  task.initialize(Trigger::Once, services);
-  for (size_t i = 0; i < trendTimes; i++) {
-    task.update(Trigger::Always, services);
+  {
+    ServiceRegistry services;
+    services.registerService<DatabaseInterface>(repository.get());
+
+    TrendingTask task;
+    task.setName(taskName);
+    task.configure(taskName, *ConfigurationFactory::getConfiguration(configFilePath));
+    task.initialize(Trigger::Once, services);
+    for (size_t i = 0; i < trendTimes; i++) {
+      task.update(Trigger::Always, services);
+    }
+    task.finalize(Trigger::UserExit, services);
   }
-  task.finalize(Trigger::UserExit, services);
 
   // The test itself
-  auto treeMO = repository->retrieveMO("qc/TST/" + taskName, taskName); //the tree is stored under the same name as task
-  TTree* tree = dynamic_cast<TTree*>(treeMO->getObject());
+  {
+    auto treeMO = repository->retrieveMO("qc/TST/" + taskName, taskName); //the tree is stored under the same name as task
+    TTree* tree = dynamic_cast<TTree*>(treeMO->getObject());
 
-  BOOST_REQUIRE(tree != nullptr);
+    BOOST_REQUIRE(tree != nullptr);
 
-  BOOST_REQUIRE_EQUAL(tree->GetEntries(), trendTimes);
-  tree->Draw("testHistoTrending.mean:testHistoTrending.entries:testTrendingTaskCheck.level", "", "goff");
+    BOOST_REQUIRE_EQUAL(tree->GetEntries(), trendTimes);
+    tree->Draw("testHistoTrending.mean:testHistoTrending.entries:testTrendingTaskCheck.level", "", "goff");
 
-  Double_t* means = tree->GetVal(0);
-  Double_t* entries = tree->GetVal(1);
-  Double_t* qualityLevels = tree->GetVal(2);
-  for (size_t i = 0; i < trendTimes; i++) {
-    BOOST_CHECK_CLOSE(means[i], 5, 0.01);
-    BOOST_CHECK_CLOSE(entries[i], 3, 0.01);
-    BOOST_CHECK_CLOSE(qualityLevels[i], 3, 0.01);
+    Double_t* means = tree->GetVal(0);
+    Double_t* entries = tree->GetVal(1);
+    Double_t* qualityLevels = tree->GetVal(2);
+    for (size_t i = 0; i < trendTimes; i++) {
+      BOOST_CHECK_CLOSE(means[i], 5, 0.01);
+      BOOST_CHECK_CLOSE(entries[i], 3, 0.01);
+      BOOST_CHECK_CLOSE(qualityLevels[i], 3, 0.01);
+    }
   }
 }
