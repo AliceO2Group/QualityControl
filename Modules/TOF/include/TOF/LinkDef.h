2210a68466d4ad19056264c3c9708d525c139650
Nicolo' Jacazio <nicolo.jacazio@cern.ch>
2020-02-06 21:51:36 +0100

    Update CMakeLists.txt and LinkDef.h for TOF
    - Add new Task, Checker, JSON for the TOF Compressed data

--- Modules/TOF/include/TOF/LinkDef.h
+++ Modules/TOF/include/TOF/LinkDef.h
@@ -4,6 +4,8 @@
 #pragma link off all functions;
 
 #pragma link C++ class o2::quality_control_modules::tof::TOFTask+;
+#pragma link C++ class o2::quality_control_modules::tof::TOFTaskCompressed+;
+#pragma link C++ class o2::quality_control_modules::tof::TOFCheckDiagnostic+;
 #pragma link C++ class o2::quality_control_modules::tof::TOFCheckRawsMulti+;
 #pragma link C++ class o2::quality_control_modules::tof::TOFCheckRawsTime+;
 #pragma link C++ class o2::quality_control_modules::tof::TOFCheckRawsToT+;
