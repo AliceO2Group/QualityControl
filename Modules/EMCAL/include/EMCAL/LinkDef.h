83c69ef6467381277537a1eeff2d59a2b8b6dfaf
Cristina Terrevoli <cristina.terrevoli@cern.ch>
2020-02-27 17:19:08 +0100

    [EMCAL-527] Adding RawCheck
    Implemented Checks:
    - checker for ErrorType histogram

--- Modules/EMCAL/include/EMCAL/LinkDef.h
+++ Modules/EMCAL/include/EMCAL/LinkDef.h
@@ -7,6 +7,9 @@
 
 #pragma link C++ class o2::quality_control_modules::emcal::DigitCheck + ;
 
-#pragma link C++ class o2::quality_control_modules::emcal::RawTask+;
+#pragma link C++ class o2::quality_control_modules::emcal::RawTask + ;
+
+#pragma link C++ class o2::quality_control_modules::emcal::RawCheck + ;
+
 
 #endif
