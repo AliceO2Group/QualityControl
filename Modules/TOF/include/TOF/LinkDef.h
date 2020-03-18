e1b7b2fe3bc71717a1be954967c86f7559fa44b5
Nicolo' Jacazio <nicolo.jacazio@cern.ch>
2020-03-18 17:47:30 +0100

    Update LinkDef

--- Modules/TOF/include/TOF/LinkDef.h
+++ Modules/TOF/include/TOF/LinkDef.h
@@ -3,11 +3,17 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+// Tasks
 #pragma link C++ class o2::quality_control_modules::tof::TOFTask+;
 #pragma link C++ class o2::quality_control_modules::tof::TOFTaskCompressed+;
-#pragma link C++ class o2::quality_control_modules::tof::TOFDecoderCompressed+;
+#pragma link C++ class o2::quality_control_modules::tof::TOFTaskCompressedCounter+;
+// Checks
+#pragma link C++ class o2::quality_control_modules::tof::TOFCheckCompressedCounter+;
 #pragma link C++ class o2::quality_control_modules::tof::TOFCheckDiagnostic+;
 #pragma link C++ class o2::quality_control_modules::tof::TOFCheckRawsMulti+;
 #pragma link C++ class o2::quality_control_modules::tof::TOFCheckRawsTime+;
 #pragma link C++ class o2::quality_control_modules::tof::TOFCheckRawsToT+;
+// Utilities
+#pragma link C++ class o2::quality_control_modules::tof::TOFCounter+;
+#pragma link C++ class o2::quality_control_modules::tof::TOFDecoderCompressed+;
 #endif
