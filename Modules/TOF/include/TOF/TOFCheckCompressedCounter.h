2b2b09c2691797fd7057b8578ca4405e01eba73e
Nicolò Jacazio <njacazio@users.noreply.github.com>
2020-03-24 16:13:30 +0100

    Add task and checker for TOF Error Words in Raw Data (#350)
    * Add Base Counter class
    
    * Update CMakeList
    
    * Remove #include <fairlogger/Logger.h>
    
    * Update Counter class
    
    - simplified
    - add histogram maker
    - remove enumerators
    - made counter header only
    
    * Add Counter for TOF Compressed data
    
    * Add Task for Compressed counter
    
    * Add checker for TOF Compressed data counter
    
    * Update LinkDef
    
    * Add json for TOF compressed data with counters
    
    * Update counter to use UInt_t instead of enums
    
    - lose the compiler ability to spot erros but gain in flexibility
    
    * Improve Diagnostic/decoder counter for TOF Words
    
    - Change name to a more explicit one
    - Add checks into decoding loop
    - Switch to new counter scheme
    
    * Rename files after main class
    
    * Fix to the number of words
    
    * Fix cmake file
    
    * Update Template for counter
    
    - Add DEBUG mode for Counter
    - Add Reset of histograms after making them
    - Remove useless static data members, added getter
    
    * Update cmakelist
    
    * Add une DRM histogram per crate
    
    - Found out that if histograms are too small, memory will be overwritten in undefined behaviour
    
    * Add histograms for RDH, DRM, TRM, TRMChain
    
    * Add mode for 2D histograms
    
    * Reduce output of counter
    
    * Remove linkdef
    
    * Rename TOFTaskCompressedCounter to TaskDiagnostics
    
    * Changed naming in code
    
    * Rename tofcompressedcounter.json to tofdiagnostics.json
    
    * Clang format
    
    * Fix json name in Cmakefile
    
    * Remove unused file
    
    * Small fix in comments
    
    * Change name in dataSamplingPolicies
    
    * Fix warnings of ano namespace

---
+++ Modules/TOF/include/TOF/TOFCheckCompressedCounter.h
@@ -0,0 +1,48 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file   TOFCheckCompressedCounter.h
+/// \author Nicolo' Jacazio
+///
+
+#ifndef QC_MODULE_TOF_TOFCHECKCOMPRESSEDCOUNTER_H
+#define QC_MODULE_TOF_TOFCHECKCOMPRESSEDCOUNTER_H
+
+#include "QualityControl/CheckInterface.h"
+#include "QualityControl/MonitorObject.h"
+#include "QualityControl/Quality.h"
+
+namespace o2::quality_control_modules::tof
+{
+
+/// \brief  Checker for diagnostic histogram of TOF Raw data
+///
+/// \author Nicolo' Jacazio
+class TOFCheckCompressedCounter : public o2::quality_control::checker::CheckInterface
+{
+ public:
+  /// Default constructor
+  TOFCheckCompressedCounter() = default;
+  /// Destructor
+  ~TOFCheckCompressedCounter() override = default;
+
+  // Override interface
+  void configure(std::string name) override;
+  Quality check(std::map<std::string, std::shared_ptr<MonitorObject>>* moMap) override;
+  void beautify(std::shared_ptr<MonitorObject> mo, Quality checkResult) override;
+  std::string getAcceptedType() override;
+
+  ClassDefOverride(TOFCheckCompressedCounter, 1);
+};
+
+} // namespace o2::quality_control_modules::tof
+
+#endif // QC_MODULE_TOF_TOFCHECKCOMPRESSEDCOUNTER_H
---
+++ Modules/TOF/include/TOF/TaskDiagnostics.h
@@ -0,0 +1,72 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file   TaskDiagnostics.h
+/// \author Nicolo' Jacazio
+///
+
+#ifndef QC_MODULE_TOF_TASKDIAGNOSTICS_H
+#define QC_MODULE_TOF_TASKDIAGNOSTICS_H
+
+#define ENABLE_2D_HISTOGRAMS // Flag to enable 2D histograms
+
+// QC includes
+#include "QualityControl/TaskInterface.h"
+#include "TOF/Diagnostics.h"
+
+class TH1F;
+class TH2F;
+
+using namespace o2::quality_control::core;
+
+namespace o2::quality_control_modules::tof
+{
+
+/// \brief TOF Quality Control DPL Task for TOF Compressed data
+/// \author Nicolo' Jacazio
+class TaskDiagnostics    /*final*/
+  : public TaskInterface // todo add back the "final" when doxygen is fixed
+{
+ public:
+  /// \brief Constructor
+  TaskDiagnostics() = default;
+  /// Destructor
+  ~TaskDiagnostics() override = default;
+
+  // Definition of the methods for the template method pattern
+  void initialize(o2::framework::InitContext& ctx) override;
+  void startOfActivity(Activity& activity) override;
+  void startOfCycle() override;
+  void monitorData(o2::framework::ProcessingContext& ctx) override;
+  void endOfCycle() override;
+  void endOfActivity(Activity& activity) override;
+  void reset() override;
+
+ private:
+  // Histograms
+#ifdef ENABLE_2D_HISTOGRAMS
+  std::shared_ptr<TH2F> mRDHCounterHisto;                                                    /// Words per RDH
+  std::shared_ptr<TH2F> mDRMCounterHisto;                                                    /// Words per DRM
+  std::shared_ptr<TH2F> mTRMCounterHisto[Diagnostics::ntrms];                                /// Words per TRM
+  std::shared_ptr<TH2F> mTRMChainCounterHisto[Diagnostics::ntrms][Diagnostics::ntrmschains]; /// Words per TRM Chain
+#else
+  std::shared_ptr<TH1F> mRDHCounterHisto[Diagnostics::ncrates];                                                    /// Words per RDH
+  std::shared_ptr<TH1F> mDRMCounterHisto[Diagnostics::ncrates];                                                    /// Words per DRM
+  std::shared_ptr<TH1F> mTRMCounterHisto[Diagnostics::ncrates][Diagnostics::ntrms];                                /// Words per TRM
+  std::shared_ptr<TH1F> mTRMChainCounterHisto[Diagnostics::ncrates][Diagnostics::ntrms][Diagnostics::ntrmschains]; /// Words per TRM Chain
+#endif
+
+  Diagnostics mCounter; /// Decoder and counter for TOF Compressed data useful for the Task
+};
+
+} // namespace o2::quality_control_modules::tof
+
+#endif // QC_MODULE_TOF_TASKDIAGNOSTICS_H
