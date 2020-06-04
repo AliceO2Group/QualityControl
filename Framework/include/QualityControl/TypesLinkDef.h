a92343719b9620c1dd08e388153cdac907ee7671
bvonhall <barthelemy.von.haller@cern.ch>
2020-06-04 13:18:18 +0200

    add schema evolution rule

--- Framework/include/QualityControl/TypesLinkDef.h
+++ Framework/include/QualityControl/TypesLinkDef.h
@@ -9,4 +9,13 @@
 #pragma link C++ class o2::quality_control::core::QualityObject + ;
 #pragma link C++ class o2::quality_control::core::Quality + ;
 
+#pragma read                                              \
+    sourceClass="o2::quality_control::core::QualityObject"                                  \
+    source="std::map<std::string, std::string> mUserMetadata"           \
+    version="[1-2]"                               \
+    targetClass="o2::quality_control::core::QualityObject"                                  \
+    embed="true"                                          \
+    include="iostream,cstdlib"                            \
+    code="{mQuality.addMetadata(onfile.mUserMetadata);}" \
+
 #endif
