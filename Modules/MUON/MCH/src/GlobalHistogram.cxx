a11caa74c2f55f17b3d86729ec26c63260578733
seperrin <52077845+seperrin@users.noreply.github.com>
2020-06-09 17:36:44 +0200

    Update GlobalHistogram.cxx
    Removed unused xmin variable

--- Modules/MUON/MCH/src/GlobalHistogram.cxx
+++ Modules/MUON/MCH/src/GlobalHistogram.cxx
@@ -211,7 +211,6 @@ void GlobalHistogram::getDeCenterST3(int de, float& xB0, float& yB0, float& xNB0
   o2::mch::contour::BBox<double> bbox = o2::mch::mapping::getBBox(csegment);
   //std::cout<<"DE "<<de<<"  BBOX "<<bbox<<std::endl;
 
-  double xmin = bbox.xmin();
   double xmax = bbox.xmax();
   if (xId == 0) {
     // ST3 left, shift all detectors to the right
@@ -318,7 +317,6 @@ void GlobalHistogram::getDeCenterST4(int de, float& xB0, float& yB0, float& xNB0
   o2::mch::contour::BBox<double> bbox = o2::mch::mapping::getBBox(csegment);
   //std::cout<<"DE "<<de<<"  BBOX "<<bbox<<std::endl;
 
-  double xmin = bbox.xmin();
   double xmax = bbox.xmax();
   if (xId == 0) {
     // ST3 left, shift all detectors to the right
