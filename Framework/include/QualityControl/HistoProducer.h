88293f23f63e8824146bc748692da7dce1bcb15b
Barthélémy von Haller <barthelemy.von.haller@cern.ch>
2020-06-16 14:01:23 +0200

    Add a histogram producer (#421)
    * Add a histogram producer (QC-170)
    
        - Add the producer for histograms
        - Send a TObjArray from the histograms producer.

---
+++ Framework/include/QualityControl/HistoProducer.h
@@ -0,0 +1,52 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file   HistoProducer.h
+/// \author Barthelemy von Haller
+///
+
+#ifndef QUALITYCONTROL_HISTOPRODUCER_H
+#define QUALITYCONTROL_HISTOPRODUCER_H
+
+#include <Framework/DataProcessorSpec.h>
+
+namespace o2::quality_control::core
+{
+
+/// \brief Returns an histogram producer specification which publishes on {"TST", "HISTO", <index>}.
+///
+/// \param index    The index of this producer (i.e. the subspec).
+/// \return         An histograms producer specification.
+framework::DataProcessorSpec getHistoProducerSpec(size_t index, size_t nbHistograms, bool noTobjArray);
+
+/// \brief Returns an algorithm generating histograms randomly filled.
+/// The histograms have 100 bins and are named `hello<index>`.
+/// The histograms are embedded in a TObjArray.
+///
+/// \param output   Origin, Description and SubSpecification of data to be produced
+/// \param index    The value the producer should produce.
+/// \return         An histogram producer algorithm
+framework::AlgorithmSpec getHistoProducerAlgorithm(framework::ConcreteDataMatcher output, size_t nbHistograms, bool noTobjArray);
+
+/// \brief Returns a printer that prints histograms coming from {"TST", "HISTO", <index>}
+///
+/// \param index  The index of the producer (i.e. the subspec) to which the printer must connect.
+/// \return       A printer.
+framework::DataProcessorSpec getHistoPrinterSpec(size_t index);
+
+/// \brief  Returns an algorithm printing histograms.
+///
+/// \return An algorithm printing histograms.
+framework::AlgorithmSpec getHistoPrinterAlgorithm();
+
+} // namespace o2::quality_control::core
+
+#endif //QUALITYCONTROL_HISTOPRODUCER_H
---
+++ Framework/src/HistoProducer.cxx
@@ -0,0 +1,139 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file   HistoProducer.cxx
+/// \author Barthelemy von Haller
+///
+#include "QualityControl/HistoProducer.h"
+
+#include <Common/Timer.h>
+#include <TH1F.h>
+#include <QualityControl/MonitorObjectCollection.h>
+#include <string>
+
+using namespace o2::framework;
+
+using SubSpec = o2::header::DataHeader::SubSpecificationType;
+using namespace AliceO2::Common;
+using namespace std;
+
+namespace o2::quality_control::core
+{
+
+framework::DataProcessorSpec getHistoProducerSpec(size_t subspec, size_t nbHistograms, bool noTobjArray)
+{
+  return DataProcessorSpec{
+    "producer" + std::to_string(subspec),
+    Inputs{},
+    Outputs{
+      { { "out" }, "TST", "HISTO", static_cast<SubSpec>(subspec) } },
+    getHistoProducerAlgorithm({ "TST", "HISTO", static_cast<SubSpec>(subspec) }, nbHistograms, noTobjArray)
+  };
+}
+
+framework::AlgorithmSpec getHistoProducerAlgorithm(framework::ConcreteDataMatcher output, size_t nbHistograms, bool noTobjArray)
+{
+  return AlgorithmSpec{
+    [=](InitContext&) {
+      // this is the initialization code
+      std::shared_ptr<Timer> timer = nullptr;
+      double period = 2; // how many seconds between the updates of the histogram
+      vector<TH1F*> allHistos;
+      allHistos.reserve(nbHistograms);
+      for (size_t i = 0; i < nbHistograms; i++) {
+        TH1F* histo = new TH1F(string("hello_") + i, "fromHistoProducer", 100, -3, 3);
+        allHistos.push_back(histo);
+      }
+
+      return [=](ProcessingContext& processingContext) mutable {
+        // setting up the timer
+        if (!timer) {
+          timer = std::make_shared<Timer>();
+          timer->reset(static_cast<int>(1000000.0 * period));
+        }
+        // keeping the message rate
+        double timeToSleep = timer->getRemainingTime();
+        if (timeToSleep > 0) {
+          usleep(timeToSleep * 1000000.0);
+        }
+        timer->increment();
+
+        if (noTobjArray) { // just send the histogram, not a tobjarray
+          TH1F& th1f = processingContext.outputs().make<TH1F>({ output.origin, output.description, output.subSpec }, "hello", "fromHistoProducer", 100, -3, 3);
+          allHistos[0]->FillRandom("gaus", 100);
+          th1f.Add(allHistos[0]);
+          LOG(INFO) << "sending 1 histo named `hello`.";
+          return;
+        }
+
+        // Prepare the tobjarray
+        MonitorObjectCollection& monitorObjects = processingContext.outputs().make<MonitorObjectCollection>({ output.origin, output.description, output.subSpec });
+        // Fill histograms
+        for (size_t i = 0; i < nbHistograms; i++) {
+          allHistos[i]->FillRandom("gaus", 100);
+          monitorObjects.Add(allHistos[i]);
+        }
+        LOG(INFO) << "Sending a TObjArray with " << nbHistograms << " histos named `hello_<index>`.";
+      };
+    }
+  };
+}
+
+DataProcessorSpec getHistoPrinterSpec(size_t subspec)
+{
+  return DataProcessorSpec{
+    "histoPrinter",
+    Inputs{ { { "in" }, "TST", "HISTO", static_cast<SubSpec>(subspec) } },
+    Outputs{},
+    getHistoPrinterAlgorithm()
+  };
+}
+
+void printHisto(shared_ptr<const TH1F>& histo)
+{
+  LOG(INFO) << "histo : " << histo->GetName() << " : " << histo->GetTitle();
+  std::string bins = "BINS:";
+  for (int i = 1; i <= histo->GetNbinsX(); i++) {
+    bins += " " + std::to_string((int)histo->GetBinContent(i));
+  }
+  LOG(INFO) << bins;
+}
+
+framework::AlgorithmSpec getHistoPrinterAlgorithm()
+{
+  return AlgorithmSpec{
+    [=](InitContext&) {
+      // this is the initialization code
+
+      return [=](ProcessingContext& processingContext) mutable {
+        // We don't know what we receive, so we test for an array and then try a TH1F.
+        shared_ptr<const TObjArray> array = nullptr;
+        shared_ptr<const TH1F> th1f = nullptr;
+        try {
+          array = processingContext.inputs().get<TObjArray*>("in");
+        } catch (runtime_error& e) {
+          // we failed to get the TObjArray, let's try a TH1F. If it fails it will throw.
+          th1f = processingContext.inputs().get<TH1F*>("in");
+        }
+        if (array != nullptr) {
+          for (auto* const tObject : *array) {
+            std::shared_ptr<const TH1F> histo{ dynamic_cast<TH1F*>(tObject) };
+            printHisto(histo);
+          }
+        } else if (th1f != nullptr) {
+          printHisto(th1f);
+        }
+      };
+    }
+  };
+}
+
+} // namespace o2::quality_control::core
