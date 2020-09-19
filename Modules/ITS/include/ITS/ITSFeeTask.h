180412178cf9870f5c031fa018216e153c0085fe
JianLIUhep <jian.liu@cern.ch>
2020-09-19 19:41:35 +0200

    filtering the stopbit and catching the DDW

--- Modules/ITS/include/ITS/ITSFeeTask.h
+++ Modules/ITS/include/ITS/ITSFeeTask.h
@@ -17,38 +17,38 @@
 #define QC_MODULE_ITS_ITSFEETASK_H
 
 #include "QualityControl/TaskInterface.h"
-#include <ITSMFTReconstruction/ChipMappingITS.h>
-#include <ITSMFTReconstruction/PixelData.h>
-#include <ITSBase/GeometryTGeo.h>
-#include <ITSMFTReconstruction/RawPixelDecoder.h>
 
-#include <THnSparse.h>
 #include <TText.h>
+#include <TH1.h>
 #include <TH2.h>
-#include <TH2I.h>
-#include <THnSparse.h>
 #include <TPaveText.h>
 
 class TH2I;
 class TH1F;
-class TH2;
 
 using namespace o2::quality_control::core;
 using namespace o2::framework;
-using namespace std;
-using namespace o2::itsmft;
 using namespace o2::header;
-using ChipPixelData = o2::itsmft::ChipPixelData;
-using PixelReader = o2::itsmft::PixelReader;
 
 namespace o2::quality_control_modules::its
 {
 
-/// \brief ITS Fake-hit rate real-time data processing task
-/// Working with the chain of "Detector -> RU -> CRU -> Readout -> STFB -> raw-proxy ->  QC"
+/// \brief ITS FEE task aiming at 100% online data integrity checking
 class ITSFeeTask final : public TaskInterface
 {
-  using ChipPixelData = o2::itsmft::ChipPixelData;
+
+  struct GBTDdw { //GBT diagnostic word
+
+    union GBTBits {
+      struct payloadBits {
+        uint64_t flags2 : 36; /// 0:35  not defined yet
+        uint64_t flags1 : 36; /// 36:71  not defined yet
+        uint64_t id : 8;      /// 72:79  0xe0; Header Status Word (HSW) identifier
+      } payload;
+
+      uint8_t data8[16]; // 80 bits GBT word + optional padding to 128 bits
+    } ddwBits;
+  };
 
  public:
   /// \brief Constructor
@@ -67,50 +67,26 @@ class ITSFeeTask final : public TaskInterface
 
  private:
   void setAxisTitle(TH1* object, const char* xTitle, const char* yTitle);
-  void createGeneralPlots(int barrel); //create General PLots for IB/OB/ALL (1/2/3)
+  void createGeneralPlots(int barrel); //create General PLots for ITS barrels
   void createErrorTriggerPlots();
   void setPlotsFormat();
   void getEnableLayers();
   void resetGeneralPlots();
-  //detector information
-  static constexpr int NCols = 1024; //column number in Alpide chip
-  static constexpr int NRows = 512;  //row number in Alpide chip
-  static constexpr int NLayer = 7;   //layer number in ITS detector
+  static constexpr int NLayer = 7;
   static constexpr int NLayerIB = 3;
-
-  const int NSubStave[NLayer] = { 1, 1, 1, 2, 2, 2, 2 };
-  const int NStaves[NLayer] = { 12, 16, 20, 24, 30, 42, 48 };
-  const int nHicPerStave[NLayer] = { 1, 1, 1, 8, 8, 14, 14 };
-  const int nChipsPerHic[NLayer] = { 9, 9, 9, 14, 14, 14, 14 };
-  //const int ChipBoundary[NLayer + 1] = { 0, 108, 252, 432, 3120, 6480, 14712, 24120 };
   const int StaveBoundary[NLayer + 1] = { 0, 12, 28, 48, 72, 102, 144, 192 };
-
   std::array<bool, NLayer> mEnableLayers = { false };
-  //detector information end
   int mTimeFrameId = 0;
   uint32_t mTriggerTypeCount[13] = { 0 };
-
   int mNTrigger = 13;
   static constexpr int NTrigger = 13;
-
   TString mTriggerType[NTrigger] = { "ORBIT", "HB", "HBr", "HC", "PHYSICS", "PP", "CAL", "SOT", "EOT", "SOC", "EOC", "TF", "INT" };
-
-  //General plots
   TH1F* mTFInfo; //count vs TF ID
   TH2I* mTriggerVsFeeid;
   TH1D* mTriggerPlots;
   //TH1D* mInfoCanvas;//TODO: default, not implemented yet
-  //General plots end
-
-
-
-  string mRunNumberPath;
-  string mRunNumber = "000000";
-
-  //Geometry decoder
-  //the memory address of Geometry decoder, the pointers in threads will point these address
-  o2::its::GeometryTGeo* mGeom;
-  //Geometry decoder end
+  std::string mRunNumberPath;
+  std::string mRunNumber = "000000";
 };
 
 } // namespace o2::quality_control_modules::its
--- Modules/ITS/src/ITSFeeTask.cxx
+++ Modules/ITS/src/ITSFeeTask.cxx
@@ -16,20 +16,15 @@
 #include "ITS/ITSFeeTask.h"
 #include "QualityControl/QcInfoLogger.h"
 
-#include <ITSMFTReconstruction/GBTWord.h>
-
 #include <DPLUtils/RawParser.h>
 #include <DPLUtils/DPLRawParser.h>
 #include <TCanvas.h>
 #include <TDatime.h>
 #include <TGraph.h>
-#include <TH1.h>
 #include <TPaveText.h>
 #include <TPaveStats.h>
 
 #include <time.h>
-
-using namespace std;
 
 namespace o2::quality_control_modules::its
 {
@@ -37,8 +32,6 @@ namespace o2::quality_control_modules::its
 ITSFeeTask::ITSFeeTask()
   : TaskInterface()
 {
-  o2::base::GeometryManager::loadGeometry();
-  mGeom = o2::its::GeometryTGeo::Instance();
 }
 
 ITSFeeTask::~ITSFeeTask()
@@ -47,13 +40,11 @@ ITSFeeTask::~ITSFeeTask()
   delete mTriggerPlots;
   delete mTriggerVsFeeid;
   //delete mInfoCanvas;
-
-  delete mGeom;
 }
 
 void ITSFeeTask::initialize(o2::framework::InitContext& /*ctx*/)
 {
-  QcInfoLogger::GetInstance() << "initialize ITSFeeTask" << AliceO2::InfoLogger::InfoLogger::endm;
+  QcInfoLogger::GetInstance() << "Initializing the ITSFeeTask" << AliceO2::InfoLogger::InfoLogger::endm;
   getEnableLayers();
   int barrel = 0;
   if (mEnableLayers[0] or mEnableLayers[1] or mEnableLayers[2]) {
@@ -78,7 +69,6 @@ void ITSFeeTask::createErrorTriggerPlots()
 void ITSFeeTask::createGeneralPlots(int barrel = 0)
 {
 
-
   createErrorTriggerPlots();
 
   mTFInfo = new TH1F("General/TFInfo", "TF vs count", 15000, 0, 15000);
@@ -100,7 +90,6 @@ void ITSFeeTask::createGeneralPlots(int barrel = 0)
   } else if (barrel == 3) { //create for All	TODO:create the Error/Trigger vs feeid plots
   }
 }
-
 
 void ITSFeeTask::setAxisTitle(TH1* object, const char* xTitle, const char* yTitle)
 {
@@ -126,7 +115,6 @@ void ITSFeeTask::setPlotsFormat()
       mTriggerVsFeeid->GetYaxis()->SetBinLabel(i + 1, mTriggerType[i]);
     }
   }
-
 }
 
 void ITSFeeTask::startOfActivity(Activity& /*activity*/)
@@ -144,8 +132,6 @@ void ITSFeeTask::monitorData(o2::framework::ProcessingContext& ctx)
   int difference;
   start = std::chrono::high_resolution_clock::now();
 
-
-
   //Fill the Trigger plots
   DPLRawParser parser(ctx.inputs());
   int StaveStart = 0;
@@ -158,12 +144,15 @@ void ITSFeeTask::monitorData(o2::framework::ProcessingContext& ctx)
     int ilink = (int)((rdh->feeId & 0x0f00) >> 8);
     istave += StaveStart;
 
-    int stopbit = (int)(rdh->stop);
-    std::cout << "stopbit is: " << stopbit  << std::endl;
-        
-    auto const* ddw = reinterpret_cast<const o2::itsmft::GBTDataTrailer*>(rdh)+4;
-    ddw->printX();    
-
+    if ((int)(rdh->stop)) {
+      ILOG(Debug) << "Stopbit : " << (int)rdh->stop << ", Payload size: " << it.size() << ENDM;
+      auto const* ddw = reinterpret_cast<const GBTDdw*>(it.data());
+      if (it.size()) {
+        LOGF(INFO, "0x: %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x",
+             ddw->ddwBits.data8[15], ddw->ddwBits.data8[14], ddw->ddwBits.data8[13], ddw->ddwBits.data8[12], ddw->ddwBits.data8[11], ddw->ddwBits.data8[10],
+             ddw->ddwBits.data8[9], ddw->ddwBits.data8[8], ddw->ddwBits.data8[7], ddw->ddwBits.data8[6], ddw->ddwBits.data8[5], ddw->ddwBits.data8[4], ddw->ddwBits.data8[3], ddw->ddwBits.data8[2], ddw->ddwBits.data8[1], ddw->ddwBits.data8[0]);
+      }
+    }
     for (int i = 0; i < 13; i++) {
       if (((uint32_t)(rdh->triggerType) >> i & 1) == 1) {
         mTriggerPlots->Fill(i + 1);
@@ -171,13 +160,8 @@ void ITSFeeTask::monitorData(o2::framework::ProcessingContext& ctx)
       }
     }
   }
-  //Fill the Trigger plots end
 
-
-
-  //get the Time Frame ID
   mTimeFrameId = ctx.inputs().get<int>("G");
-  //get the Time Frame ID end
 
   mTFInfo->Fill(mTimeFrameId);
   end = std::chrono::high_resolution_clock::now();
@@ -201,7 +185,7 @@ void ITSFeeTask::endOfCycle()
 {
   std::ifstream runNumberFile("/home/its/QC_Online/workdir/infiles/RunNumber.dat"); //catching ITS run number in commissioning
   if (runNumberFile) {
-    string runNumber;
+    std::string runNumber;
     runNumberFile >> runNumber;
     ILOG(Info) << "runNumber : " << runNumber << ENDM;
     if (runNumber == mRunNumber) {
