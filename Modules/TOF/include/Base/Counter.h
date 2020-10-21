d3fab91037508794f0426b15ebd566f3f3a1aa7a
Nicolo' Jacazio <nicolo.jacazio@cern.ch>
2020-10-21 13:38:35 +0200

    Align TOF diagnostic naming scheme to O2
    - Using correct loop when reading raw data
    - Simplified Counter class, remove intermediate class

--- Modules/TOF/include/Base/Counter.h
+++ Modules/TOF/include/Base/Counter.h
@@ -35,7 +35,7 @@ namespace o2::quality_control_modules::tof
 
 /// \brief Class to count events
 /// \author Nicolo' Jacazio
-template <typename Tc>
+template <const unsigned size, const char* labels[size]>
 class Counter
 {
  public:
@@ -47,9 +47,9 @@ class Counter
 
   /// Function to increment a counter
   /// @param v Index in the counter array to increment
-  void Count(UInt_t v)
+  void Count(const unsigned int& v)
   {
-    if (v > Tc::size) {
+    if (v > size) {
       LOG(FATAL) << "Incrementing counter too far! " << v << "/" << Size();
     }
 #ifdef ENABLE_COUNTER_DEBUG_MODE
@@ -62,28 +62,28 @@ class Counter
   void Reset()
   {
     LOG(INFO) << "Resetting Counter";
-    for (UInt_t i = 0; i < Tc::size; i++) {
+    for (unsigned int i = 0; i < size; i++) {
       counter[i] = 0;
     }
   }
 
   /// Function to get how many counts where observed
   /// @return Returns the number of counts observed for a particular index
-  uint32_t HowMany(UInt_t pos) const { return counter[pos]; }
+  uint32_t HowMany(const unsigned int& pos) const { return counter[pos]; }
 
   /// Function to make a histogram out of the counters
   /// @param h histogram to shape in order to have room for the counter size
   void MakeHistogram(TH1* h) const
   {
-    LOG(INFO) << "Making Histogram " << h->GetName() << " to accomodate counter of size " << Tc::size;
+    LOG(INFO) << "Making Histogram " << h->GetName() << " to accomodate counter of size " << size;
     TAxis* axis = h->GetXaxis();
-    if (((UInt_t)axis->GetNbins()) < Tc::size) {
-      LOG(FATAL) << "The histogram size (" << axis->GetNbins() << ") is not large enough to accomodate the counter size (" << Tc::size << ")";
+    if (static_cast<unsigned int>(axis->GetNbins()) < size) {
+      LOG(FATAL) << "The histogram size (" << axis->GetNbins() << ") is not large enough to accomodate the counter size (" << size << ")";
     }
     h->Reset();
-    UInt_t histo_size = Tc::size;
-    for (UInt_t i = 0; i < Tc::size; i++) {
-      if (Tc::names[i].IsNull()) {
+    unsigned int histo_size = size;
+    for (unsigned int i = 0; i < size; i++) {
+      if (labels[i] && labels[i][0]) {
         histo_size--;
       }
     }
@@ -91,16 +91,16 @@ class Counter
       LOG(FATAL) << "Asked to produce a histogram with size " << histo_size << ", check counter bin labels";
     }
     axis->Set(histo_size, 0, histo_size);
-    UInt_t binx = 1;
-    for (UInt_t i = 0; i < Tc::size; i++) {
-      if (Tc::names[i].IsNull()) {
+    unsigned int binx = 1;
+    for (unsigned int i = 0; i < size; i++) {
+      if (labels[i] && labels[i][0]) {
         continue;
       }
-      LOG(INFO) << "Setting bin " << binx << "/" << histo_size << " to contain counter for " << Tc::names[i] << " (index " << i << "/" << Tc::size - 1 << ")";
+      LOG(INFO) << "Setting bin " << binx << "/" << histo_size << " to contain counter for " << labels[i] << " (index " << i << "/" << size - 1 << ")";
       if (histo_size < binx) {
         LOG(FATAL) << "Making bin outside of histogram limits!";
       }
-      axis->SetBinLabel(binx++, Tc::names[i]);
+      axis->SetBinLabel(binx++, labels[i]);
     }
     h->Reset();
 #ifdef ENABLE_PRINT_HISTOGRAMS_MODE
@@ -112,35 +112,38 @@ class Counter
   /// @param h The histogram to fill
   /// @param biny Y offset to fill to histogram, useful for TH2 and TH3
   /// @param binz Z offset to fill to histogram, useful for TH3
-  void FillHistogram(TH1* h, UInt_t biny = 0, UInt_t binz = 0) const
+  void FillHistogram(TH1* h, const unsigned int& biny = 0, const unsigned int& binz = 0) const
   {
     LOG(INFO) << "Filling Histogram " << h->GetName() << " with counter contents";
-    UInt_t binx = 1;
-    const UInt_t nbinsx = h->GetNbinsX();
-    for (UInt_t i = 0; i < Tc::size; i++) {
-      if (Tc::names[i].IsNull()) {
+    unsigned int binx = 1;
+    const unsigned int nbinsx = h->GetNbinsX();
+    for (unsigned int i = 0; i < size; i++) {
+      if (labels[i] && labels[i][0]) {
         if (counter[i] > 0) {
-          LOG(WARNING) << "Counter at position " << i << " was non empty (" << counter[i] << ") but was discarded because of empty lables";
+          LOG(FATAL) << "Counter at position " << i << " was non empty (" << counter[i] << ") but was discarded because of empty labels";
         }
         continue;
       }
 #ifdef ENABLE_COUNTER_DEBUG_MODE
-      LOG(INFO) << "Filling bin " << binx << " of position " << i << " of label " << Tc::names[i] << " with " << counter[i];
+      LOG(INFO) << "Filling bin " << binx << " of position " << i << " of label " << labels[i] << " with " << counter[i];
 #endif
       if (binx > nbinsx) {
         LOG(FATAL) << "Filling histogram " << h->GetName() << " at position " << binx << " i.e. past its size (" << nbinsx << ")!";
       }
-      if (!Tc::names[i].EqualTo(h->GetXaxis()->GetBinLabel(binx))) {
-        LOG(FATAL) << "Bin " << binx << " does not have the expected label '" << h->GetXaxis()->GetBinLabel(binx) << "' vs '" << Tc::names[i] << "'";
+      const char* bin_label = h->GetXaxis()->GetBinLabel(binx);
+      if (strcmp(labels[i], bin_label) != 0) {
+        LOG(FATAL) << "Bin " << binx << " does not have the expected label '" << bin_label << "' vs '" << labels[i] << "'";
       }
-      if (biny > 0) {
-        if (binz > 0) {
-          h->SetBinContent(binx, biny, binz, counter[i]);
+      if (counter[i] > 0) {
+        if (biny > 0) {
+          if (binz > 0) {
+            h->SetBinContent(binx, biny, binz, counter[i]);
+          } else {
+            h->SetBinContent(binx, biny, counter[i]);
+          }
         } else {
-          h->SetBinContent(binx, biny, counter[i]);
+          h->SetBinContent(binx, counter[i]);
         }
-      } else {
-        h->SetBinContent(binx, counter[i]);
       }
       binx++;
     }
@@ -148,15 +151,14 @@ class Counter
     h->Print("All");
 #endif
   }
-  /// Getter for the Tc::size
+  /// Getter for the size
   /// @return Returns the size of the counter
-  UInt_t Size() const { return Tc::size; }
+  unsigned int Size() const { return size; }
 
  private:
-  static_assert(std::is_same<decltype(Tc::size), const UInt_t>::value, "size must be const UInt_t");
-  static_assert(std::is_same<decltype(Tc::names), const TString[Tc::size]>::value, "names must be const TString arrays");
+  // static_assert((sizeof(labels) / sizeof(const char*)) == size, "size of the counter and the one of the labels must coincide");
   /// Containers to fill
-  std::array<uint32_t, Tc::size> counter = { 0 };
+  std::array<uint32_t, size> counter = { 0 };
 };
 
 } // namespace o2::quality_control_modules::tof
