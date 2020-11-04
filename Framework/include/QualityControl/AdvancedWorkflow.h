1734bc0a25c8a93c3574a99ba8713b9b6ebad337
Barthelemy <barthelemy.von.haller@cern.ch>
2020-11-04 11:17:54 +0100

    Extract advanced topology creation

---
+++ Framework/include/QualityControl/AdvancedWorkflow.h
@@ -0,0 +1,35 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file   AdvancedWorkflow.h
+/// \author Barthelemy von Haller
+/// \author Piotr Konopka
+///
+
+#ifndef QUALITYCONTROL_AdvancedWorkflow_H
+#define QUALITYCONTROL_AdvancedWorkflow_H
+
+#include <Framework/CompletionPolicyHelpers.h>
+#include <Framework/ConfigContext.h>
+#include <Framework/DataSpecUtils.h>
+#include <Framework/CompletionPolicyHelpers.h>
+#include <Framework/WorkflowSpec.h>
+
+/// These methods can be used to build a complex processing topology. It spawns 3 separate dummy processing chains,
+/// each made of a
+
+namespace o2::quality_control::core
+{
+  o2::framework::WorkflowSpec getProcessingTopology(o2::framework::DataAllocator::SubSpecificationType subspec);
+  o2::framework::WorkflowSpec getFullProcessingTopology();
+} // namespace o2::quality_control::core
+
+#endif //QUALITYCONTROL_AdvancedWorkflow_H
