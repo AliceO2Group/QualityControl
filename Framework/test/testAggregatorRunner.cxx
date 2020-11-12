ac863f01a7c8a6ae50468e306f8fa3a648696f1f
Barthelemy <barthelemy.von.haller@cern.ch>
2020-11-12 16:04:07 +0100

    fix

--- Framework/test/testAggregatorRunner.cxx
+++ Framework/test/testAggregatorRunner.cxx
@@ -31,6 +31,5 @@ BOOST_AUTO_TEST_CASE(test_check_runner_static)
 {
   BOOST_CHECK(AggregatorRunner::createAggregatorRunnerDataDescription("qwertyuiop") == DataDescription("qwertyuiop"));
   BOOST_CHECK(AggregatorRunner::createAggregatorRunnerDataDescription("012345678901234567890") == DataDescription("012345678901"));
-  AggregatorRunner::createAggregatorRunnerDataDescription("012345678901234567890")
     BOOST_CHECK_THROW(AggregatorRunner::createAggregatorRunnerDataDescription(""), AliceO2::Common::FatalException);
 }
