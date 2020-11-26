1cdb7d1a933924dcc9e4abc88ad7b10512817b82
tmtetwa <tokozanivn@gmail.com>
2020-11-26 08:12:59 +0200

    added the superficial check, it's a place holder for now

--- Modules/TRD/include/TRD/LinkDef.h
+++ Modules/TRD/include/TRD/LinkDef.h
@@ -4,5 +4,6 @@
 #pragma link off all functions;
 
 #pragma link C++ class o2::quality_control_modules::trd::DigitsTask+;
+#pragma link C++ class o2::quality_control_modules::trd::DigitsCheck+;
 
 #endif
