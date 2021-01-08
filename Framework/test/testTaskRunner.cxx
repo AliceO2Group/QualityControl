bc7d0dfd9b6d2a4d14f1c23e77aeb7ea27dc6ff6
Barthelemy <barthelemy.von.haller@cern.ch>
2021-01-08 13:31:21 +0100

    fix test

--- Framework/test/testTaskRunner.cxx
+++ Framework/test/testTaskRunner.cxx
@@ -76,7 +76,7 @@ BOOST_AUTO_TEST_CASE(test_task_runner)
 
   BOOST_CHECK_EQUAL(qcTask.getOutputSpec(), (OutputSpec{ { "mo" }, "QC", "abcTask-mo", 0 }));
 
-  BOOST_REQUIRE_EQUAL(qcTask.getOptions().size(), 1);
+  BOOST_REQUIRE_EQUAL(qcTask.getOptions().size(), 2);
   BOOST_CHECK_EQUAL(qcTask.getOptions()[0].name, "period-timer-cycle");
 
   // This is maximum that we can do until we are able to test the DPL algorithms in isolation.
