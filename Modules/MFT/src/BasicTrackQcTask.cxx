546f0f01a1649cdc2265edb0cde4f8b9d95fdc56
tomas-herman <45421964+tomas-herman@users.noreply.github.com>
2021-03-24 15:17:38 +0100

    [MFT-8] MFT Track QC Task update (#649)
    * Adding histograms to the Track task.
    
    * Removing dummy metadata.
    
    * Hist range change.

--- Modules/MFT/src/BasicTrackQcTask.cxx
+++ Modules/MFT/src/BasicTrackQcTask.cxx
@@ -12,7 +12,7 @@
 /// \file   BasicTrackQcTask.cxx
 /// \author Tomas Herman
 /// \author Guillermo Contreras
-///
+/// \author Diana Maria Krupova
 
 // ROOT
 #include <TH1.h>
@@ -44,33 +44,44 @@ void BasicTrackQcTask::initialize(o2::framework::InitContext& /*ctx*/)
     ILOG(Info, Devel) << "Custom parameter - myOwnKey: " << param->second << ENDM;
   }
 
-  mMFT_number_of_clusters_per_track_H = std::make_unique<TH1F>("mMFT_number_of_clusters_per_track_H", "mMFT_number_of_clusters_per_track_H", 16, -0.5, 15.5);
+  mMFT_number_of_clusters_per_track_H = std::make_unique<TH1F>("mMFT_number_of_clusters_per_track_H", "mMFT_number_of_clusters_per_track_H", 10, 0.5, 10.5);
   getObjectsManager()->startPublishing(mMFT_number_of_clusters_per_track_H.get());
-  getObjectsManager()->addMetadata(mMFT_number_of_clusters_per_track_H->GetName(), "custom", "34");
+
+  mMFT_number_of_clusters_per_CA_track_H = std::make_unique<TH1F>("mMFT_number_of_clusters_per_CA_track_H", "mMFT_number_of_clusters_per_CA_track_H", 10, 0.5, 10.5);
+  getObjectsManager()->startPublishing(mMFT_number_of_clusters_per_CA_track_H.get());
+
+  mMFT_number_of_clusters_per_LTF_track_H = std::make_unique<TH1F>("mMFT_number_of_clusters_per_LTF_track_H", "mMFT_number_of_clusters_per_LTF_track_H", 10, 0.5, 10.5);
+  getObjectsManager()->startPublishing(mMFT_number_of_clusters_per_LTF_track_H.get());
+
+  mMFT_track_inv_qpt_H = std::make_unique<TH1F>("mMFT_track_inv_qpt_H", "mMFT_track_inv_qpt_H", 100, -5, 5);
+  getObjectsManager()->startPublishing(mMFT_track_inv_qpt_H.get());
 
   mMFT_track_chi2_H = std::make_unique<TH1F>("mMFT_track_chi2_H", "mMFT_track_chi2_H", 81, -0.5, 80.5);
   getObjectsManager()->startPublishing(mMFT_track_chi2_H.get());
-  getObjectsManager()->addMetadata(mMFT_track_chi2_H->GetName(), "custom", "34");
 
   mMFT_charge_H = std::make_unique<TH1F>("mMFT_charge_H", "mMFT_charge_H", 3, -1.5, 1.5);
   getObjectsManager()->startPublishing(mMFT_charge_H.get());
-  getObjectsManager()->addMetadata(mMFT_charge_H->GetName(), "custom", "34");
+
+  mMFT_phi_H = std::make_unique<TH1F>("mMFT_phi_H", "mMFT_phi_H", 100, -3.2, 3.2);
+  getObjectsManager()->startPublishing(mMFT_phi_H.get());
 
   mMFT_pos_phi_H = std::make_unique<TH1F>("mMFT_pos_phi_H", "mMFT_pos_phi_H", 100, -3.2, 3.2);
   getObjectsManager()->startPublishing(mMFT_pos_phi_H.get());
-  getObjectsManager()->addMetadata(mMFT_pos_phi_H->GetName(), "custom", "34");
 
   mMFT_neg_phi_H = std::make_unique<TH1F>("mMFT_neg_phi_H", "mMFT_neg_phi_H", 100, -3.2, 3.2);
   getObjectsManager()->startPublishing(mMFT_neg_phi_H.get());
-  getObjectsManager()->addMetadata(mMFT_neg_phi_H->GetName(), "custom", "34");
 
-  mMFT_eta_H = std::make_unique<TH1F>("mMFT_eta_H", "mMFT_eta_H", 100, -5, -1);
-  getObjectsManager()->startPublishing(mMFT_eta_H.get());
-  getObjectsManager()->addMetadata(mMFT_eta_H->GetName(), "custom", "34");
+  mMFT_eta_track_H = std::make_unique<TH1F>("mMFT_eta_track_H", "mMFT_eta_track_H", 100, -5, -1);
+  getObjectsManager()->startPublishing(mMFT_eta_track_H.get());
+
+  mMFT_eta_CA_track_H = std::make_unique<TH1F>("mMFT_eta_CA_track_H", "mMFT_eta_CA_track_H", 100, -5, -1);
+  getObjectsManager()->startPublishing(mMFT_eta_CA_track_H.get());
+
+  mMFT_eta_LTF_track_H = std::make_unique<TH1F>("mMFT_eta_LTF_track_H", "mMFT_eta_LTF_track_H", 100, -5, -1);
+  getObjectsManager()->startPublishing(mMFT_eta_LTF_track_H.get());
 
   mMFT_tang_lambda_H = std::make_unique<TH1F>("mMFT_tang_lambda_H", "mMFT_tang_lambda_H", 100, -25, 0);
   getObjectsManager()->startPublishing(mMFT_tang_lambda_H.get());
-  getObjectsManager()->addMetadata(mMFT_tang_lambda_H->GetName(), "custom", "34");
 }
 
 void BasicTrackQcTask::startOfActivity(Activity& /*activity*/)
@@ -78,11 +89,17 @@ void BasicTrackQcTask::startOfActivity(Activity& /*activity*/)
   ILOG(Info, Support) << "startOfActivity" << ENDM;
 
   mMFT_number_of_clusters_per_track_H->Reset();
+  mMFT_number_of_clusters_per_CA_track_H->Reset();
+  mMFT_number_of_clusters_per_LTF_track_H->Reset();
+  mMFT_track_inv_qpt_H->Reset();
   mMFT_track_chi2_H->Reset();
   mMFT_charge_H->Reset();
+  mMFT_phi_H->Reset();
   mMFT_pos_phi_H->Reset();
   mMFT_neg_phi_H->Reset();
-  mMFT_eta_H->Reset();
+  mMFT_eta_track_H->Reset();
+  mMFT_eta_CA_track_H->Reset();
+  mMFT_eta_LTF_track_H->Reset();
   mMFT_tang_lambda_H->Reset();
 }
 
@@ -102,13 +119,28 @@ void BasicTrackQcTask::monitorData(o2::framework::ProcessingContext& ctx)
     mMFT_number_of_clusters_per_track_H->Fill(one_track.getNumberOfPoints());
     mMFT_track_chi2_H->Fill(one_track.getTrackChi2());
     mMFT_charge_H->Fill(one_track.getCharge());
-    mMFT_eta_H->Fill(one_track.getEta());
+    mMFT_phi_H->Fill(one_track.getPhi());
+    mMFT_eta_track_H->Fill(one_track.getEta());
     mMFT_tang_lambda_H->Fill(one_track.getTanl());
 
-    if (one_track.getCharge() == +1)
+    if (one_track.getCharge() == +1) {
       mMFT_pos_phi_H->Fill(one_track.getPhi());
-    if (one_track.getCharge() == -1)
+      mMFT_track_inv_qpt_H->Fill(1 / one_track.getPt());
+    }
+
+    if (one_track.getCharge() == -1) {
       mMFT_neg_phi_H->Fill(one_track.getPhi());
+      mMFT_track_inv_qpt_H->Fill(-1 / one_track.getPt());
+    }
+
+    if (one_track.isCA()) {
+      mMFT_number_of_clusters_per_CA_track_H->Fill(one_track.getNumberOfPoints());
+      mMFT_eta_CA_track_H->Fill(one_track.getEta());
+    }
+    if (one_track.isLTF()) {
+      mMFT_number_of_clusters_per_LTF_track_H->Fill(one_track.getNumberOfPoints());
+      mMFT_eta_LTF_track_H->Fill(one_track.getEta());
+    }
   }
 }
 
@@ -129,11 +161,17 @@ void BasicTrackQcTask::reset()
   ILOG(Info, Support) << "Resetting the histogram" << ENDM;
 
   mMFT_number_of_clusters_per_track_H->Reset();
+  mMFT_number_of_clusters_per_CA_track_H->Reset();
+  mMFT_number_of_clusters_per_LTF_track_H->Reset();
+  mMFT_track_inv_qpt_H->Reset();
   mMFT_track_chi2_H->Reset();
   mMFT_charge_H->Reset();
+  mMFT_phi_H->Reset();
   mMFT_pos_phi_H->Reset();
   mMFT_neg_phi_H->Reset();
-  mMFT_eta_H->Reset();
+  mMFT_eta_track_H->Reset();
+  mMFT_eta_CA_track_H->Reset();
+  mMFT_eta_LTF_track_H->Reset();
   mMFT_tang_lambda_H->Reset();
 }
 
