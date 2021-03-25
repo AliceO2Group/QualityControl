504fd35fdc62d2dd71a379878b801cdc6afd30b7
Barthélémy von Haller <barthelemy.von.haller@gmail.com>
2021-03-25 10:09:30 +0100

    Update SkeletonTask.cxx

--- Modules/Skeleton/src/SkeletonTask.cxx
+++ Modules/Skeleton/src/SkeletonTask.cxx
@@ -19,7 +19,6 @@
 #include "QualityControl/QcInfoLogger.h"
 #include "Skeleton/SkeletonTask.h"
 #include <Framework/InputRecord.h>
-#include <Framework/Logger.h>
 
 namespace o2::quality_control_modules::skeleton
 {
