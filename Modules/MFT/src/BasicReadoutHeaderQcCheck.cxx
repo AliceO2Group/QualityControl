eece593167218731b4f7078ba41ee0f9be2f5918
Tomas Herman <tomas.herman@cern.ch>
2021-03-31 14:42:34 +0200

    Modified the readout task to monitor each lane on all RUs.

--- Modules/MFT/src/BasicReadoutHeaderQcCheck.cxx
+++ Modules/MFT/src/BasicReadoutHeaderQcCheck.cxx
@@ -17,7 +17,8 @@
 #include <fairlogger/Logger.h>
 // ROOT
 #include <TH1.h>
-#include <TPaveText.h>
+#include <TH2.h>
+#include <TLatex.h>
 #include <TList.h>
 // Quality Control
 #include "MFT/BasicReadoutHeaderQcCheck.h"
@@ -39,7 +40,7 @@ Quality BasicReadoutHeaderQcCheck::check(std::map<std::string, std::shared_ptr<M
 
     (void)moName;
 
-    if (mo->getName() == "mMFT_LaneStatus_H") {
+    if (mo->getName() == "mMFT_SummaryLaneStatus_H") {
       auto* h = dynamic_cast<TH1F*>(mo->getObject());
 
       // test it: if entries in bins 1 to 4
@@ -53,28 +54,24 @@ Quality BasicReadoutHeaderQcCheck::check(std::map<std::string, std::shared_ptr<M
   return result;
 }
 
-std::string BasicReadoutHeaderQcCheck::getAcceptedType() { return "TH1"; }
+std::string BasicReadoutHeaderQcCheck::getAcceptedType() { return "TH1, TH2"; }
 
 void BasicReadoutHeaderQcCheck::beautify(std::shared_ptr<MonitorObject> mo, Quality checkResult)
 {
-  if (mo->getName() == "mMFT_LaneStatus_H") {
+  if (mo->getName() == "mMFT_SummaryLaneStatus_H") {
     auto* h = dynamic_cast<TH1F*>(mo->getObject());
-
-    TPaveText* message = new TPaveText(0.3, 0.8, 0.5, 0.9, "NDC");
-    h->GetListOfFunctions()->Add(message);
-    message->SetBorderSize(1);
 
     if (checkResult == Quality::Good) {
       h->SetLineColor(kGreen + 2);
-      message->AddText("Check status: Good!");
-      message->SetFillColor(kGreen + 2);
-      message->SetTextColor(kWhite);
+      TLatex* tl = new TLatex(1.4, 1.05 * h->GetMaximum(), "#color[418]{Check status: Good!}");
+      h->GetListOfFunctions()->Add(tl);
+      tl->Draw();
     } else if (checkResult == Quality::Bad) {
       LOG(INFO) << "Quality::Bad, setting to red";
       h->SetLineColor(kRed + 1);
-      message->AddText("Check status: Bad!");
-      message->SetFillColor(kRed + 1);
-      message->SetTextColor(kWhite);
+      TLatex* tl = new TLatex(1.4, 1.05 * h->GetMaximum(), "#color[633]{Check status: Bad!}");
+      h->GetListOfFunctions()->Add(tl);
+      tl->Draw();
     }
   }
 }
