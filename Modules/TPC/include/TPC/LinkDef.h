461e2f1b952592224ea1a8902e2cdf9ec49dc4e7
Thomas Klemenz <thomas.klemenz@tum.de>
2021-04-29 15:14:17 +0200

    TPC: rawDataQA

--- Modules/TPC/include/TPC/LinkDef.h
+++ Modules/TPC/include/TPC/LinkDef.h
@@ -11,6 +11,7 @@
 #pragma link C++ class o2::quality_control_modules::tpc::ROCReductor+;
 #pragma link C++ class o2::quality_control_modules::tpc::Clusters+;
 #pragma link C++ class o2::quality_control_modules::tpc::CalDetPublisher+;
+#pragma link C++ class o2::quality_control_modules::tpc::RawDigits+;
 
 #pragma link C++ function o2::quality_control_modules::tpc::addAndPublish+;
 #pragma link C++ function o2::quality_control_modules::tpc::toVector+;
---
+++ Modules/TPC/src/RawDigits.cxx
@@ -0,0 +1,101 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file   RawDigits.cxx
+/// \author Thomas Klemenz
+///
+
+// O2 includes
+#include "Framework/ProcessingContext.h"
+#include "DataFormatsTPC/ClusterNative.h"
+#include "TPCBase/Painter.h"
+
+// QC includes
+#include "QualityControl/QcInfoLogger.h"
+#include "TPC/RawDigits.h"
+#include "TPC/Utility.h"
+
+namespace o2::quality_control_modules::tpc
+{
+
+RawDigits::RawDigits() : TaskInterface()
+{
+  mWrapperVector.emplace_back(&mRawDigitQC.getNClusters());
+  mWrapperVector.emplace_back(&mRawDigitQC.getQMax());
+  mWrapperVector.emplace_back(&mRawDigitQC.getTimeBin());
+}
+
+void RawDigits::initialize(o2::framework::InitContext& /*ctx*/)
+{
+  QcInfoLogger::GetInstance() << "initialize TPC RawDigits QC task" << AliceO2::InfoLogger::InfoLogger::endm;
+
+  mRawReader.createReader("");
+
+  addAndPublish(getObjectsManager(), mNRawDigitsCanvasVec, { "c_Sides_N_RawDigits", "c_ROCs_N_RawDigits_1D", "c_ROCs_N_RawDigits_2D" });
+  addAndPublish(getObjectsManager(), mQMaxCanvasVec, { "c_Sides_Q_Max", "c_ROCs_Q_Max_1D", "c_ROCs_Q_Max_2D" });
+  addAndPublish(getObjectsManager(), mTimeBinCanvasVec, { "c_Sides_Time_Bin", "c_ROCs_Time_Bin_1D", "c_ROCs_Time_Bin_2D" });
+
+  for (auto& wrapper : mWrapperVector) {
+    getObjectsManager()->startPublishing(&wrapper);
+    getObjectsManager()->addMetadata(wrapper.getObj()->getName().data(), "custom", "87");
+  }
+
+  mRawReader.setLinkZSCallback([this](int cru, int rowInSector, int padInRow, int timeBin, float adcValue) -> bool {
+    mRawDigitQC.fillADCValue(cru, rowInSector, padInRow, timeBin, adcValue);
+    return true;
+  });
+}
+
+void RawDigits::startOfActivity(Activity& /*activity*/)
+{
+  QcInfoLogger::GetInstance() << "startOfActivity" << AliceO2::InfoLogger::InfoLogger::endm;
+}
+
+void RawDigits::startOfCycle()
+{
+  QcInfoLogger::GetInstance() << "startOfCycle" << AliceO2::InfoLogger::InfoLogger::endm;
+}
+
+void RawDigits::monitorData(o2::framework::ProcessingContext& ctx)
+{
+  auto& reader = mRawReader.getReaders()[0];
+  o2::tpc::calib_processing_helper::processRawData(ctx.inputs(), reader, false);
+
+  mRawDigitQC.analyse();
+
+  auto vecPtrNRawDigits = toVector(mNRawDigitsCanvasVec);
+  o2::tpc::painter::makeSummaryCanvases(mRawDigitQC.getNClusters(), 300, 0, 0, true, &vecPtrNRawDigits);
+
+  auto vecPtrQMax = toVector(mQMaxCanvasVec);
+  o2::tpc::painter::makeSummaryCanvases(mRawDigitQC.getQMax(), 300, 0, 0, true, &vecPtrQMax);
+
+  auto vecPtrTimeBin = toVector(mTimeBinCanvasVec);
+  o2::tpc::painter::makeSummaryCanvases(mRawDigitQC.getTimeBin(), 300, 0, 0, true, &vecPtrTimeBin);
+}
+
+void RawDigits::endOfCycle()
+{
+  QcInfoLogger::GetInstance() << "endOfCycle" << AliceO2::InfoLogger::InfoLogger::endm;
+}
+
+void RawDigits::endOfActivity(Activity& /*activity*/)
+{
+  QcInfoLogger::GetInstance() << "endOfActivity" << AliceO2::InfoLogger::InfoLogger::endm;
+}
+
+void RawDigits::reset()
+{
+  // clean all the monitor objects here
+
+  QcInfoLogger::GetInstance() << "Resetting the histogram" << AliceO2::InfoLogger::InfoLogger::endm;
+}
+
+} // namespace o2::quality_control_modules::tpc
