07863c23f2fc8da65a8cc3b4dbef76ccac965fde
Piotr Konopka <piotr.jan.konopka@cern.ch>
2021-05-19 10:09:36 +0200

    Multinode test - wait a few seconds after running the first workflow (#697)
    That hopefully will avoid potential problems with DPL trying to use the same resources (i am not sure if this was actually the problem of the failed test in https://github.com/AliceO2Group/QualityControl/pull/696)
    
    Co-authored-by: Barthélémy von Haller <barthelemy.von.haller@cern.ch>

--- Framework/script/o2-qc-multinode-test.sh
+++ Framework/script/o2-qc-multinode-test.sh
@@ -85,6 +85,7 @@ delete_data
 # store data
 o2-qc-run-producer --producers 2 --message-amount 15  --message-rate 1 -b | timeout -k 5s 25s o2-qc --config json://${JSON_DIR}/multinode-test.json -b --local --host localhost --run &
 
+sleep 3
 timeout -k 5s 30s o2-qc --config json://${JSON_DIR}/multinode-test.json -b --remote --run
 
 # wait until the local QC quits before moving forward
