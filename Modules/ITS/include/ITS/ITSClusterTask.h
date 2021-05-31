87b85409b14f22cb13a747fec14d5bfeeca4fb57
IsakovAD <jian.liu@cern.ch>
2021-05-31 10:42:09 +0200

    removed not used variables

--- Modules/ITS/include/ITS/ITSClusterTask.h
+++ Modules/ITS/include/ITS/ITSClusterTask.h
@@ -71,7 +71,6 @@ class ITSClusterTask : public TaskInterface
   const int mOccUpdateFrequency = 100000;
   int mNThreads = 1;
   int mNRofs = 0;
-  int ChipIDprev = 0;
   std::string mDictPath;
   std::string mRunNumberPath;
   std::string mGeomPath;
