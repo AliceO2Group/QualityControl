fb56679b1e406cf8a9f31578a8f2df14364e9d86
Barthelemy <barthelemy.von.haller@cern.ch>
2021-06-03 16:18:29 +0200

    QC-594
    Add an example of a custom class with double inheritance that confuses JSROOT. There is a member mTreatMeAs which holds the type under which jsroot should be able to handle it.
    
    The object is stored under `qc/TST/MO/QcTask/customTH2F`

---
+++ Modules/Example/src/CustomTH2F.cxx
@@ -0,0 +1,26 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file   CustomTH2F.cxx
+/// \author Barthelemy von Haller
+///
+
+#include "Example/CustomTH2F.h"
+#include "QualityControl/QcInfoLogger.h"
+
+namespace o2::quality_control_modules::example
+{
+
+CustomTH2F::CustomTH2F(std::string name): TH2F(name.c_str(), "Custom object inheriting from TH2F", 100, 0, 99, 100, 0, 99)
+{
+}
+
+} // namespace o2::quality_control_modules::example
