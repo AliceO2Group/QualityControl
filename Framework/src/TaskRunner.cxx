efcd21ebc1d806b077215b53d3486641210a389a
Chiara Zampolli <chiara.zampolli@cern.ch>
2021-06-09 08:07:02 +0200

    Vertexing QC task (#721)
    * Vertexing QC task
    
    Version with MC
    
    Additional histograms
    
    Forgotten selection on source
    
    Change log to Debug level
    
    Residuals and pulls
    
    clang-format
    
    Removing some commented lines
    
    clang-format
    
    * trigger rebuild
    
    Co-authored-by: Barthélémy von Haller <barthelemy.von.haller@gmail.com>

--- Framework/src/TaskRunner.cxx
+++ Framework/src/TaskRunner.cxx
@@ -376,7 +376,7 @@ std::string TaskRunner::validateDetectorName(std::string name) const
   int nDetectors = 16;
   const char* detNames[16] = // once we can use DetID, remove this hard-coded list
     { "ITS", "TPC", "TRD", "TOF", "PHS", "CPV", "EMC", "HMP", "MFT", "MCH", "MID", "ZDC", "FT0", "FV0", "FDD", "ACO" };
-  vector<string> permitted = { "MISC", "DAQ", "GENERAL", "TST", "BMK", "CTP", "TRG", "DCS" };
+  vector<string> permitted = { "MISC", "DAQ", "GENERAL", "TST", "BMK", "CTP", "TRG", "DCS", "REC" };
   for (auto i = 0; i < nDetectors; i++) {
     permitted.push_back(detNames[i]);
     //    permitted.push_back(o2::detectors::DetID::getName(i));
