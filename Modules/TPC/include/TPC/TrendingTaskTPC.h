eeafd6b196978e9a5c2edce34bea8604500e66aa
Cindy <cindy.mordasini@cern.ch>
2021-07-07 15:16:35 +0200

    Ensure clang format of the new coments

--- Modules/TPC/include/TPC/TrendingTaskTPC.h
+++ Modules/TPC/include/TPC/TrendingTaskTPC.h
@@ -35,7 +35,7 @@ class DatabaseInterface;
 namespace o2::quality_control_modules::tpc
 {
 
-/// \brief  A post-processing task tuned for the needs of the trending of the TPC, which trends values, stores them in a TTree and produces plots. 
+/// \brief  A post-processing task tuned for the needs of the trending of the TPC, which trends values, stores them in a TTree and produces plots.
 ///
 /// A post-processing task which trends objects inside QC database (QCDB). It extracts some values of one or multiple objects using the Reductor classes, then stores them inside a TTree.
 /// One can generate plots out the TTree - the class exposes the TTree::Draw interface to the user. The TTree and plots are stored in the QCDB. The class is configured with configuration files, see Framework/postprocessing.json as an example.
