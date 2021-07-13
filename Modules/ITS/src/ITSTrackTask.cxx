76ac3ec8a9f275c594e48351b9514bde88c90f05
Isakov Artem <rhawww@gmail.com>
2021-07-13 17:30:51 +0200

    Fixed issues with the geometry file for Cluster and Track tasks (#764)
    * OpenMP realisation for ITS Cluster Task
    
    * ITS Cluster Task: moved nThreads as parameter to .json
    
    * CLang format fixes
    
    * removed not used variables
    
    * Fixed issues with path to Geometry file for Track and Cluster Tasks
    
    Co-authored-by: IsakovAD <jian.liu@cern.ch>

--- Modules/ITS/src/ITSTrackTask.cxx
+++ Modules/ITS/src/ITSTrackTask.cxx
@@ -30,9 +30,6 @@ namespace o2::quality_control_modules::its
 ITSTrackTask::ITSTrackTask() : TaskInterface()
 {
   createAllHistos();
-
-  o2::base::GeometryManager::loadGeometry();
-  mGeom = o2::its::GeometryTGeo::Instance();
 }
 
 ITSTrackTask::~ITSTrackTask()
@@ -44,14 +41,15 @@ ITSTrackTask::~ITSTrackTask()
   delete hOccupancyROF;
   delete hClusterUsage;
   delete hAngularDistribution;
-
-  delete mGeom;
 }
 
 void ITSTrackTask::initialize(o2::framework::InitContext& /*ctx*/)
 {
 
   QcInfoLogger::GetInstance() << "initialize ITSTrackTask" << AliceO2::InfoLogger::InfoLogger::endm;
+
+  mRunNumberPath = mCustomParameters["runNumberPath"];
+
   publishHistos();
   std::string dictfile = o2::base::NameConf::getAlpideClusterDictionaryFileName(o2::detectors::DetID::ITS, "", ".bin");
   std::ifstream file(dictfile.c_str());
@@ -116,7 +114,7 @@ void ITSTrackTask::monitorData(o2::framework::ProcessingContext& ctx)
 void ITSTrackTask::endOfCycle()
 {
 
-  std::ifstream runNumberFile("/home/its/QC/workdir/infiles/RunNumber.dat"); //catching ITS run number in commissioning
+  std::ifstream runNumberFile(mRunNumberPath.c_str());
   if (runNumberFile) {
 
     std::string runNumber;
