d5d5c399de7298ffb44cc7085e2819be3554c40a
its <its@cern.ch>
2021-08-12 08:48:09 +0200

    ITS: FHR Task add a new Plot to monitor the Noisy pixel number in one stave

--- Modules/ITS/src/ITSFeeTask.cxx
+++ Modules/ITS/src/ITSFeeTask.cxx
@@ -161,6 +161,7 @@ void ITSFeeTask::monitorData(o2::framework::ProcessingContext& ctx)
   start = std::chrono::high_resolution_clock::now();
 
   std::vector<InputSpec> rawDataFilter{ InputSpec{ "", ConcreteDataTypeMatcher{ "DS", "feedata0" }, Lifetime::Timeframe } };
+  rawDataFilter.push_back(InputSpec{ "", ConcreteDataTypeMatcher{ "ITS", "RAWDATA" }, Lifetime::Timeframe });
   DPLRawParser parser(ctx.inputs(), rawDataFilter);
   for (auto it = parser.begin(), end = parser.end(); it != end; ++it) {
     auto const* rdh = it.get_if<o2::header::RAWDataHeaderV6>();
