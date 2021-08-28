98f013a605b723b8943e90a3d71229deff15350b
afurs <afurs@cern.ch>
2021-08-28 18:56:53 +0200

    FIT: FV0 and FDD modules added

---
+++ Modules/FDD/test/testQcFDD.cxx
@@ -0,0 +1,30 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file   testFDD.cxx
+/// \author My Name
+///
+
+#include "QualityControl/TaskFactory.h"
+
+#define BOOST_TEST_MODULE Publisher test
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+
+namespace o2::quality_control_modules::fdd
+{
+
+BOOST_AUTO_TEST_CASE(instantiate_task) { BOOST_CHECK(true); }
+
+} // namespace o2::quality_control_modules::fdd
