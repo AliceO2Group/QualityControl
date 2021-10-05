cacf4302125d42e2550235fe612d17d9e0631c9c
Chiara Zampolli <Chiara.Zampolli@cern.ch>
2021-10-05 16:06:22 +0200

    ITSTPC tracking QC
    some fixes
    
    Improvements
    
    clang-format

--- Modules/REC/include/REC/LinkDef.h
+++ Modules/REC/include/REC/LinkDef.h
@@ -4,4 +4,5 @@
 #pragma link off all functions;
 
 #pragma link C++ class o2::quality_control_modules::rec::VertexingQcTask + ;
+#pragma link C++ class o2::quality_control_modules::rec::ITSTPCMatchingTask + ;
 #endif
