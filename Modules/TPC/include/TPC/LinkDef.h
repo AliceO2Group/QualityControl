1bbcf790c7032a7825d7274d8600eb80824a17e8
lauraser <lauraserksnyte@gmail.com>
2021-10-05 11:32:41 +0200

    The check for trends, comparing new point to average

--- Modules/TPC/include/TPC/LinkDef.h
+++ Modules/TPC/include/TPC/LinkDef.h
@@ -4,7 +4,7 @@
 #pragma link off all functions;
 
 #pragma link C++ class o2::quality_control_modules::tpc::PID+;
-#pragma link C++ class o2::quality_control_modules::tpc::Tracking + ;
+#pragma link C++ class o2::quality_control_modules::tpc::Tracking+;
 #pragma link C++ class o2::quality_control_modules::tpc::Tracks+;
 #pragma link C++ class o2::quality_control_modules::tpc::PIDClusterCheck+;
 #pragma link C++ class o2::quality_control_modules::tpc::TrackClusterCheck+;
@@ -12,6 +12,7 @@
 #pragma link C++ class o2::quality_control_modules::tpc::Clusters+;
 #pragma link C++ class o2::quality_control_modules::tpc::CalDetPublisher+;
 #pragma link C++ class o2::quality_control_modules::tpc::RawDigits+;
+#pragma link C++ class o2::quality_control_modules::tpc::CheckOfTrendings+;
 
 #pragma link C++ function o2::quality_control_modules::tpc::addAndPublish+;
 #pragma link C++ function o2::quality_control_modules::tpc::toVector+;
