65943670d1a33916da958965bf53f6c1be468882
Chiara Zampolli <chiara.zampolli@cern.ch>
2021-10-12 18:54:16 +0200

    Match ITSTPC QC  (#891)
    * ITSTPC tracking QC
    
    some fixes
    
    Improvements
    
    clang-format
    
    ITSTPC tracking QC
    
    some fixes
    
    Improvements
    
    clang-format
    
    * Pass GRP and geom file names
    
    * Adding json to store objects in QCG from DPL
    
    And using a dataSamplingPolicy for QC standard processing.
    
    * Moving to GLO module
    
    clang-format

--- Modules/GLO/include/GLO/LinkDef.h
+++ Modules/GLO/include/GLO/LinkDef.h
@@ -4,4 +4,6 @@
 #pragma link off all functions;
 
 #pragma link C++ class o2::quality_control_modules::glo::VertexingQcTask + ;
+#pragma link C++ class o2::quality_control_modules::glo::ITSTPCMatchingTask + ;
+
 #endif
