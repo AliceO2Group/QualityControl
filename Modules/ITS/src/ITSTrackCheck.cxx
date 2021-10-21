dd61afb7aa242b5a203bf5eaea569a9aaa9ec7a5
JianLIUhep <jian.liu@cern.ch>
2021-10-22 00:48:31 +0200

    ClusterTask: solved bugs, new histo, added checks; TrackTask: added checks

--- Modules/ITS/src/ITSTrackCheck.cxx
+++ Modules/ITS/src/ITSTrackCheck.cxx
@@ -1,9 +1,8 @@
-// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
-// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
-// All rights not expressly granted are reserved.
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
 //
-// This software is distributed under the terms of the GNU General Public
-// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+// See http://alice-o2.web.cern.ch/license for full licensing information.
 //
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
@@ -21,7 +20,11 @@
 
 #include <TList.h>
 #include <TH1.h>
+#include <TH2.h>
 #include <TText.h>
+#include "TMath.h"
+
+#include <iostream>
 
 namespace o2::quality_control_modules::its
 {
@@ -33,14 +36,52 @@ Quality ITSTrackCheck::check(std::map<std::string, std::shared_ptr<MonitorObject
   Quality result = Quality::Null;
   std::map<std::string, std::shared_ptr<MonitorObject>>::iterator iter;
   for (iter = moMap->begin(); iter != moMap->end(); ++iter) {
+
     if (iter->second->getName() == "NClusters") {
       auto* h = dynamic_cast<TH1D*>(iter->second->getObject());
-      if (h->GetMean() > 8) {
-        result = Quality::Bad;
-      } else {
-        result = Quality::Good;
-      }
+      if (h->GetMean() <= 10) result = Quality::Good;
+      else if (h->GetMean() > 10 && h->GetMean() < 15) result = Quality::Medium;
+           else if (h->GetMean() >= 15) result = Quality::Bad;
+      
     }
+
+
+    if (iter->second->getName() == "PhiDistribution") {
+      auto* h = dynamic_cast<TH1D*>(iter->second->getObject());
+      Double_t ratio = abs ( h->Integral(h->FindBin(0),h->FindBin(TMath::Pi()))  / h->Integral(h->FindBin(TMath::Pi()),h->FindBin(TMath::TwoPi())) -1 );   
+
+
+      if (ratio > 0.3) result = Quality::Bad;
+      else result = Quality::Good;          
+    }
+
+    if (iter->second->getName() == "AngularDistribution") {
+      auto* hAngluar = dynamic_cast<TH2D*>(iter->second->getObject());
+      TH1D* projectPhi = hAngluar->ProjectionY("hAngluar", hAngluar->FindBin(-1.5), hAngluar->FindBin(1.5));
+      Double_t ratio = abs ( projectPhi->Integral(projectPhi->FindBin(0),projectPhi->FindBin(TMath::Pi()))  / projectPhi->Integral(projectPhi->FindBin(TMath::Pi()),projectPhi->FindBin(TMath::TwoPi())) -1);   
+
+      if (ratio > 0.3) result = Quality::Bad;
+      else result = Quality::Good;
+    }
+
+    if (iter->second->getName() == "ClusterUsage") {
+      auto* h = dynamic_cast<TH1D*>(iter->second->getObject());
+      if (h->GetMaximum() < 0.1) result = Quality::Bad;
+      else result = Quality::Good;
+     
+    }
+
+    if (iter->second->getName() == "EtaDistribution") {
+      auto* h = dynamic_cast<TH1D*>(iter->second->getObject());
+      if ( abs( h->GetBinCenter( h->FindBin(h->GetMaximum()))) > 0.5)  result = Quality::Bad;
+      else result = Quality::Good;
+
+    }
+
+
+
+
+
   }
   return result;
 }
@@ -49,19 +90,89 @@ std::string ITSTrackCheck::getAcceptedType() { return "TH1D"; }
 
 void ITSTrackCheck::beautify(std::shared_ptr<MonitorObject> mo, Quality checkResult)
 {
-  auto* h = dynamic_cast<TH1D*>(mo->getObject());
-  auto* tInfo = new TText();
+ auto* tInfo = new TText();
 
-  if (checkResult == Quality::Good) {
-    tInfo->SetText(0.1, 0.8, "Quality::GOOD");
-    tInfo->SetTextColor(kGreen);
-  } else if (checkResult == Quality::Bad) {
-    tInfo->SetText(0.1, 0.8, "Quality::BAD");
-    tInfo->SetTextColor(kRed);
-  }
-  tInfo->SetTextSize(17);
-  tInfo->SetNDC();
-  h->GetListOfFunctions()->Add(tInfo);
+
+  if (mo->getName() == "NClusters") {
+     auto*  h = dynamic_cast<TH1D*>(mo->getObject());
+     if (checkResult == Quality::Good) {
+        tInfo->SetText(0.1, 0.8, "Quality::GOOD");
+        tInfo->SetTextColor(kGreen);
+     } else if (checkResult == Quality::Medium) {
+        tInfo->SetText(0.1, 0.8, "Info: a track(s) has between 10 and 15 clusters, inform expert on MM");
+        tInfo->SetTextColor(kOrange);
+       } else if (checkResult == Quality::Bad) {
+        tInfo->SetText(0.1, 0.8, "Info: a track(s) has more than 15 clusters, call expert");
+        tInfo->SetTextColor(kRed);
+       }
+     tInfo->SetTextSize(17);
+     tInfo->SetNDC();
+     h->GetListOfFunctions()->Add(tInfo);
+
+ 
+    }
+   
+   if (mo->getName() == "PhiDistribution") {
+   auto*  h = dynamic_cast<TH1D*>(mo->getObject());
+     if (checkResult == Quality::Good) {
+        tInfo->SetText(0.1, 0.8, "Quality::GOOD");
+        tInfo->SetTextColor(kGreen);
+     } else if (checkResult == Quality::Bad) {
+        tInfo->SetText(0.1, 0.8, "Info: distribution asymmetric in phi, call expert");
+        tInfo->SetTextColor(kRed);
+       }
+      tInfo->SetTextSize(17);
+      tInfo->SetNDC();
+      h->GetListOfFunctions()->Add(tInfo);
+    }
+
+   if (mo->getName() == "AngularDistribution") {
+   auto*  h = dynamic_cast<TH2D*>(mo->getObject());
+     if (checkResult == Quality::Good) {
+        tInfo->SetText(0.1, 0.8, "Quality::GOOD");
+        tInfo->SetTextColor(kGreen);
+     } else if (checkResult == Quality::Bad) {
+        tInfo->SetText(0.1, 0.8, "Info: distribution asymmetric in phi, call expert");
+        tInfo->SetTextColor(kRed);
+       }
+     tInfo->SetTextSize(17);
+     tInfo->SetNDC();
+     h->GetListOfFunctions()->Add(tInfo);
+    
+
+    }
+
+
+
+   if (mo->getName() == "ClusterUsage") {
+    auto* h = dynamic_cast<TH1D*>(mo->getObject());
+     if (checkResult == Quality::Good) {
+        tInfo->SetText(0.1, 0.8, "Quality::GOOD");
+        tInfo->SetTextColor(kGreen);
+     } else if (checkResult == Quality::Bad) {
+        tInfo->SetText(0.1, 0.8, "Info: fraction of clusters below 0.1, call expert");
+        tInfo->SetTextColor(kRed);
+       }
+     tInfo->SetTextSize(17);
+     tInfo->SetNDC();
+     h->GetListOfFunctions()->Add(tInfo);
+
+    }
+  if (mo->getName() == "EtaDistribution") {
+   auto* h = dynamic_cast<TH1D*>(mo->getObject());
+    if (checkResult == Quality::Good) {
+        tInfo->SetText(0.1, 0.8, "Quality::GOOD");
+        tInfo->SetTextColor(kGreen);
+     } else if (checkResult == Quality::Bad) {
+        tInfo->SetText(0.1, 0.8, "Info: distribution asymmetric in eta, call expert");
+        tInfo->SetTextColor(kRed);
+       }
+      tInfo->SetTextSize(17);
+      tInfo->SetNDC();
+      h->GetListOfFunctions()->Add(tInfo);
+
+    }
+
 }
 
 } // namespace o2::quality_control_modules::its
