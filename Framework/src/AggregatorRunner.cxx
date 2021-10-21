bd7ae210766c66b5a778242f2ab9cba9a43faa53
Barthélémy von Haller <barthelemy.von.haller@cern.ch>
2021-10-21 16:02:49 +0200

    [QC-682] override stop() but do not register a callback (#922)
    * [QC-682] override stop() but do not register a callback
    
    * Put back the callback

--- Framework/src/AggregatorRunner.cxx
+++ Framework/src/AggregatorRunner.cxx
@@ -113,8 +113,8 @@ void AggregatorRunner::init(framework::InitContext& iCtx)
   try {
     // registering state machine callbacks
     iCtx.services().get<CallbackService>().set(CallbackService::Id::Start, [this, &services = iCtx.services()]() { start(services); });
-    iCtx.services().get<CallbackService>().set(CallbackService::Id::Stop, [this]() { stop(); });
     iCtx.services().get<CallbackService>().set(CallbackService::Id::Reset, [this]() { reset(); });
+    iCtx.services().get<CallbackService>().set(CallbackService::Id::Stop, [this]() { stop(); });
   } catch (o2::framework::RuntimeErrorRef& ref) {
     ILOG(Error) << "Error during initialization: " << o2::framework::error_from_ref(ref).what << ENDM;
   }
