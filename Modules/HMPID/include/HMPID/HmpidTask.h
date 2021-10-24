2d4bf4e20de1a6e1c75d96b6ea28524853492e64
Giacomo Volpe <gvolpe@pcgvolpe.ba.infn.it>
2021-10-25 00:27:44 +0200

    Updating fork

--- Modules/HMPID/include/HMPID/HmpidTask.h
+++ Modules/HMPID/include/HMPID/HmpidTask.h
@@ -1,8 +1,9 @@
-// Copyright CERN and copyright holders of ALICE O2. This software is
-// distributed under the terms of the GNU General Public License v3 (GPL
-// Version 3), copied verbatim in the file "COPYING".
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
 //
-// See http://alice-o2.web.cern.ch/license for full licensing information.
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
 //
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
@@ -20,6 +21,7 @@
 #include "HMPIDReconstruction/HmpidDecoder2.h"
 
 class TH1F;
+class TProfile;
 
 using namespace o2::quality_control::core;
 
@@ -48,8 +50,8 @@ class HmpidTask final : public TaskInterface
  private:
   TH1F* hPedestalMean = nullptr;
   TH1F* hPedestalSigma = nullptr;
-  TH1F* hBusyTime = nullptr;
-  TH1F* hEventSize = nullptr;
+  TProfile* hBusyTime = nullptr;
+  TProfile* hEventSize = nullptr;
   o2::hmpid::HmpidDecoder2* mDecoder = nullptr;
 };
 
--- Modules/HMPID/src/HmpidTask.cxx
+++ Modules/HMPID/src/HmpidTask.cxx
@@ -1,8 +1,9 @@
-// Copyright CERN and copyright holders of ALICE O2. This software is
-// distributed under the terms of the GNU General Public License v3 (GPL
-// Version 3), copied verbatim in the file "COPYING".
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
 //
-// See http://alice-o2.web.cern.ch/license for full licensing information.
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
 //
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
@@ -15,8 +16,10 @@
 
 #include <TCanvas.h>
 #include <TH1.h>
+#include <TProfile.h>
 #include <TMath.h>
 #include <Framework/InputRecord.h>
+#include <Framework/InputRecordWalker.h>
 
 #include "QualityControl/QcInfoLogger.h"
 //#include "HMPID/HmpidDecodeRawMem.h"
@@ -50,11 +53,11 @@ Int_t NumCycles = 0;
 
 void HmpidTask::initialize(o2::framework::InitContext& /*ctx*/)
 {
-  ILOG(Info) << "initialize HmpidTask" << ENDM; // QcInfoLogger is used. FairMQ logs will go to there as well.
+  ILOG(Info, Support) << "initialize HmpidTask" << ENDM; // QcInfoLogger is used. FairMQ logs will go to there as well.
 
   // this is how to get access to custom parameters defined in the config file at qc.tasks.<task_name>.taskParameters
   if (auto param = mCustomParameters.find("myOwnKey"); param != mCustomParameters.end()) {
-    ILOG(Info) << "Custom parameter - myOwnKey: " << param->second << ENDM;
+    ILOG(Info, Support) << "Custom parameter - myOwnKey: " << param->second << ENDM;
   }
 
   hPedestalMean = new TH1F("hPedestalMean", "Pedestal Mean", 2000, 0, 2000);
@@ -65,16 +68,45 @@ void HmpidTask::initialize(o2::framework::InitContext& /*ctx*/)
   hPedestalSigma->SetXTitle("Pedestal sigma (ADC channel)");
   hPedestalMean->SetYTitle("Entries/0.1 ADC");
 
-  hBusyTime = new TH1F("hBusyTime", "Average Busy Time", 14, 0, 14);
-  hBusyTime->SetXTitle("Equipment");
-  hBusyTime->SetYTitle("Busy time (#mus)");
+  // TProfiles
+  hBusyTime = new TProfile("hBusyTime", "HMP Busy Time per DDL;;Busy Time (#mus)", 14, 0.5, 14.5);
+  hBusyTime->Sumw2();
+  hBusyTime->SetOption("P");
+  hBusyTime->SetMinimum(0);
   hBusyTime->SetMarkerStyle(20);
+  hBusyTime->SetMarkerColor(kBlack);
+  hBusyTime->SetLineColor(kBlack);
+  for (Int_t iddl = 0; iddl < 14; iddl++)
+    hBusyTime->GetXaxis()->SetBinLabel(iddl + 1, Form("%d", iddl + 1));
+  hBusyTime->GetXaxis()->SetLabelSize(0.02);
+  hBusyTime->SetStats(0);
 
-  hEventSize = new TH1F("hEventSize", "Average Event Size", 14, 0, 14);
-  hEventSize->SetXTitle("Equipment");
-  hEventSize->SetYTitle("Event size (kB)");
+  hEventSize = new TProfile("hEventSize", "HMP Event Size per DDL;;Event Size (kB)", 14, 0.5, 14.5);
+  hEventSize->Sumw2();
+  hEventSize->SetOption("P");
+  hEventSize->SetMinimum(0);
   hEventSize->SetMarkerStyle(20);
+  hEventSize->SetMarkerColor(kBlack);
+  hEventSize->SetLineColor(kBlack);
+  for (Int_t iddl = 0; iddl < 14; iddl++)
+    hEventSize->GetXaxis()->SetBinLabel(iddl + 1, Form("%d", iddl + 1));
+  hEventSize->GetXaxis()->SetLabelSize(0.02);
+  hEventSize->SetStats(0);
 
+  /*hBusyTime = new TGraph(14);
+  hBusyTime->SetName("hBusyTime");
+  hBusyTime->SetMarkerStyle(20);
+  hBusyTime->SetLineWidth(0);
+  hBusyTime->GetXaxis()->SetTitle("Equipment");
+  hBusyTime->GetYaxis()->SetTitle("Busy time (#mus)");
+
+  hEventSize = new TGraph(14);
+  hEventSize->SetName("hEventSize");
+  hEventSize->SetMarkerStyle(20);
+  hEventSize->SetLineWidth(0);
+  hEventSize->GetXaxis()->SetTitle("Equipment");
+  hEventSize->GetYaxis()->SetTitle("Event size (kB)");
+*/
   getObjectsManager()->startPublishing(hPedestalMean);
   getObjectsManager()->addMetadata(hPedestalMean->GetName(), "custom", "34");
 
@@ -90,11 +122,9 @@ void HmpidTask::initialize(o2::framework::InitContext& /*ctx*/)
 
 void HmpidTask::startOfActivity(Activity& /*activity*/)
 {
-  ILOG(Info) << "startOfActivity" << ENDM;
+  ILOG(Info, Support) << "startOfActivity" << ENDM;
   hPedestalMean->Reset();
   hPedestalSigma->Reset();
-  hBusyTime->Reset();
-  hEventSize->Reset();
 
   mDecoder = new o2::hmpid::HmpidDecoder2(14);
   mDecoder->init();
@@ -103,7 +133,7 @@ void HmpidTask::startOfActivity(Activity& /*activity*/)
 
 void HmpidTask::startOfCycle()
 {
-  ILOG(Info) << "startOfCycle" << ENDM;
+  ILOG(Info, Support) << "startOfCycle" << ENDM;
 }
 
 void HmpidTask::monitorData(o2::framework::ProcessingContext& ctx)
@@ -112,7 +142,8 @@ void HmpidTask::monitorData(o2::framework::ProcessingContext& ctx)
   mDecoder->init();
   mDecoder->setVerbosity(2); // this is for Debug
 
-  for (auto&& input : ctx.inputs()) {
+  //for (auto&& input : ctx.inputs()) {
+  for (auto&& input : o2::framework::InputRecordWalker(ctx.inputs())) {
     // get message header
     if (input.header != nullptr && input.payload != nullptr) {
       const auto* header = header::get<header::DataHeader*>(input.header);
@@ -123,18 +154,18 @@ void HmpidTask::monitorData(o2::framework::ProcessingContext& ctx)
       }
       mDecoder->setUpStream(ptrToPayload, (long int)header->payloadSize);
       if (!mDecoder->decodeBufferFast()) {
-        ILOG(Error) << "Error decoding the Superpage !" << ENDM;
+        ILOG(Error, Devel) << "Error decoding the Superpage !" << ENDM;
       }
+
+      // Double_t ddl[14], EventSize[14], BusyTime[14];
+
       for (Int_t eq = 0; eq < 14; eq++) {
         if (mDecoder->getAverageEventSize(eq) > 0.) {
-          hEventSize->SetBinContent(eq + 1, mDecoder->getAverageEventSize(eq) / 1000.);
-          hEventSize->SetBinError(eq + 1, 0.0000001);
+          hEventSize->Fill(eq + 1, mDecoder->getAverageEventSize(eq) / 1000.);
         }
         if (mDecoder->getAverageBusyTime(eq) > 0.) {
-          hBusyTime->SetBinContent(eq + 1, mDecoder->getAverageBusyTime(eq) * 1000000);
-          hBusyTime->SetBinError(eq + 1, 0.00000001);
+          hBusyTime->Fill(eq + 1, mDecoder->getAverageBusyTime(eq) * 1000000);
         }
-        Printf("eq = %i, size = %f, busy = %f", eq, mDecoder->getAverageEventSize(eq), mDecoder->getAverageBusyTime(eq));
         for (Int_t column = 0; column < 24; column++) {
           for (Int_t dilogic = 0; dilogic < 10; dilogic++) {
             for (Int_t channel = 0; channel < 48; channel++) {
@@ -170,23 +201,23 @@ void HmpidTask::monitorData(o2::framework::ProcessingContext& ctx)
 
 void HmpidTask::endOfCycle()
 {
-  ILOG(Info) << "endOfCycle" << ENDM;
+  ILOG(Info, Support) << "endOfCycle" << ENDM;
 }
 
 void HmpidTask::endOfActivity(Activity& /*activity*/)
 {
-  ILOG(Info) << "endOfActivity" << ENDM;
+  ILOG(Info, Support) << "endOfActivity" << ENDM;
 }
 
 void HmpidTask::reset()
 {
   // clean all the monitor objects here
 
-  ILOG(Info) << "Resetting the histogram" << ENDM;
+  ILOG(Info, Support) << "Resetting the histogram" << ENDM;
   hPedestalMean->Reset();
   hPedestalSigma->Reset();
-  hBusyTime->Reset();
-  hEventSize->Reset();
+  hBusyTime->Set(0);
+  hEventSize->Set(0);
 }
 
 } // namespace o2::quality_control_modules::hmpid
