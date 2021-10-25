bc8cd0759092c1646ec10bfb85e49d0073d0ba4b
zla2019 <52201070+zla2019@users.noreply.github.com>
2021-10-25 14:50:57 +0800

    ITS: New FHR, FEE Task check feature (#925)
    * ITS:new check feature for FHR and FEE Task
    
    * ITS:update FHR, FEE json file
    
    * update message and Noisy cut
    
    Co-authored-by: its <its@cern.ch>

--- Modules/ITS/src/ITSFhrCheck.cxx
+++ Modules/ITS/src/ITSFhrCheck.cxx
@@ -33,24 +33,65 @@ void ITSFhrCheck::configure(std::string) {}
 
 Quality ITSFhrCheck::check(std::map<std::string, std::shared_ptr<MonitorObject>>* moMap)
 {
-  Quality result = Quality::Null;
-  std::map<std::string, std::shared_ptr<MonitorObject>>::iterator iter;
-  for (iter = moMap->begin(); iter != moMap->end(); ++iter) {
-    if (iter->second->getName() == "General/ErrorPlots") {
-      auto* h = dynamic_cast<TH1D*>(iter->second->getObject());
-      if (h->GetMaximum() > 0) {
-        result = Quality::Bad;
-      } else {
-        result = Quality::Good;
+  Quality result = 0;                                                    //The FHR Checker will check three plots:
+  std::map<std::string, std::shared_ptr<MonitorObject>>::iterator iter;  //General/ErrorPlots, General/General_Occupancy and
+  for (iter = moMap->begin(); iter != moMap->end(); ++iter) {            //General/Noisy_Pixel. and store the result in a
+    if (iter->second->getName() == "General/ErrorPlots") {               //check the General/ErrorPlots		//three digits: XXX
+      auto* h = dynamic_cast<TH1D*>(iter->second->getObject());          //		                                        |||-> ErrorPlots
+      if (h->GetMaximum() > 0) {                                         //		                                        ||-> General_Occupancy
+        result = result.getLevel() + 1;                                  //		                                        |-> Noisy_Pixel
+      }                                                                  //the number for each digits is correspond:
+    } else if (iter->second->getName() == "General/General_Occupancy") { //0: Good, 1: Bad, 2: Medium(if exist)
+      auto* h = dynamic_cast<TH2Poly*>(iter->second->getObject());       //So if the quality level is 210, that means
+      for (int ibin = 0; ibin < h->GetNumberOfBins(); ++ibin) {          //Medium Noisy_Pixel, Bad General_Occupancy and
+        if (h->GetBinContent(ibin) == 0) {                               //Good ErrorPlots
+          continue;
+        }
+        if (ibin < 48) {
+          if (h->GetBinContent(ibin) > pow(10, -5)) {
+            result = (result.getLevel() % 10) + 10;
+          } else if (h->GetBinContent(ibin) > pow(10, -6) && (result.getLevel() / 10 < 1)) {
+            result = (result.getLevel() % 10) + 20;
+          }
+        } else if (ibin < 102) {
+          if (h->GetBinContent(ibin) > pow(10, -5)) {
+            result = (result.getLevel() % 10) + 10;
+          } else if (h->GetBinContent(ibin) > pow(10, -6) && (result.getLevel() / 10 < 1)) {
+            result = (result.getLevel() % 10) + 20;
+          }
+        } else {
+          if (h->GetBinContent(ibin) > pow(10, -5)) {
+            result = (result.getLevel() % 10) + 10;
+          } else if (h->GetBinContent(ibin) > pow(10, -6) && (result.getLevel() / 10 < 1)) {
+            result = (result.getLevel() % 10) + 20;
+          }
+        }
       }
-    } else if (iter->second->getName() == "General/General_Occupancy") {
+    } else if (iter->second->getName() == "General/Noisy_Pixel") {
       auto* h = dynamic_cast<TH2Poly*>(iter->second->getObject());
-      result = Quality::Good;
-      if (h->GetMaximum() > pow(10, -6)) {
-        result = Quality::Medium;
-      }
-      if (h->GetMaximum() > pow(10, -5)) {
-        result = Quality::Bad;
+      for (int ibin = 0; ibin < h->GetNumberOfBins(); ++ibin) {
+        if (h->GetBinContent(ibin) == 0) {
+          continue;
+        }
+        if (ibin < 48) {
+          if ((h->GetBinContent(ibin) / (double)mNPixelPerStave[0]) > 0.0001) {
+            result = (result.getLevel() % 100) + 100;
+          } else if (h->GetBinContent(ibin) > 0.00005 && (result.getLevel() / 100 < 1)) {
+            result = (result.getLevel() % 100) + 200;
+          }
+        } else if (ibin < 102) {
+          if ((h->GetBinContent(ibin) / (double)mNPixelPerStave[1]) > 0.0001) {
+            result = (result.getLevel() % 100) + 100;
+          } else if ((h->GetBinContent(ibin) / (double)mNPixelPerStave[1]) > 0.00005 && (result.getLevel() / 100 < 1)) {
+            result = (result.getLevel() % 100) + 200;
+          }
+        } else {
+          if ((h->GetBinContent(ibin) / (double)mNPixelPerStave[2]) > 0.0001) {
+            result = (result.getLevel() % 100) + 100;
+          } else if ((h->GetBinContent(ibin) / (double)mNPixelPerStave[2]) > 0.00005 && (result.getLevel() / 100 < 1)) {
+            result = (result.getLevel() % 100) + 200;
+          }
+        }
       }
     }
   }
@@ -64,19 +105,19 @@ void ITSFhrCheck::beautify(std::shared_ptr<MonitorObject> mo, Quality checkResul
   TLatex* text[5];
   if (mo->getName() == "General/ErrorPlots") {
     auto* h = dynamic_cast<TH1D*>(mo->getObject());
-    if (checkResult == Quality::Good) {
-      text[0] = new TLatex(0, 0, "Quality::Good");
-      text[1] = new TLatex(0, -100, "There is no Error found");
+    if ((checkResult.getLevel() % 10) == 0) {
+      text[0] = new TLatex(10, 0.6, "Quality::Good");
+      text[1] = new TLatex(10, 0.4, "There is no Error found");
       for (int i = 0; i < 2; ++i) {
         text[i]->SetTextAlign(23);
         text[i]->SetTextSize(0.08);
         text[i]->SetTextColor(kGreen);
         h->GetListOfFunctions()->Add(text[i]);
       }
-    } else if (checkResult == Quality::Bad) {
-      text[0] = new TLatex(0, 100, "Quality::Bad");
-      text[1] = new TLatex(0, 0, "Decoding ERROR detected");
-      text[2] = new TLatex(0, -100, "please inform SL");
+    } else if ((checkResult.getLevel() % 10) == 1) {
+      text[0] = new TLatex(10, 0.7, "Quality::Bad");
+      text[1] = new TLatex(10, 0.5, "Decoding ERROR detected");
+      text[2] = new TLatex(10, 0.3, "Please Call Expert");
       for (int i = 0; i < 3; ++i) {
         text[i]->SetTextAlign(23);
         text[i]->SetTextSize(0.08);
@@ -86,18 +127,16 @@ void ITSFhrCheck::beautify(std::shared_ptr<MonitorObject> mo, Quality checkResul
     }
   } else if (mo->getName() == "General/General_Occupancy") {
     auto* h = dynamic_cast<TH2Poly*>(mo->getObject());
-    auto* msg = new TPaveText(0.5, 0.5, 0.9, 0.75, "NDC");
-    msg->SetName(Form("%s_msg", mo->GetName()));
-    if (checkResult == Quality::Good) {
+    if (((checkResult.getLevel() % 100) / 10) == 0) {
       text[0] = new TLatex(0, 0, "Quality::Good");
       text[0]->SetTextAlign(23);
       text[0]->SetTextSize(0.08);
       text[0]->SetTextColor(kGreen);
       h->GetListOfFunctions()->Add(text[0]);
-    } else if (checkResult == Quality::Bad) {
+    } else if (((checkResult.getLevel() % 100) / 10) == 1) {
       text[0] = new TLatex(0, 100, "Quality::Bad");
       text[1] = new TLatex(0, 0, "Max Occupancy over 10^{-5}");
-      text[2] = new TLatex(0, -100, "or ERROR detected, Please Inform SL");
+      text[2] = new TLatex(0, -100, "Please Call Expert");
       for (int i = 0; i < 3; ++i) {
         text[i]->SetTextAlign(23);
         text[i]->SetTextSize(0.08);
@@ -107,7 +146,37 @@ void ITSFhrCheck::beautify(std::shared_ptr<MonitorObject> mo, Quality checkResul
     } else if (checkResult == Quality::Medium) {
       text[0] = new TLatex(0, 0, "Quality::Medium");
       text[1] = new TLatex(0, -100, "Max Occupancy over 10^{-6}");
+      text[2] = new TLatex(0, -100, "Please Notify Expert On MM");
       for (int i = 0; i < 2; ++i) {
+        text[i]->SetTextAlign(23);
+        text[i]->SetTextSize(0.08);
+        text[i]->SetTextColor(kOrange);
+        h->GetListOfFunctions()->Add(text[i]);
+      }
+    }
+  } else if (mo->getName() == "General/Noisy_Pixel") {
+    auto* h = dynamic_cast<TH2Poly*>(mo->getObject());
+    if ((checkResult.getLevel() / 100) == 0) {
+      text[0] = new TLatex(0, 0, "Quality::Good");
+      text[0]->SetTextAlign(23);
+      text[0]->SetTextSize(0.08);
+      text[0]->SetTextColor(kGreen);
+      h->GetListOfFunctions()->Add(text[0]);
+    } else if ((checkResult.getLevel() / 100) == 1) {
+      text[0] = new TLatex(0, 100, "Quality::Bad");
+      text[1] = new TLatex(0, 0, "Noisy Pixel over 0.01%");
+      text[2] = new TLatex(0, -100, "Please Call Expert");
+      for (int i = 0; i < 3; ++i) {
+        text[i]->SetTextAlign(23);
+        text[i]->SetTextSize(0.08);
+        text[i]->SetTextColor(kRed);
+        h->GetListOfFunctions()->Add(text[i]);
+      }
+    } else if ((checkResult.getLevel() / 100) == 2) {
+      text[0] = new TLatex(0, 100, "Quality::Medium");
+      text[1] = new TLatex(0, 0, "Noisy Pixel over 0.005%");
+      text[2] = new TLatex(0, -100, "Please Notify Expert On MM");
+      for (int i = 0; i < 3; ++i) {
         text[i]->SetTextAlign(23);
         text[i]->SetTextSize(0.08);
         text[i]->SetTextColor(kOrange);
