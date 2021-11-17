f13ab56bdcc37a9ccf5409e942413ac334ac0ff1
Sean Murray <hamiltonthomas@gmail.com>
2021-11-17 12:52:00 +0100

    TRD: add noise removal

--- Modules/TRD/include/TRD/LinkDef.h
+++ Modules/TRD/include/TRD/LinkDef.h
@@ -8,7 +8,6 @@
 
 #pragma link C++ class o2::quality_control_modules::trd::RawData + ;
 #pragma link C++ class o2::quality_control_modules::trd::PulseHeight + ;
-#pragma link C++ class o2::quality_control_modules::trd::Noise + ;
 #pragma link C++ class o2::quality_control_modules::trd::PulseHeightCheck + ;
 #pragma link C++ class o2::quality_control_modules::trd::TrackletsTask + ;
 #pragma link C++ class o2::quality_control_modules::trd::TrackletsCheck + ;
