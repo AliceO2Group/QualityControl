7b9279f8479b11d3642d0d209e7de6533989c7ef
iravasen <ivan.ravasenga@cern.ch>
2022-01-13 19:06:00 +0100

    removed TH1Xline reductor also from LinkDef

--- Modules/ITS/include/ITS/LinkDef.h
+++ Modules/ITS/include/ITS/LinkDef.h
@@ -9,7 +9,6 @@
 #pragma link C++ class o2::quality_control::postprocessing::TrendingTaskITSCluster + ;
 #pragma link C++ class o2::quality_control::postprocessing::TrendingTaskITSTracks + ;
 #pragma link C++ class o2::quality_control_modules::its::TH2XlineReductor + ;
-#pragma link C++ class o2::quality_control_modules::its::TH1XlineReductor + ;
 #pragma link C++ class o2::quality_control_modules::its::ITSFhrTask + ;
 #pragma link C++ class o2::quality_control_modules::its::ITSFeeTask + ;
 #pragma link C++ class o2::quality_control_modules::its::ITSClusterTask + ;
