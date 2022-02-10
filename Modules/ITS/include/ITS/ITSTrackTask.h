7a0fdeef0f1e6ed0214806e93f155304afa6615e
delico18 <52651451+delico18@users.noreply.github.com>
2022-02-10 17:09:09 +0900

    New feature track task (#1038)
    * new plot added TrackTask BunchCrossingID vs ClusterSize
    
    * new plot(fixed) added TrackTask BunchCrossingID vs ClusterRatio

--- Modules/ITS/include/ITS/ITSTrackTask.h
+++ Modules/ITS/include/ITS/ITSTrackTask.h
@@ -68,6 +68,7 @@ class ITSTrackTask : public TaskInterface
   TH1D* hAssociatedClusterFraction;
   TH1D* hNtracks;
   TH2D* hNClustersPerTrackEta;
+  TH2D* hClusterVsBunchCrossing;
 
   std::string mRunNumber;
   std::string mRunNumberPath;
