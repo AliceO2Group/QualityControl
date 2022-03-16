9889156df92a0fe083c3e8ac59f30d6188818ee7
Tomas Herman <tomas.herman@cern.ch>
2022-03-16 23:46:41 +0100

    Fixing histogram names in digit checker.

--- Modules/MFT/src/QcMFTDigitCheck.cxx
+++ Modules/MFT/src/QcMFTDigitCheck.cxx
@@ -45,42 +45,42 @@ Quality QcMFTDigitCheck::check(std::map<std::string, std::shared_ptr<MonitorObje
     (void)moName;
 
     if (mo->getName() == "mDigitChipOccupancy") {
-      auto* histogram = dynamic_cast<TH1F*>(mo->getObject());
+      auto* hChipOccupancy = dynamic_cast<TH1F*>(mo->getObject());
 
-      float den = histogram->GetBinContent(0); // normalisation stored in the uderflow bin
+      float den = hChipOccupancy->GetBinContent(0); // normalisation stored in the uderflow bin
 
-      for (int iBin = 0; iBin < histogram->GetNbinsX(); iBin++) {
-        float num = histogram->GetBinContent(iBin + 1);
+      for (int iBin = 0; iBin < hChipOccupancy->GetNbinsX(); iBin++) {
+        float num = hChipOccupancy->GetBinContent(iBin + 1);
         float ratio = (den > 0) ? (num / den) : 0.0;
-        histogram->SetBinContent(iBin + 1, ratio);
+        hChipOccupancy->SetBinContent(iBin + 1, ratio);
       }
     }
 
     if (mo->getName() == "mDigitOccupancySummary") {
-      auto* histogram = dynamic_cast<TH2F*>(mo->getObject());
+      auto* hOccupancySummary = dynamic_cast<TH2F*>(mo->getObject());
 
-      float den = histogram->GetBinContent(0, 0); // normalisation stored in the uderflow bin
+      float den = hOccupancySummary->GetBinContent(0, 0); // normalisation stored in the uderflow bin
 
-      for (int iBinX = 0; iBinX < histogram->GetNbinsX(); iBinX++) {
-        for (int iBinY = 0; iBinY < histogram->GetNbinsY(); iBinY++) {
-          float num = histogram->GetBinContent(iBinX + 1, iBinY + 1);
+      for (int iBinX = 0; iBinX < hOccupancySummary->GetNbinsX(); iBinX++) {
+        for (int iBinY = 0; iBinY < hOccupancySummary->GetNbinsY(); iBinY++) {
+          float num = hOccupancySummary->GetBinContent(iBinX + 1, iBinY + 1);
           float ratio = (den > 0) ? (num / den) : 0.0;
-          histogram->SetBinContent(iBinX + 1, iBinY + 1, ratio);
+          hOccupancySummary->SetBinContent(iBinX + 1, iBinY + 1, ratio);
         }
       }
     }
 
     if (mo->getName().find("mDigitChipStdDev") != std::string::npos) {
-      auto* histogram = dynamic_cast<TH1F*>(mo->getObject());
+      auto* hStdDev = dynamic_cast<TH1F*>(mo->getObject());
 
       // test it
-      if (histogram->GetBinContent(histogram->GetMinimumBin()) > 250) {
+      if (hStdDev->GetBinContent(hStdDev->GetMinimumBin()) > 250) {
         // result = Quality::Good;
       }
-      if ((histogram->GetBinContent(histogram->GetMinimumBin()) < 250) && (histogram->GetBinContent(histogram->GetMinimumBin()) > 200)) {
+      if ((hStdDev->GetBinContent(hStdDev->GetMinimumBin()) < 250) && (hStdDev->GetBinContent(hStdDev->GetMinimumBin()) > 200)) {
         // result = Quality::Medium;
       }
-      if (histogram->GetBinContent(histogram->GetMinimumBin()) < 200) {
+      if (hStdDev->GetBinContent(hStdDev->GetMinimumBin()) < 200) {
         // result = Quality::Bad;
       }
     }
@@ -93,22 +93,22 @@ std::string QcMFTDigitCheck::getAcceptedType() { return "TH1"; }
 void QcMFTDigitCheck::beautify(std::shared_ptr<MonitorObject> mo, Quality checkResult)
 {
   if (mo->getName().find("mDigitChipStdDev") != std::string::npos) {
-    auto* histogram = dynamic_cast<TH1F*>(mo->getObject());
+    auto* hStdDev = dynamic_cast<TH1F*>(mo->getObject());
 
     if (checkResult == Quality::Good) {
       LOG(info) << "Quality::Good";
-      TLatex* tl = new TLatex(350, 1.05 * histogram->GetMaximum(), "#color[418]{Dummy check status: Good!}");
-      histogram->GetListOfFunctions()->Add(tl);
+      TLatex* tl = new TLatex(350, 1.05 * hStdDev->GetMaximum(), "#color[418]{Dummy check status: Good!}");
+      hStdDev->GetListOfFunctions()->Add(tl);
       tl->Draw();
     } else if (checkResult == Quality::Bad) {
       LOG(info) << "Quality::Bad";
-      TLatex* tl = new TLatex(350, 1.05 * histogram->GetMaximum(), "#color[633]{Dummy check status: Bad!}");
-      histogram->GetListOfFunctions()->Add(tl);
+      TLatex* tl = new TLatex(350, 1.05 * hStdDev->GetMaximum(), "#color[633]{Dummy check status: Bad!}");
+      hStdDev->GetListOfFunctions()->Add(tl);
       tl->Draw();
     } else if (checkResult == Quality::Medium) {
       LOG(info) << "Quality::Medium";
-      TLatex* tl = new TLatex(350, 1.05 * histogram->GetMaximum(), "#color[800]{Dummy check status: Medium!}");
-      histogram->GetListOfFunctions()->Add(tl);
+      TLatex* tl = new TLatex(350, 1.05 * hStdDev->GetMaximum(), "#color[800]{Dummy check status: Medium!}");
+      hStdDev->GetListOfFunctions()->Add(tl);
       tl->Draw();
     }
   }
