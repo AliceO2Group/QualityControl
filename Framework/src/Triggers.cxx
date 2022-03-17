c7663d034732d5d8e8eac04c94197cef1e1d2b06
Piotr Konopka <piotr.jan.konopka@cern.ch>
2022-03-17 15:37:35 +0100

    update also triggers

--- Framework/src/Triggers.cxx
+++ Framework/src/Triggers.cxx
@@ -142,6 +142,7 @@ TriggerFcn NewObject(std::string databaseUrl, std::string objectPath, const Acti
   // Key names in the header map.
   constexpr auto md5key = "Content-MD5";
   constexpr auto timestampKey = "Valid-From";
+  auto fullObjectPath = activity.mProvenance + "/" + objectPath;
 
   // We support only CCDB here.
   auto db = std::make_shared<o2::ccdb::CcdbApi>();
@@ -154,16 +155,16 @@ TriggerFcn NewObject(std::string databaseUrl, std::string objectPath, const Acti
   // If someone reuploaded an old object, it should not have an influence.
   std::string lastMD5;
   auto metadata = repository::database_helpers::asDatabaseMetadata(activity, false);
-  if (auto headers = db->retrieveHeaders(objectPath, metadata); headers.count(md5key)) {
+  if (auto headers = db->retrieveHeaders(fullObjectPath, metadata); headers.count(md5key)) {
     lastMD5 = headers[md5key];
   } else {
     // We don't make a fuss over it, because we might be just waiting for the first version of such object.
     // It should not happen often though, so having a warning makes sense.
-    ILOG(Warning, Support) << "No MD5 of the file '" << objectPath << "' in the db '" << databaseUrl << "', probably the file is missing." << ENDM;
+    ILOG(Warning, Support) << "No MD5 of the file '" << fullObjectPath << "' in the db '" << databaseUrl << "', probably the file is missing." << ENDM;
   }
 
-  return [db, databaseUrl = std::move(databaseUrl), objectPath = std::move(objectPath), lastMD5, activity, metadata]() mutable -> Trigger {
-    if (auto headers = db->retrieveHeaders(objectPath, metadata); headers.count(md5key)) {
+  return [db, databaseUrl = std::move(databaseUrl), fullObjectPath = std::move(fullObjectPath), lastMD5, activity, metadata]() mutable -> Trigger {
+    if (auto headers = db->retrieveHeaders(fullObjectPath, metadata); headers.count(md5key)) {
       auto newMD5 = headers[md5key];
       if (lastMD5 != newMD5) {
         lastMD5 = newMD5;
@@ -172,7 +173,7 @@ TriggerFcn NewObject(std::string databaseUrl, std::string objectPath, const Acti
     } else {
       // We don't make a fuss over it, because we might be just waiting for the first version of such object.
       // It should not happen often though, so having a warning makes sense.
-      ILOG(Warning, Support) << "No MD5 of the file '" << objectPath << "' in the db '" << databaseUrl << "', probably the file is missing." << ENDM;
+      ILOG(Warning, Support) << "No MD5 of the file '" << fullObjectPath << "' in the db '" << databaseUrl << "', probably the file is missing." << ENDM;
     }
 
     return { TriggerType::No, false };
@@ -184,13 +185,14 @@ TriggerFcn ForEachObject(std::string databaseUrl, std::string objectPath, const 
   // Key names in the header map.
   constexpr auto md5key = "Content-MD5";
   constexpr auto timestampKey = "Valid-From";
+  auto fullObjectPath = activity.mProvenance + "/" + objectPath;
 
   // We support only CCDB here.
   auto db = std::make_shared<repository::CcdbDatabase>();
   db->connect(databaseUrl, "", "", "");
 
-  auto objects = db->getListingAsPtree(objectPath).get_child("objects");
-  ILOG(Info, Support) << "Got " << objects.size() << " objects for the path '" << objectPath << "'" << ENDM;
+  auto objects = db->getListingAsPtree(fullObjectPath).get_child("objects");
+  ILOG(Info, Support) << "Got " << objects.size() << " objects for the path '" << fullObjectPath << "'" << ENDM;
   auto filteredObjects = std::make_shared<std::vector<boost::property_tree::ptree>>();
   const auto& filter = activity;
 
@@ -231,13 +233,14 @@ TriggerFcn ForEachLatest(std::string databaseUrl, std::string objectPath, const 
   // Key names in the header map.
   constexpr auto md5key = "Content-MD5";
   constexpr auto timestampKey = "Created";
+  auto fullObjectPath = activity.mProvenance + "/" + objectPath;
 
   // We support only CCDB here.
   auto db = std::make_shared<repository::CcdbDatabase>();
   db->connect(databaseUrl, "", "", "");
 
-  auto objects = db->getListingAsPtree(objectPath).get_child("objects");
-  ILOG(Info, Support) << "Got " << objects.size() << " objects for the path '" << objectPath << "'" << ENDM;
+  auto objects = db->getListingAsPtree(fullObjectPath).get_child("objects");
+  ILOG(Info, Support) << "Got " << objects.size() << " objects for the path '" << fullObjectPath << "'" << ENDM;
   auto filteredObjects = std::make_shared<std::vector<std::pair<Activity, boost::property_tree::ptree>>>();
   const auto& filter = activity;
 
