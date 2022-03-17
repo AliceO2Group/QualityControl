951a9be9f339442a95e79d4ae428d98474be8852
sandor-lokos <lokos@LAPTOP-OFGROSC5.localdomain>
2022-03-17 10:17:21 +0100

    git-clang formatted

--- Modules/FV0/include/FV0/DigitQcTaskLaser.h
+++ Modules/FV0/include/FV0/DigitQcTaskLaser.h
@@ -36,34 +36,36 @@
 
 using namespace o2::quality_control::core;
 
-namespace o2::quality_control_modules::fv0 {
+namespace o2::quality_control_modules::fv0
+{
 namespace ch_data = helper::channel_data;
 using ChannelData = o2::fv0::ChannelData;
 using Digit = o2::fv0::BCData;
 /// \brief Quality Control DPL Task for FV0's digit visualization
 /// \author Artur Furs afurs@cern.ch
-class DigitQcTaskLaser final : public TaskInterface {
-public:
+class DigitQcTaskLaser final : public TaskInterface 
+{
+ public:
   /// \brief Constructor
   DigitQcTaskLaser() = default;
   /// Destructor
   ~DigitQcTaskLaser() override;
 
   // Definition of the methods for the template method pattern
-  void initialize(o2::framework::InitContext &ctx) override;
-  void startOfActivity(Activity &activity) override;
+  void initialize(o2::framework::InitContext& ctx) override;
+  void startOfActivity(Activity& activity) override;
   void startOfCycle() override;
-  void monitorData(o2::framework::ProcessingContext &ctx) override;
+  void monitorData(o2::framework::ProcessingContext& ctx) override;
   void endOfCycle() override;
-  void endOfActivity(Activity &activity) override;
+  void endOfActivity(Activity& activity) override;
   void reset() override;
   constexpr static std::size_t sNCHANNELS_PM =
-      60; // 48(for PM) + 12(just in case for possible PM-LCS)
+    60; // 48(for PM) + 12(just in case for possible PM-LCS)
   constexpr static std::size_t sOrbitsPerTF = 256;
   constexpr static uint8_t sLaserBitPos = 5;
   constexpr static uint8_t sDataIsValidBitPos = 7;
 
-private:
+ private:
   // three ways of computing cycle duration:
   // 1) number of time frames
   // 2) time in ns from InteractionRecord: total range (totalMax - totalMin)
@@ -81,9 +83,10 @@ private:
                                   std::is_same<std::string, Param_t>::value ||
                                   (std::is_integral<Param_t>::value &&
                                    !std::is_same<bool, Param_t>::value)>::type>
-  auto parseParameters(const std::string &param, const std::string &del) {
+  auto parseParameters(const std::string &param, const std::string &del)
+  {
     std::regex reg(del);
-    std::sregex_token_iterator first{param.begin(), param.end(), reg, -1}, last;
+    std::sregex_token_iterator first{ param.begin(), param.end(), reg, -1 }, last;
     std::vector<Param_t> vecResult;
     for (auto it = first; it != last; it++) {
       if constexpr (std::is_integral<Param_t>::value &&
@@ -100,18 +103,22 @@ private:
 
   void rebinFromConfig();
 
-  TList *mListHistGarbage;
+  TList* mListHistGarbage;
   std::set<unsigned int> mSetAllowedChIDs;
   std::array<o2::InteractionRecord, sNCHANNELS_PM> mStateLastIR2Ch;
   std::map<int, std::string> mMapDigitTrgNames;
   std::map<int, std::string> mMapChTrgNames;
   std::map<std::string, std::vector<int>>
-      mMapPmModuleChannels; // PM name to its channels
+    mMapPmModuleChannels; // PM name to its channels
   std::unique_ptr<TH1F> mHistNumADC;
   std::unique_ptr<TH1F> mHistNumCFD;
 
   // temp
-  enum ETrgMenu { kMinBias, kOuterRing, kNChannels, kCharge, kInnerRing };
+  enum ETrgMenu { kMinBias, 
+                  kOuterRing,
+                  kNChannels,
+                  kCharge,
+                  kInnerRing };
 
   // Object which will be published
   std::unique_ptr<TH2F> mHistAmp2Ch;
@@ -134,12 +141,12 @@ private:
   std::unique_ptr<TH1D> mHistCycleDuration;
   std::unique_ptr<TH1D> mHistCycleDurationNTF;
   std::unique_ptr<TH1D> mHistCycleDurationRange;
-  std::map<unsigned int, TH1F *> mMapHistAmp1D;
-  std::map<unsigned int, TH1F *> mMapHistTime1D;
-  std::map<unsigned int, TH1F *> mMapHistPMbits;
-  std::map<unsigned int, TH2F *> mMapHistAmpVsTime;
-  std::map<unsigned int, TH2F *> mMapTrgBcOrbit;
-  std::map<std::string, TH2F *> mMapPmModuleBcOrbit;
+  std::map<unsigned int, TH1F*> mMapHistAmp1D;
+  std::map<unsigned int, TH1F*> mMapHistTime1D;
+  std::map<unsigned int, TH1F*> mMapHistPMbits;
+  std::map<unsigned int, TH2F*> mMapHistAmpVsTime;
+  std::map<unsigned int, TH2F*> mMapTrgBcOrbit;
+  std::map<std::string, TH2F*> mMapPmModuleBcOrbit;
 };
 
 } // namespace o2::quality_control_modules::fv0
