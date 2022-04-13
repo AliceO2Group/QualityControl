77e2e699cc6c0fc342eaf76d4184866ee2d45596
Dennis Klein <d.klein@gsi.de>
2022-04-13 15:57:19 +0200

    Do not use header file from FairRoot repo
    Context:
    
    FairLogger was originally part of FairRoot, but got split off into a
    separate repository in the past.
    
    | repo       | provides                                                        |
    | ---------- | --------------------------------------------------------------- |
    | FairLogger | <fairlogger/Logger.h>                                           |
    | FairRoot   | <FairLogger.h> (wraps <fairlogger/Logger.h>)                    |
    | FairMQ     | <FairMQLogger.h> (soon deprecated, wraps <fairlogger/Logger.h>) |
    | O2         | <Framework/Logger.h> (wraps <fairlogger/Logger.h>)              |

--- Modules/ZDC/src/ZDCRawDataTask.cxx
+++ Modules/ZDC/src/ZDCRawDataTask.cxx
@@ -1,4 +1,4 @@
-// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// Copyright 2019-2022 CERN and copyright holders of ALICE O2.
 // See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
 // All rights not expressly granted are reserved.
 //
@@ -30,7 +30,7 @@
 #include "CommonConstants/LHCConstants.h"
 #include "ZDCSimulation/Digits2Raw.h"
 #include "ZDCSimulation/ZDCSimParam.h"
-#include "FairLogger.h"
+#include <fairlogger/Logger.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
