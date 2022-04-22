f22e1d8bb4f6bdce569129365558cd725ad35094
Nasir Mehdi Malik <89008506+namalikgit@users.noreply.github.com>
2022-04-22 19:04:42 +0530

    pulseheight/chamber

--- Modules/TRD/include/TRD/PulseHeight.h
+++ Modules/TRD/include/TRD/PulseHeight.h
@@ -25,6 +25,8 @@ class TH1F;
 class TH1D;
 class TH2F;
 class TH2D;
+class TProfile;
+class TProfile2D;
 
 using namespace o2::quality_control::core;
 
@@ -56,18 +58,20 @@ class PulseHeight final : public TaskInterface
   std::shared_ptr<TH1F> mPulseHeight = nullptr;
   std::shared_ptr<TH1F> mPulseHeightScaled = nullptr;
   std::shared_ptr<TH2F> mTotalPulseHeight2D = nullptr;
-  std::array<std::shared_ptr<TH1F>, 18> mPulseHeight2DperSM; //ph2DSM;
+  std::array<std::shared_ptr<TH1F>, 18> mPulseHeight2DperSM; // ph2DSM;
   std::shared_ptr<TH1F> mPulseHeight2 = nullptr;
   std::shared_ptr<TH1F> mPulseHeight2n = nullptr;
   std::shared_ptr<TH1F> mPulseHeightScaled2 = nullptr;
   std::shared_ptr<TH2F> mTotalPulseHeight2D2 = nullptr;
-  std::array<std::shared_ptr<TH1F>, 18> mPulseHeight2DperSM2; //ph2DSM;
+  std::array<std::shared_ptr<TH1F>, 18> mPulseHeight2DperSM2; // ph2DSM;
   std::pair<float, float> mDriftRegion;
   std::pair<float, float> mPulseHeightPeakRegion;
   std::shared_ptr<TH1F> mPulseHeightDuration;
   std::shared_ptr<TH1F> mPulseHeightDuration1;
   std::shared_ptr<TH1F> mPulseHeightDurationDiff;
   std::shared_ptr<o2::trd::NoiseStatusMCM> mNoiseMap = nullptr;
+  std::shared_ptr<TProfile> mPulseHeightpro = nullptr;
+  std::shared_ptr<TProfile2D> mPulseHeightperchamber = nullptr;
 };
 
 } // namespace o2::quality_control_modules::trd
