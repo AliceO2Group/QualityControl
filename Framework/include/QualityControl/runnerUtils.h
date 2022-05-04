355856632e382af30f1f26d075e188955355826a
Piotr Konopka <piotr.jan.konopka@cern.ch>
2022-05-04 09:47:56 +0200

    Allow for empty values when overriding config files (#1162)

--- Framework/include/QualityControl/runnerUtils.h
+++ Framework/include/QualityControl/runnerUtils.h
@@ -139,10 +139,13 @@ inline std::vector<std::pair<std::string, std::string>> parseOverrideValues(cons
   std::vector<std::pair<std::string, std::string>> keyValuePairs;
   for (const auto& keyValueToken : utils::Str::tokenize(input, ';', true)) {
     auto keyValue = utils::Str::tokenize(keyValueToken, '=', true);
-    if (keyValue.size() != 2) {
+    if (keyValue.size() == 1) {
+      keyValuePairs.emplace_back(keyValue[0], "");
+    } else if (keyValue.size() == 2) {
+      keyValuePairs.emplace_back(keyValue[0], keyValue[1]);
+    } else {
       throw std::runtime_error("Token '" + keyValueToken + "' in the --override-values argument is malformed, use key=value.");
     }
-    keyValuePairs.emplace_back(keyValue[0], keyValue[1]);
   }
   return keyValuePairs;
 }
