15b9523111a8246c73bd287976cf9ae76db0d3b2
aferrero2707 <aferrero1975@gmail.com>
2022-05-23 22:39:06 +0200

    [MCH] improved preclusters analysis task
    - improved selection of pre-clusters for efficiency analysis
    - added plots with number of pre-clusters per TF
    - added plots with mean efficiency per detection element
    - added efficiency plot in electronics coordinates
    - improved beautify method in checker

--- Modules/MUON/MCH/src/PhysicsPreclustersCheck.cxx
+++ Modules/MUON/MCH/src/PhysicsPreclustersCheck.cxx
@@ -16,6 +16,7 @@
 #include "MCHMappingInterface/Segmentation.h"
 #include "MCHMappingSegContour/CathodeSegmentationContours.h"
 #include "MCH/PhysicsPreclustersCheck.h"
+#include "MCH/GlobalHistogram.h"
 
 // ROOT
 #include <fairlogger/Logger.h>
@@ -34,41 +35,33 @@ using namespace std;
 namespace o2::quality_control_modules::muonchambers
 {
 
-PhysicsPreclustersCheck::PhysicsPreclustersCheck()
+PhysicsPreclustersCheck::PhysicsPreclustersCheck() : mMinPseudoeff(0.5), mMaxPseudoeff(1.0)
 {
-  mPrintLevel = 1;
-  minPseudoeff = 0.5;
-  maxPseudoeff = 1.0;
-  minMPV = 300;
-  maxMPV = 700;
 }
 
 PhysicsPreclustersCheck::~PhysicsPreclustersCheck() {}
 
 void PhysicsPreclustersCheck::configure()
 {
-  //   if (AliRecoParam::ConvertIndex(specie) == AliRecoParam::kCosmic) {
-  //     minTOFrawTime = 150.; //ns
-  //     maxTOFrawTime = 250.; //ns
-  //   }
+  if (auto param = mCustomParameters.find("MinPseudoefficiency"); param != mCustomParameters.end()) {
+    mMinPseudoeff = std::stof(param->second);
+  }
+  if (auto param = mCustomParameters.find("MaxPseudoefficiency"); param != mCustomParameters.end()) {
+    mMaxPseudoeff = std::stof(param->second);
+  }
 }
 
 Quality PhysicsPreclustersCheck::check(std::map<std::string, std::shared_ptr<MonitorObject>>* moMap)
 {
-  //  std::cout << "=================================" << std::endl;
-  //  std::cout << "PhysicsOccupancyCheck::check() called" << std::endl;
-  //  std::cout << "=================================" << std::endl;
   Quality result = Quality::Null;
 
   for (auto& [moName, mo] : *moMap) {
 
     (void)moName;
-    //   std::cout << mo->getName() <<endl;
-    if (mo->getName().find("QcMuonChambers_MeanPseudoeff_Mergeable_DoesGoodBHaveSomethingNB") != std::string::npos) {
+    if ((mo->getName().find("MeanPseudoeffPerDE_B") != std::string::npos) ||
+        (mo->getName().find("MeanPseudoeffPerDE_NB") != std::string::npos)) {
       auto* h = dynamic_cast<TH1F*>(mo->getObject());
-      std::cout << mo->ClassName() << endl;
       if (!h) {
-        std::cout << "Pas trouvé l'objet" << std::endl;
         return result;
       }
 
@@ -78,140 +71,8 @@ Quality PhysicsPreclustersCheck::check(std::map<std::string, std::shared_ptr<Mon
         int nbinsx = h->GetXaxis()->GetNbins();
         int nbad = 0;
         for (int i = 1; i <= nbinsx; i++) {
-          Float_t occ = h->GetBinContent(i);
-          if (occ < minPseudoeff || occ > maxPseudoeff) {
-            nbad += 1;
-          }
-        }
-        if (nbad < 1) {
-          result = Quality::Good;
-          std::cout << "GOOD" << endl;
-        } else {
-          result = Quality::Bad;
-          std::cout << "BAD" << endl;
-        }
-      }
-    }
-  }
-  for (auto& [moName, mo] : *moMap) {
-    if (mo->getName().find("QcMuonChambers_MeanPseudoeff_Mergeable_DoesGoodNBHaveSomethingB") != std::string::npos) {
-      auto* h = dynamic_cast<TH1F*>(mo->getObject());
-      std::cout << mo->ClassName() << endl;
-      if (!h) {
-        std::cout << "Pas trouvé l'objet" << std::endl;
-        return result;
-      }
-
-      if (h->GetEntries() == 0) {
-        result = Quality::Medium;
-      } else {
-        int nbinsx = h->GetXaxis()->GetNbins();
-        int nbad = 0;
-        for (int i = 1; i <= nbinsx; i++) {
-          Float_t occ = h->GetBinContent(i);
-          if (occ < minPseudoeff || occ > maxPseudoeff) {
-            nbad += 1;
-          }
-        }
-        if (nbad < 1) {
-          result = Quality::Good;
-          std::cout << "GOOD" << endl;
-        } else {
-          result = Quality::Bad;
-          std::cout << "BAD" << endl;
-        }
-      }
-    }
-  }
-
-  for (auto& [moName, mo] : *moMap) {
-
-    (void)moName;
-    //  std::cout << mo->getName() <<endl;
-    if (mo->getName().find("mMeanPseudoeffPerDE_DoesGoodBHaveSomethingNB_OnCycle") != std::string::npos) {
-      auto* h = dynamic_cast<TH1F*>(mo->getObject());
-      std::cout << mo->ClassName() << endl;
-      if (!h) {
-        std::cout << "Pas trouvé l'objet" << std::endl;
-        return result;
-      }
-
-      if (h->GetEntries() == 0) {
-        result = Quality::Medium;
-      } else {
-        int nbinsx = h->GetXaxis()->GetNbins();
-        int nbad = 0;
-        for (int i = 1; i <= nbinsx; i++) {
-          Float_t occ = h->GetBinContent(i);
-          if (occ < minPseudoeff || occ > maxPseudoeff) {
-            nbad += 1;
-          }
-        }
-        if (nbad < 1) {
-          result = Quality::Good;
-          std::cout << "GOOD" << endl;
-        } else {
-          result = Quality::Bad;
-          std::cout << "BAD" << endl;
-        }
-      }
-    }
-  }
-
-  for (auto& [moName, mo] : *moMap) {
-
-    (void)moName;
-    //  std::cout << mo->getName() <<endl;
-    if (mo->getName().find("mMeanPseudoeffPerDE_DoesGoodNBHaveSomethingB_OnCycle") != std::string::npos) {
-      auto* h = dynamic_cast<TH1F*>(mo->getObject());
-      std::cout << mo->ClassName() << endl;
-      if (!h) {
-        std::cout << "Pas trouvé l'objet" << std::endl;
-        return result;
-      }
-
-      if (h->GetEntries() == 0) {
-        result = Quality::Medium;
-      } else {
-        int nbinsx = h->GetXaxis()->GetNbins();
-        int nbad = 0;
-        for (int i = 1; i <= nbinsx; i++) {
-          Float_t occ = h->GetBinContent(i);
-          if (occ < minPseudoeff || occ > maxPseudoeff) {
-            nbad += 1;
-          }
-        }
-        if (nbad < 1) {
-          result = Quality::Good;
-          std::cout << "GOOD" << endl;
-        } else {
-          result = Quality::Bad;
-          std::cout << "BAD" << endl;
-        }
-      }
-    }
-  }
-
-  for (auto& [moName, mo] : *moMap) {
-
-    (void)moName;
-    //     std::cout << mo->getName() <<endl;
-    if (mo->getName().find("QcMuonChambers_MPV_Mergeable_OnCycle") != std::string::npos) {
-      auto* h = dynamic_cast<TH1F*>(mo->getObject());
-      std::cout << mo->ClassName() << endl;
-      if (!h) {
-        std::cout << "Pas trouvé l'objet" << std::endl;
-        return result;
-      }
-
-      if (h->GetEntries() == 0) {
-        result = Quality::Medium;
-      } else {
-        int nbinsx = h->GetXaxis()->GetNbins();
-        int nbad = 0;
-        for (int i = 1; i <= nbinsx; i++) {
-          Float_t occ = h->GetBinContent(i);
-          if (occ < minMPV || occ > maxMPV) {
+          Float_t eff = h->GetBinContent(i);
+          if (eff < mMinPseudoeff || eff > mMaxPseudoeff) {
             nbad += 1;
           }
         }
@@ -231,264 +92,106 @@ Quality PhysicsPreclustersCheck::check(std::map<std::string, std::shared_ptr<Mon
 
 std::string PhysicsPreclustersCheck::getAcceptedType() { return "TH1"; }
 
+static void updateTitle(TH1* hist, std::string suffix)
+{
+  if (!hist) {
+    return;
+  }
+  TString title = hist->GetTitle();
+  title.Append(" ");
+  title.Append(suffix.c_str());
+  hist->SetTitle(title);
+}
+
+static std::string getCurrentTime()
+{
+  time_t t;
+  time(&t);
+
+  struct tm* tmp;
+  tmp = localtime(&t);
+
+  char timestr[500];
+  strftime(timestr, sizeof(timestr), "(%x - %X)", tmp);
+
+  std::string result = timestr;
+  return result;
+}
+
 void PhysicsPreclustersCheck::beautify(std::shared_ptr<MonitorObject> mo, Quality checkResult)
 {
-  //  std::cout<<"===================================="<<std::endl;
-  //  std::cout<<"PhysicsOccupancyCheck::beautify() called"<<std::endl;
-  //  std::cout<<"===================================="<<std::endl;
-  if (mo->getName().find("QcMuonChambers_MeanPseudoeff_Mergeable_DoesGoodBHaveSomethingNB") != std::string::npos) {
+  auto currentTime = getCurrentTime();
+  updateTitle(dynamic_cast<TH1*>(mo->getObject()), currentTime);
+
+  if ((mo->getName().find("MeanPseudoeffPerDE_B") != std::string::npos) ||
+      (mo->getName().find("MeanPseudoeffPerDE_NB") != std::string::npos) ||
+      (mo->getName().find("PreclustersPerDE") != std::string::npos) ||
+      (mo->getName().find("PreclustersSignalPerDE") != std::string::npos)) {
     auto* h = dynamic_cast<TH1F*>(mo->getObject());
+    // disable ticks on axis
+    h->GetXaxis()->SetTickLength(0.0);
+    h->GetXaxis()->SetLabelSize(0.0);
+    h->GetYaxis()->SetTickLength(0);
+    h->GetYaxis()->SetTitle("efficiency");
+
+    h->SetMinimum(0);
+    h->SetMaximum(2);
+
+    TText* xtitle = new TText();
+    xtitle->SetNDC();
+    xtitle->SetText(0.87, 0.03, "chamber #");
+    xtitle->SetTextSize(15);
+    h->GetListOfFunctions()->Add(xtitle);
+
+    // draw chamber delimiters
+    for (int demin = 200; demin <= 1000; demin += 100) {
+      float xpos = static_cast<float>(getDEindex(demin));
+      TLine* delimiter = new TLine(xpos, 0, xpos, 2);
+      delimiter->SetLineColor(kBlack);
+      delimiter->SetLineStyle(kDashed);
+      h->GetListOfFunctions()->Add(delimiter);
+    }
+
+    // draw x-axis labels
+    for (int ch = 1; ch <= 10; ch += 1) {
+      float x1 = static_cast<float>(getDEindex(ch * 100));
+      float x2 = (ch < 10) ? static_cast<float>(getDEindex(ch * 100 + 100)) : h->GetXaxis()->GetXmax();
+      float x0 = 0.8 * (x1 + x2) / (2 * h->GetXaxis()->GetXmax()) + 0.1;
+      float y0 = 0.05;
+      TText* label = new TText();
+      label->SetNDC();
+      label->SetText(x0, y0, TString::Format("%d", ch));
+      label->SetTextSize(15);
+      label->SetTextAlign(22);
+      h->GetListOfFunctions()->Add(label);
+    }
+
     TPaveText* msg = new TPaveText(0.3, 0.9, 0.7, 0.95, "NDC");
     h->GetListOfFunctions()->Add(msg);
     msg->SetName(Form("%s_msg", mo->GetName()));
 
-    TLine* lmin = new TLine(0, minPseudoeff, 1100, minPseudoeff);
-    TLine* lmax = new TLine(0, maxPseudoeff, 1100, maxPseudoeff);
+    TLine* lmin = new TLine(0, mMinPseudoeff, getDEindexMax(), mMinPseudoeff);
+    TLine* lmax = new TLine(0, mMaxPseudoeff, getDEindexMax(), mMaxPseudoeff);
 
     h->GetListOfFunctions()->Add(lmin);
     h->GetListOfFunctions()->Add(lmax);
 
-    //      lmin->Draw();
-    //      lmax->Draw();
     if (checkResult == Quality::Good) {
       msg->Clear();
       msg->AddText("Pseudo-efficiency consistently within limits: OK!!!");
       msg->SetFillColor(kGreen);
-      //
-      //   h->SetFillColor(kGreen);
     } else if (checkResult == Quality::Bad) {
       LOG(info) << "Quality::Bad, setting to red";
-      //
       msg->Clear();
       msg->AddText("Call MCH on-call.");
       msg->SetFillColor(kRed);
-      //
-      //  h->SetFillColor(kRed);
     } else if (checkResult == Quality::Medium) {
       LOG(info) << "Quality::medium, setting to orange";
-      //
       msg->Clear();
       msg->AddText("No entries. If MCH in the run, check MCH TWiki");
       msg->SetFillColor(kYellow);
-      //   h->SetFillColor(kOrange);
     }
     h->SetLineColor(kBlack);
-
-    for (Int_t i = 1; i <= h->GetNbinsX(); i += 1) {
-      TBox* b = new TBox(h->GetBinLowEdge(i),
-                         h->GetMinimum(),
-                         h->GetBinWidth(i) + h->GetBinLowEdge(i),
-                         h->GetBinContent(i));
-      b->SetFillColor(kGreen);
-      if (h->GetBinContent(i) < minPseudoeff || h->GetBinContent(i) > maxPseudoeff) {
-        b->SetFillColor(kRed);
-      }
-      h->GetListOfFunctions()->Add(b);
-    }
-  }
-
-  if (mo->getName().find("QcMuonChambers_MeanPseudoeff_Mergeable_DoesGoodNBHaveSomethingB") != std::string::npos) {
-    auto* h = dynamic_cast<TH1F*>(mo->getObject());
-    TPaveText* msg = new TPaveText(0.3, 0.9, 0.7, 0.95, "NDC");
-    h->GetListOfFunctions()->Add(msg);
-    msg->SetName(Form("%s_msg", mo->GetName()));
-
-    TLine* lmin = new TLine(0, minPseudoeff, 1100, minPseudoeff);
-    TLine* lmax = new TLine(0, maxPseudoeff, 1100, maxPseudoeff);
-
-    h->GetListOfFunctions()->Add(lmin);
-    h->GetListOfFunctions()->Add(lmax);
-
-    //      lmin->Draw();
-    //      lmax->Draw();
-    if (checkResult == Quality::Good) {
-      msg->Clear();
-      msg->AddText("Pseudo-efficiency consistently within limits: OK!!!");
-      msg->SetFillColor(kGreen);
-      //
-      //   h->SetFillColor(kGreen);
-    } else if (checkResult == Quality::Bad) {
-      LOG(info) << "Quality::Bad, setting to red";
-      //
-      msg->Clear();
-      msg->AddText("Call MCH on-call.");
-      msg->SetFillColor(kRed);
-      //
-      //  h->SetFillColor(kRed);
-    } else if (checkResult == Quality::Medium) {
-      LOG(info) << "Quality::medium, setting to orange";
-      //
-      msg->Clear();
-      msg->AddText("No entries. If MCH in the run, check MCH TWiki");
-      msg->SetFillColor(kYellow);
-      //   h->SetFillColor(kOrange);
-    }
-    h->SetLineColor(kBlack);
-
-    for (Int_t i = 1; i <= h->GetNbinsX(); i += 1) {
-      TBox* b = new TBox(h->GetBinLowEdge(i),
-                         h->GetMinimum(),
-                         h->GetBinWidth(i) + h->GetBinLowEdge(i),
-                         h->GetBinContent(i));
-      b->SetFillColor(kGreen);
-      if (h->GetBinContent(i) < minPseudoeff || h->GetBinContent(i) > maxPseudoeff) {
-        b->SetFillColor(kRed);
-      }
-      h->GetListOfFunctions()->Add(b);
-    }
-  }
-
-  if (mo->getName().find("mMeanPseudoeffPerDE_DoesGoodBHaveSomethingNB_OnCycle") != std::string::npos) {
-    auto* h = dynamic_cast<TH1F*>(mo->getObject());
-    TPaveText* msg = new TPaveText(0.3, 0.9, 0.7, 0.95, "NDC");
-    h->GetListOfFunctions()->Add(msg);
-    msg->SetName(Form("%s_msg", mo->GetName()));
-
-    TLine* lmin = new TLine(0, minPseudoeff, 1100, minPseudoeff);
-    TLine* lmax = new TLine(0, maxPseudoeff, 1100, maxPseudoeff);
-
-    h->GetListOfFunctions()->Add(lmin);
-    h->GetListOfFunctions()->Add(lmax);
-
-    //      lmin->Draw();
-    //      lmax->Draw();
-    if (checkResult == Quality::Good) {
-      msg->Clear();
-      msg->AddText("Pseudo-efficiency consistently within limits: OK!!!");
-      msg->SetFillColor(kGreen);
-      //
-      //   h->SetFillColor(kGreen);
-    } else if (checkResult == Quality::Bad) {
-      LOG(info) << "Quality::Bad, setting to red";
-      //
-      msg->Clear();
-      msg->AddText("Call MCH on-call.");
-      msg->SetFillColor(kRed);
-      //
-      //  h->SetFillColor(kRed);
-    } else if (checkResult == Quality::Medium) {
-      LOG(info) << "Quality::medium, setting to orange";
-      //
-      msg->Clear();
-      msg->AddText("No entries. If MCH in the run, check MCH TWiki");
-      msg->SetFillColor(kYellow);
-      //   h->SetFillColor(kOrange);
-    }
-    h->SetLineColor(kBlack);
-
-    for (Int_t i = 1; i <= h->GetNbinsX(); i += 1) {
-      TBox* b = new TBox(h->GetBinLowEdge(i),
-                         h->GetMinimum(),
-                         h->GetBinWidth(i) + h->GetBinLowEdge(i),
-                         h->GetBinContent(i));
-      b->SetFillColor(kGreen);
-      if (h->GetBinContent(i) < minPseudoeff || h->GetBinContent(i) > maxPseudoeff) {
-        b->SetFillColor(kRed);
-      }
-      h->GetListOfFunctions()->Add(b);
-    }
-  }
-
-  if (mo->getName().find("mMeanPseudoeffPerDE_DoesGoodNBHaveSomethingB_OnCycle") != std::string::npos) {
-    auto* h = dynamic_cast<TH1F*>(mo->getObject());
-    TPaveText* msg = new TPaveText(0.3, 0.9, 0.7, 0.95, "NDC");
-    h->GetListOfFunctions()->Add(msg);
-    msg->SetName(Form("%s_msg", mo->GetName()));
-
-    TLine* lmin = new TLine(0, minPseudoeff, 1100, minPseudoeff);
-    TLine* lmax = new TLine(0, maxPseudoeff, 1100, maxPseudoeff);
-
-    h->GetListOfFunctions()->Add(lmin);
-    h->GetListOfFunctions()->Add(lmax);
-
-    //      lmin->Draw();
-    //      lmax->Draw();
-    if (checkResult == Quality::Good) {
-      msg->Clear();
-      msg->AddText("Pseudo-efficiency consistently within limits: OK!!!");
-      msg->SetFillColor(kGreen);
-      //
-      //   h->SetFillColor(kGreen);
-    } else if (checkResult == Quality::Bad) {
-      LOG(info) << "Quality::Bad, setting to red";
-      //
-      msg->Clear();
-      msg->AddText("Call MCH on-call.");
-      msg->SetFillColor(kRed);
-      //
-      //  h->SetFillColor(kRed);
-    } else if (checkResult == Quality::Medium) {
-      LOG(info) << "Quality::medium, setting to orange";
-      //
-      msg->Clear();
-      msg->AddText("No entries. If MCH in the run, check MCH TWiki");
-      msg->SetFillColor(kYellow);
-      //   h->SetFillColor(kOrange);
-    }
-    h->SetLineColor(kBlack);
-
-    for (Int_t i = 1; i <= h->GetNbinsX(); i += 1) {
-      TBox* b = new TBox(h->GetBinLowEdge(i),
-                         h->GetMinimum(),
-                         h->GetBinWidth(i) + h->GetBinLowEdge(i),
-                         h->GetBinContent(i));
-      b->SetFillColor(kGreen);
-      if (h->GetBinContent(i) < minPseudoeff || h->GetBinContent(i) > maxPseudoeff) {
-        b->SetFillColor(kRed);
-      }
-      h->GetListOfFunctions()->Add(b);
-    }
-  }
-
-  if (mo->getName().find("QcMuonChambers_MPV_Mergeable_OnCycle") != std::string::npos) {
-    auto* h = dynamic_cast<TH1F*>(mo->getObject());
-    TPaveText* msg = new TPaveText(0.3, 0.9, 0.7, 0.95, "NDC");
-    h->GetListOfFunctions()->Add(msg);
-    msg->SetName(Form("%s_msg", mo->GetName()));
-
-    TLine* lmin = new TLine(0, minMPV, 1100, minMPV);
-    TLine* lmax = new TLine(0, maxMPV, 1100, maxMPV);
-
-    h->GetListOfFunctions()->Add(lmin);
-    h->GetListOfFunctions()->Add(lmax);
-
-    //      lmin->Draw();
-    //      lmax->Draw();
-    if (checkResult == Quality::Good) {
-      msg->Clear();
-      msg->AddText("MPV consistently within limits: OK!!!");
-      msg->SetFillColor(kGreen);
-      //
-      //   h->SetFillColor(kGreen);
-    } else if (checkResult == Quality::Bad) {
-      LOG(info) << "Quality::Bad, setting to red";
-      //
-      msg->Clear();
-      msg->AddText("Call MCH on-call.");
-      msg->SetFillColor(kRed);
-      //
-      //  h->SetFillColor(kRed);
-    } else if (checkResult == Quality::Medium) {
-      LOG(info) << "Quality::medium, setting to orange";
-      //
-      msg->Clear();
-      msg->AddText("No entries. If MCH in the run, check MCH TWiki");
-      msg->SetFillColor(kYellow);
-      //   h->SetFillColor(kOrange);
-    }
-    h->SetLineColor(kBlack);
-
-    for (Int_t i = 1; i <= h->GetNbinsX(); i += 1) {
-      TBox* b = new TBox(h->GetBinLowEdge(i),
-                         h->GetMinimum(),
-                         h->GetBinWidth(i) + h->GetBinLowEdge(i),
-                         h->GetBinContent(i));
-      b->SetFillColor(kGreen);
-      if (h->GetBinContent(i) < minMPV || h->GetBinContent(i) >= maxMPV) {
-        b->SetFillColor(kRed);
-      }
-      h->GetListOfFunctions()->Add(b);
-    }
   }
 }
 
