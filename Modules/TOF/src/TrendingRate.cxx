faf968f04de8cd77eb72d31d26a4cf065ca7928e
Francesca Ercolessi <francesca.ercolessi@cern.ch>
2022-06-06 14:33:44 +0200

    first commit trending of interaction rate
    minor

---
+++ Modules/TOF/src/TrendingRate.cxx
@@ -0,0 +1,210 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file    TrendingRate.cxx
+/// \author  Francesca Ercolessi francesca.ercolessi@cern.ch
+/// \brief  
+/// \since   06/06/2022
+///
+
+#include "TOF/TrendingRate.h"
+#include "QualityControl/QcInfoLogger.h"
+#include "QualityControl/DatabaseInterface.h"
+#include "QualityControl/MonitorObject.h"
+#include "QualityControl/Reductor.h"
+#include "QualityControl/RootClassFactory.h"
+#include <boost/property_tree/ptree.hpp>
+#include <TH1.h>
+#include <TH2.h>
+#include <TCanvas.h>
+#include <TPaveText.h>
+#include <TDatime.h>
+#include <TGraphErrors.h>
+#include <TPoint.h>
+
+using namespace o2::quality_control;
+using namespace o2::quality_control::core;
+using namespace o2::quality_control::postprocessing;
+using namespace o2::quality_control_modules::tof;
+
+void TrendingRate::configure(std::string name, const boost::property_tree::ptree& config)
+{
+  mConfig = TrendingConfigTOF(name, config);
+}
+
+void TrendingRate::initialize(Trigger, framework::ServiceRegistry&)
+{
+  // Preparing data structure of TTree
+  mTrend = std::make_unique<TTree>(); 
+  mTrend->SetName(PostProcessingInterface::getName().c_str());
+  mTrend->Branch("runNumber", &mMetaData.runNumber);
+  mTrend->Branch("time", &mTime);
+
+  /*if (auto param = mCustomParameters.find("ThresholdSgn"); param != mCustomParameters.end()) {
+    mThresholdSgn = ::atof(param->second.c_str());
+  }
+  if (auto param = mCustomParameters.find("ThresholdBkg"); param != mCustomParameters.end()) {
+    mThresholdBkg = ::atof(param->second.c_str());
+  }*/
+  for (const auto& source : mConfig.dataSources) {
+    std::unique_ptr<Reductor> reductor(root_class_factory::create<Reductor>(source.moduleName, source.reductorName));
+    mTrend->Branch(source.name.c_str(), reductor->getBranchAddress(), reductor->getBranchLeafList());
+    mReductors[source.name] = std::move(reductor);
+  }
+  getObjectsManager()->startPublishing(mTrend.get());
+}
+
+//todo: see if OptimizeBaskets() indeed helps after some time
+void TrendingRate::update(Trigger t, framework::ServiceRegistry& services)
+{
+  auto& qcdb = services.get<repository::DatabaseInterface>();
+
+  trendValues(t, qcdb);
+  generatePlots();
+}
+
+void TrendingRate::finalize(Trigger t, framework::ServiceRegistry&)
+{
+  generatePlots();
+}
+
+void TrendingRate::trendValues(const Trigger& t, repository::DatabaseInterface& qcdb)
+{
+  mTime = t.timestamp / 1000; // ROOT expects seconds since epoch
+  // todo get run number when it is available. consider putting it inside monitor object's metadata (this might be not
+  //  enough if we trend across runs).
+  mMetaData.runNumber = -1;
+
+  for (auto& dataSource : mConfig.dataSources) {
+
+    auto HitMap = qcdb.retrieveMO(dataSource.path, nameHitMap, t.timestamp, t.activity);
+    TH2F *objHitMap = HitMap ? (TH2F *)HitMap->getObject() : nullptr;
+    if (objHitMap) {
+      objHitMap->Divide(objHitMap);
+      mReductors[dataSource.name]->update(objHitMap);
+    }
+    else {
+      ILOG(Error, Support) << "Unknown type of data source '" << dataSource.type << "'." << ENDM;
+    }
+    unsigned int nactivechannels = objHitMap->Integral()*24;
+    ILOG(Info, Support) << "Number of active channels...:  " << nactivechannels << ENDM;
+
+    auto MultVsBC = qcdb.retrieveMO(dataSource.path, nameMultVsBC, t.timestamp, t.activity);
+    auto objMultVsBC = MultVsBC ? MultVsBC->getObject() : nullptr;
+    if (objMultVsBC) {
+      mReductors[dataSource.name]->update(objMultVsBC);
+    }
+    else {
+      ILOG(Error, Support) << "Unknown type of data source '" << dataSource.type << "'." << ENDM;
+    }
+
+    auto MultVsBCpro = qcdb.retrieveMO(dataSource.path, nameMultVsBCpro, t.timestamp, t.activity);
+    auto objMultVsBCpro = MultVsBCpro ? MultVsBCpro->getObject() : nullptr;
+    if (objMultVsBCpro) {
+      mReductors[dataSource.name]->update(objMultVsBCpro);
+    }
+    else {
+      ILOG(Error, Support) << "Unknown type of data source '" << dataSource.type << "'." << ENDM;
+    }
+   
+  }  
+  mTrend->Fill();
+}
+
+void TrendingRate::generatePlots()
+{
+  if (mTrend->GetEntries() < 1) {
+    ILOG(Info, Support) << "No entries in the trend so far, won't generate any plots." << ENDM;
+    return;
+  }
+
+  ILOG(Info, Support) << "Generating " << mConfig.plots.size() << " plots." << ENDM;
+
+  for (const auto& plot : mConfig.plots) {
+
+    // Before we generate any new plots, we have to delete existing under the same names.
+    // It seems that ROOT cannot handle an existence of two canvases with a common name in the same process.
+    if (mPlots.count(plot.name)) {
+      getObjectsManager()->stopPublishing(plot.name);
+      delete mPlots[plot.name];
+    }
+
+    // we determine the order of the plot, i.e. if it is a histogram (1), graph (2), or any higher dimension.
+    const size_t plotOrder = std::count(plot.varexp.begin(), plot.varexp.end(), ':') + 1;
+    // we have to delete the graph errors after the plot is saved, unfortunately the canvas does not take its ownership
+    TGraphErrors* graphErrors = nullptr;
+
+    TCanvas* c = new TCanvas();
+
+    mTrend->Draw(plot.varexp.c_str(), plot.selection.c_str(), plot.option.c_str());
+
+    c->SetName(plot.name.c_str());
+    c->SetTitle(plot.title.c_str());
+
+    // For graphs we allow to draw errors if they are specified.
+    if (!plot.graphErrors.empty()) {
+      if (plotOrder != 2) {
+        ILOG(Error, Support) << "Non empty graphErrors seen for the plot '" << plot.name << "', which is not a graph, ignoring." << ENDM;
+      } else {
+        // We generate some 4-D points, where 2 dimensions represent graph points and 2 others are the error bars
+        std::string varexpWithErrors(plot.varexp + ":" + plot.graphErrors);
+        mTrend->Draw(varexpWithErrors.c_str(), plot.selection.c_str(), "goff");
+        graphErrors = new TGraphErrors(mTrend->GetSelectedRows(), mTrend->GetVal(1), mTrend->GetVal(0), mTrend->GetVal(2), mTrend->GetVal(3));
+        // We draw on the same plot as the main graph, but only error bars
+        graphErrors->Draw("SAME E");
+        // We try to convince ROOT to delete graphErrors together with the rest of the canvas.
+        if (auto* pad = c->GetPad(0)) {
+          if (auto* primitives = pad->GetListOfPrimitives()) {
+            primitives->Add(graphErrors);
+          }
+        }
+      }
+    }
+
+    // Postprocessing the plot - adding specified titles, configuring time-based plots, flushing buffers.
+    // Notice that axes and title are drawn using a histogram, even in the case of graphs.
+    if (auto histo = dynamic_cast<TH1*>(c->GetPrimitive("htemp"))) {
+      // The title of histogram is printed, not the title of canvas => we set it as well.
+      histo->SetTitle(plot.title.c_str());
+      // We have to update the canvas to make the title appear.
+      c->Update();
+
+      // After the update, the title has a different size and it is not in the center anymore. We have to fix that.
+      if (auto title = dynamic_cast<TPaveText*>(c->GetPrimitive("title"))) {
+        title->SetBBoxCenterX(c->GetBBoxCenter().fX);
+        // It will have an effect only after invoking Draw again.
+        title->Draw();
+      } else {
+        ILOG(Error, Devel) << "Could not get the title TPaveText of the plot '" << plot.name << "'." << ENDM;
+      }
+
+      // We have to explicitly configure showing time on x axis.
+      // I hope that looking for ":time" is enough here and someone doesn't come with an exotic use-case.
+      if (plot.varexp.find(":time") != std::string::npos) {
+        histo->GetXaxis()->SetTimeDisplay(1);
+        // It deals with highly congested dates labels
+        histo->GetXaxis()->SetNdivisions(505);
+        // Without this it would show dates in order of 2044-12-18 on the day of 2019-12-19.
+        histo->GetXaxis()->SetTimeOffset(0.0);
+        histo->GetXaxis()->SetTimeFormat("%Y-%m-%d %H:%M");
+      }
+      // QCG doesn't empty the buffers before visualizing the plot, nor does ROOT when saving the file,
+      // so we have to do it here.
+      histo->BufferEmpty();
+    } else {
+      ILOG(Error, Devel) << "Could not get the htemp histogram of the plot '" << plot.name << "'." << ENDM;
+    }
+
+    mPlots[plot.name] = c;
+    getObjectsManager()->startPublishing(c);
+  }
+}
