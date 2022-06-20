bddf3799030ff5baa03b44860e0041c74a934e08
cpuggion84 <90757182+cpuggion84@users.noreply.github.com>
2022-06-20 09:10:35 +0200

    Update Default histograms fixed bug reset (#1280)

--- Modules/ZDC/include/ZDC/ZDCRawDataTask.h
+++ Modules/ZDC/include/ZDC/ZDCRawDataTask.h
@@ -1,4 +1,4 @@
-// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// Copyright 2019-2022 CERN and copyright holders of ALICE O2.
 // See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
 // All rights not expressly granted are reserved.
 //
@@ -8,7 +8,6 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
-
 ///
 /// \file   ZDCRawDataTask.h
 /// \author Carlo Puggioni
