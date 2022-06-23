67ca29a33bd74a79bed5797cb2aa60ec52b22847
sandor-lokos <lokos@LAPTOP-OFGROSC5.localdomain>
2022-06-23 18:06:34 +0200

    Developed TH1Reductor and simplified DigitQcTaskLaser

--- Modules/FT0/src/TH1ReductorLaser.cxx
+++ Modules/FT0/src/TH1ReductorLaser.cxx
@@ -19,68 +19,76 @@
 #include "QualityControl/QcInfoLogger.h"
 #include <TF2.h>
 #include <TH2.h>
+#include <sstream>
 
 namespace o2::quality_control_modules::ft0
 {
 void* TH1ReductorLaser::getBranchAddress() { return &mStats; }
 const char* TH1ReductorLaser::getBranchLeafList()
 {
-  return "validity1/D:validity2/D:mean/D:mean1/D:mean2/D:stddev:stddev1:stddev2:entries";
+  return Form("validity1/D:validity2/D:mean1/D:mean2/D:mean[%i]/D:stddev1:stddev2:stddev[%i]", NChannel, NChannel);
 }
 
 void TH1ReductorLaser::update(TObject* obj)
 {
-  if (auto histo = dynamic_cast<TH2*>(obj)) {
-    TH1* bc_projection = histo->ProjectionY("bc_projection", 0, -1);
-    int ibc = 0;
-    int ibc_max = 0;
-    if (bc_projection->GetEntries() > 0) {
-      ibc = bc_projection->GetMean() - 2. * bc_projection->GetStdDev();
-      ibc_max = bc_projection->GetMean() + 2. * bc_projection->GetStdDev();
+  if (auto histo = dynamic_cast<TH2F*>(obj)) {
+    int channel = -1;
+    sscanf(histo->GetName(), "%*[^0-9]%d", &channel);
+    if (channel < NChannel) {
+      for(int ichannel = 1; ichannel < NChannel; ichannel++) {
+        TH1* bc_projection = histo->ProjectionY(Form("first peak in BC #%d", ichannel), ichannel, ichannel+1);
+        mStats.mean[ichannel] = bc_projection->GetMean();
+      }
     }
+    else {
+      TH1* bc_projection = histo->ProjectionY("bc_projection", 0, -1);
+      int ibc = 0;
+      int ibc_max = 0;
+      if (bc_projection->GetEntries() > 0) {
+        ibc = bc_projection->GetMean() - 2. * bc_projection->GetStdDev();
+        ibc_max = bc_projection->GetMean() + 2. * bc_projection->GetStdDev();
+      }
 
-    TH1* slice_first_peak;
-    bool gotFirstPeak = false;
-    mStats.mean1 = 0.;
-    mStats.stddev1 = 0.;
-    mStats.validity1 = 0.;
-    while (!gotFirstPeak && ibc < ibc_max) {
-      slice_first_peak = histo->ProjectionX(Form("first peak in BC #%d", ibc), ibc, ibc + 1);
-      if (slice_first_peak->GetEntries() > 1000) {
-        mStats.mean1 = slice_first_peak->GetMean();
-        mStats.stddev1 = slice_first_peak->GetStdDev();
-        mStats.validity1 = 1.;
-        gotFirstPeak = true;
-        ibc += 2;
-        break;
-      } else
-        ibc++;
+      TH1* slice_first_peak;
+      bool gotFirstPeak = false;
+      mStats.mean1 = 0.;
+      mStats.stddev1 = 0.;
+      mStats.validity1 = 0.;
+      while (!gotFirstPeak && ibc < ibc_max) {
+        slice_first_peak = histo->ProjectionX(Form("first peak in BC #%d", ibc), ibc, ibc + 1);
+        if (slice_first_peak->GetEntries() > 1000) {
+          mStats.mean1 = slice_first_peak->GetMean();
+          mStats.stddev1 = slice_first_peak->GetStdDev();
+          mStats.validity1 = 1.;
+          gotFirstPeak = true;
+          ibc += 2;
+          break;
+        } else
+          ibc++;
+      }
+
+      TH1* slice_second_peak;
+      bool gotSecondPeak = false;
+      mStats.mean2 = 0.;
+      mStats.stddev2 = 0.;
+      mStats.validity2 = 0.;
+      while (!gotSecondPeak && gotFirstPeak && ibc < ibc_max) {
+        slice_second_peak = histo->ProjectionX(Form("second peak in BC #%d", ibc), ibc, ibc + 1);
+        if (slice_second_peak->GetEntries() > 1000) {
+          mStats.mean2 = slice_second_peak->GetMean();
+          mStats.stddev2 = slice_second_peak->GetStdDev();
+          mStats.validity2 = 1.;
+          gotSecondPeak = true;
+          break;
+        } else
+          ibc++;
+      }
+
+      if (!gotSecondPeak)
+        ILOG(Warning) << "TH1ReductorLaser: one of the peaks of the reference PMT is missing!" << ENDM;
+      if (!gotFirstPeak && !gotSecondPeak)
+        ILOG(Warning) << "TH1ReductorLaser: cannot find peaks of the reference PMT distribution at all !" << ENDM;
     }
-
-    TH1* slice_second_peak;
-    bool gotSecondPeak = false;
-    mStats.mean2 = 0.;
-    mStats.stddev2 = 0.;
-    mStats.validity2 = 0.;
-    while (!gotSecondPeak && gotFirstPeak && ibc < ibc_max) {
-      slice_second_peak = histo->ProjectionX(Form("second peak in BC #%d", ibc), ibc, ibc + 1);
-      if (slice_second_peak->GetEntries() > 1000) {
-        mStats.mean2 = slice_second_peak->GetMean();
-        mStats.stddev2 = slice_second_peak->GetStdDev();
-        mStats.validity2 = 1.;
-        gotSecondPeak = true;
-        break;
-      } else
-        ibc++;
-    }
-
-    if (!gotSecondPeak)
-      ILOG(Warning) << "TH1ReductorLaser: one of the peaks of the reference PMT is missing!" << ENDM;
-    if (!gotFirstPeak && !gotSecondPeak)
-      ILOG(Warning) << "TH1ReductorLaser: cannot find peaks of the reference PMT distribution at all !" << ENDM;
-  } else if (auto histo = dynamic_cast<TH1*>(obj)) {
-    mStats.mean = histo->GetMean();
-    mStats.stddev = histo->GetMean();
   }
 }
 } // namespace o2::quality_control_modules::ft0
