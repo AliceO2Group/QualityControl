8634b01b5731393f9fea19ac366ab9f54aa03f60
Tomas Herman <tomas.herman@cern.ch>
2022-06-27 17:59:53 +0200

    MFT: Adding drawing options and info on empty bins for trending.

--- Modules/MFT/src/QcMFTDigitCheck.cxx
+++ Modules/MFT/src/QcMFTDigitCheck.cxx
@@ -50,6 +50,9 @@ Quality QcMFTDigitCheck::check(std::map<std::string, std::shared_ptr<MonitorObje
       float den = hChipOccupancy->GetBinContent(0); // normalisation stored in the uderflow bin
 
       for (int iBin = 0; iBin < hChipOccupancy->GetNbinsX(); iBin++) {
+        if (hChipOccupancy->GetBinContent(iBin + 1) == 0) {
+          hChipOccupancy->Fill(937); // number of chips with zero digits stored in the overflow bin
+        }
         float num = hChipOccupancy->GetBinContent(iBin + 1);
         float ratio = (den > 0) ? (num / den) : 0.0;
         hChipOccupancy->SetBinContent(iBin + 1, ratio);
