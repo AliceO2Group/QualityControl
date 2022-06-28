6a571825b5a014f3c1d1e223e76a464ca5a1fd7c
sandor-lokos <69418976+sandor-lokos@users.noreply.github.com>
2022-06-28 11:27:17 +0200

    Developed TH1Reductor and simplified DigitQcTaskLaser (#1301)
    * Developed TH1Reductor and simplified DigitQcTaskLaser
    
    * DigitQcTaskLaser only generates the necessary histograms
    
    * TH1ReductorLaser CLang formatted correctly
    
    Co-authored-by: sandor-lokos <lokos@LAPTOP-OFGROSC5.localdomain>

--- Modules/FT0/include/FT0/TH1ReductorLaser.h
+++ Modules/FT0/include/FT0/TH1ReductorLaser.h
@@ -37,15 +37,17 @@ class TH1ReductorLaser : public quality_control::postprocessing::Reductor
   void update(TObject* obj) override;
 
  private:
+  static constexpr int NChannel = 208;
   struct {
     Double_t validity1;
     Double_t validity2;
-    Double_t mean;
     Double_t mean1;
     Double_t mean2;
-    Double_t stddev;
+    Double_t mean[NChannel];
     Double_t stddev1;
     Double_t stddev2;
+    Double_t stddev[NChannel];
+
   } mStats;
 };
 
