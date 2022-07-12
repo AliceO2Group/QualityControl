fcf3d68fd0a3f0c6d20eb9ccdaf21125abfab37c
Barthelemy <barthelemy.von.haller@cern.ch>
2022-07-12 15:55:36 +0200

    fix

--- Modules/EMCAL/include/EMCAL/RawCheck.h
+++ Modules/EMCAL/include/EMCAL/RawCheck.h
@@ -38,6 +38,7 @@ class RawCheck final : public o2::quality_control::checker::CheckInterface
   ~RawCheck() override = default;
 
   // Override interface
+  void configure() override;
   Quality check(std::map<std::string, std::shared_ptr<MonitorObject>>* moMap) override;
   void beautify(std::shared_ptr<MonitorObject> mo, Quality checkResult = Quality::Null) override;
   std::string getAcceptedType() override;
