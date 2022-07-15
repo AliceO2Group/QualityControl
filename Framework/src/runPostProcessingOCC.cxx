d036a5c7f731f9802e48ca253499658f9b262eb1
Barthelemy <barthelemy.von.haller@cern.ch>
2022-07-15 10:26:51 +0200

    format

--- Framework/src/runPostProcessingOCC.cxx
+++ Framework/src/runPostProcessingOCC.cxx
@@ -51,7 +51,7 @@ class PostProcessingOCCStateMachine : public RuntimeControlledObject
     try {
       auto config = properties.count(qcConfigurationKey) > 0 ? properties.get_child(qcConfigurationKey) : properties;
       mRunner->init(config);
-    }  catch (const boost::exception& ex) {
+    } catch (const boost::exception& ex) {
       ILOG(Error, Support) << "Exception caught: " << boost::current_exception_diagnostic_information(true) << ENDM;
       success = false;
     } catch (const std::exception& ex) {
