993b168c67905ea578a41cd5047d46cb58d29655
Cristina Terrevoli <cristina.terrevoli@cern.ch>
2022-08-01 10:10:38 +0200

    [EMCAL-767] add post processing task for Calib

---
+++ Modules/EMCAL/include/EMCAL/CalibMonitoringTask.h
@@ -0,0 +1,89 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file   CalibMonitoringTask.h
+/// \author Cristina Terrevoli, Markus Fasel
+///
+
+#ifndef QUALITYCONTROL_CALIBMONITORINGTASK_H
+#define QUALITYCONTROL_CALIBMONITORINGTASK_H
+
+// QC includes
+#include "QualityControl/PostProcessingInterface.h"
+#include "EMCALCalib/CalibDB.h"
+#include <memory>
+#include <vector>
+#include <string>
+
+class TCanvas;
+class TPaveText;
+class TH1;
+class TH2;
+
+namespace o2::emcal
+{
+class CalibDB;
+}
+
+namespace o2::quality_control_modules::emcal
+{
+
+/// \brief Quality Control task for the calibration data of the EMCAL
+class CalibMonitoringTask final : public quality_control::postprocessing::PostProcessingInterface
+{
+ public:
+  /// \brief Constructor
+  CalibMonitoringTask() = default;
+  /// \brief Destructor
+  ~CalibMonitoringTask() = default;
+
+  /// \brief Configuration of a post-processing task.
+  /// Configuration of a post-processing task. Can be overridden if user wants to retrieve the configuration of the task.
+  /// \param name     Name of the task
+  /// \param config   ConfigurationInterface with prefix set to ""
+  void configure(std::string name, const boost::property_tree::ptree& config) override;
+  /// \brief Initialization of a post-processing task.
+  /// Initialization of a post-processing task. User receives a Trigger which caused the initialization and a service
+  /// registry with singleton interfaces.
+  /// \param trigger  Trigger which caused the initialization, for example Trigger::SOR
+  /// \param services Interface containing optional interfaces, for example DatabaseInterface
+  void initialize(quality_control::postprocessing::Trigger, framework::ServiceRegistry&) override;
+  /// \brief Update of a post-processing task.
+  /// Update of a post-processing task. User receives a Trigger which caused the update and a service
+  /// registry with singleton interfaces.
+  /// \param trigger  Trigger which caused the initialization, for example Trigger::Period
+  /// \param services Interface containing optional interfaces, for example DatabaseInterface
+  void update(quality_control::postprocessing::Trigger, framework::ServiceRegistry&) override;
+  /// \brief Finalization of a post-processing task.
+  /// Finalization of a post-processing task. User receives a Trigger which caused the finalization and a service
+  /// registry with singleton interfaces.
+  /// \param trigger  Trigger which caused the initialization, for example Trigger::EOR
+  /// \param services Interface containing optional interfaces, for example DatabaseInterface
+  void finalize(quality_control::postprocessing::Trigger, framework::ServiceRegistry&) override;
+
+  void reset();
+
+ public:
+  std::vector<std::string> mCalibObjects;       ///< list of vectors of parm objects to be processed
+  TH1* mTimeCalibParamHisto = nullptr;          ///< Monitor Time Calib Param
+  TH2* mBadChannelMapHisto = nullptr;           ///< Monitor Bad channel map
+  std::unique_ptr<o2::emcal::CalibDB> mCalibDB; ///< EMCAL calibration DB handler
+  o2::emcal::BadChannelMap* mBadChannelMap;     ///< EMCAL channel map
+  o2::emcal::TimeCalibrationParams* mTimeCalib; ///< EMCAL time calib
+
+ private:
+  //  std::vector<std::map<std::string, std::string>> mStoreMaps{};          ///< meta data to be stored with the output in the QCDB
+};
+
+} // namespace o2::quality_control_modules::emcal
+
+#endif //QUALITYCONTROL_CALIBMONITORINGTASK_H
