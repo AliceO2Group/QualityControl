129580feb099ef080a4555892ffdfa7034f79681
Sebastian Bysiak <sebastian.bysiak@gmail.com>
2022-08-30 20:36:23 +0200

    Add GenericCheck to FDD

--- Modules/FDD/include/FDD/LinkDef.h
+++ Modules/FDD/include/FDD/LinkDef.h
@@ -5,6 +5,7 @@
 
 #pragma link C++ class o2::quality_control_modules::fdd::DigitQcTask+;
 #pragma link C++ class o2::quality_control_modules::fdd::DigitQcTaskLaser + ;
+#pragma link C++ class o2::quality_control_modules::fdd::GenericCheck + ;
 #pragma link C++ class o2::quality_control_modules::fdd::RecPointsQcTask + ;
 #pragma link C++ class o2::quality_control_modules::fdd::PostProcTask + ;
 #pragma link C++ class o2::quality_control_modules::fdd::CFDEffCheck + ;
