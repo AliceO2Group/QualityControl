376fc002c78a7958a1e2ab5c09b8ad73787b5fd9
cpuggion84 <90757182+cpuggion84@users.noreply.github.com>
2022-09-07 14:22:16 +0200

    ZDC: Task reconstruction First Version (#1430)
    * ZDC: Task reconstruction First Version
    
    * Update CMakeLists.txt
    
    Add O2::DataFormatsZDC

--- Modules/ZDC/include/ZDC/LinkDef.h
+++ Modules/ZDC/include/ZDC/LinkDef.h
@@ -5,4 +5,5 @@
 
 #pragma link C++ class o2::quality_control_modules::zdc::ZDCRawDataTask+;
 #pragma link C++ class o2::quality_control_modules::zdc::ZDCRawDataCheck+;
+#pragma link C++ class o2::quality_control_modules::zdc::ZDCRecDataTask+;
 #endif
