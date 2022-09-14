c8904f938c3248ff253b31f6c7ecf76bf2650d21
Isakov Artem <isakov@ujf.cas.cz>
2022-09-14 16:45:08 +0200

    ITS FHR online task: filtering of noise pixel vector  (#1438)
    * First commit
    
    * Added correct condition for filtering to avoid pruning before hitmap will be drawn
    
    * Clang changes
    
    * Clang v2
    
    * Ivan's comments
    
    * Clang
    
    * Fixed filling of ThnSparse for hitmap

--- Modules/ITS/include/ITS/ITSFhrTask.h
+++ Modules/ITS/include/ITS/ITSFhrTask.h
@@ -112,8 +112,10 @@ class ITSFhrTask final : public TaskInterface
   int mGetTFFromBinding = 0;
   int mHitCutForNoisyPixel = 1024;        // Hit number cut for noisy pixel, this number should be define according how many TF will be accumulated before reset(one can reference the cycle time)
   float mOccupancyCutForNoisyPixel = 0.1; // Occupancy cut for noisy pixel. check if the hit/event value over this cut. similar with mHitCutForNoisyPixel
-  double mCutTFForSparse = 1;             // cut to stop THnSparse filling after mCutTrgForSparse triggers
-
+  float mPhysicalOccupancyIB = 1.7e-3;
+  float mPhysicalOccupancyOB = 4.3e-5;
+  double mCutTFForSparse = 1; // cut to stop THnSparse filling after mCutTrgForSparse triggers
+  int mDoHitmapFilter;        // do filtering of noise pixel vector
   std::unordered_map<unsigned int, int>*** mHitPixelID_InStave /* = new std::unordered_map<unsigned int, int>**[NStaves[lay]]*/;
   int** mHitnumberLane /* = new int*[NStaves[lay]]*/;       // IB : hitnumber[stave][chip]; OB : hitnumber[stave][lane]
   double** mOccupancyLane /* = new double*[NStaves[lay]]*/; // IB : occupancy[stave][chip]; OB : occupancy[stave][Lane]
