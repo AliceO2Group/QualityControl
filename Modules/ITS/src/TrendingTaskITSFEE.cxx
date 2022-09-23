2da3185e5296ad978748805a3c37ffdd8b8d3713
KotliarovAr <artem.kotliarov@cern.ch>
2022-09-23 15:05:16 +0200

    [WIP] Post-processing trending FEE task

---
+++ Modules/ITS/src/TrendingTaskITSFEE.cxx
@@ -0,0 +1,356 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file    TrendingTaskITSFEE.cxx
+/// \author  Artem Kotliarov on the structure from Piotr Konopka
+///
+
+#include "ITS/TrendingTaskITSFEE.h"
+#include "ITS/ReductorBinContent.h"
+
+#include "QualityControl/RootClassFactory.h"
+#include "QualityControl/DatabaseInterface.h"
+#include "QualityControl/MonitorObject.h"
+#include "QualityControl/QcInfoLogger.h"
+#include "QualityControl/Reductor.h"
+#include "QualityControl/ObjectMetadataKeys.h"
+
+#include <TCanvas.h>
+#include <TH1.h>
+#include <TGraph.h>
+#include <TMultiGraph.h>
+#include <TFile.h>
+#include <TDatime.h>
+#include <map>
+#include <string>
+#include <TTreeReader.h>
+#include <TTreeReaderValue.h>
+#include <TTreeReaderArray.h>
+
+using namespace o2::quality_control;
+using namespace o2::quality_control::core;
+using namespace o2::quality_control::postprocessing;
+using namespace o2::quality_control::repository;
+using namespace o2::quality_control_modules::its;
+
+void TrendingTaskITSFEE::configure(std::string name,
+                                   const boost::property_tree::ptree& config)
+{
+  mConfig = TrendingTaskConfigITS(name, config);
+}
+
+void TrendingTaskITSFEE::initialize(Trigger, framework::ServiceRegistry&)
+{
+  // Preparing data structure of TTree
+  mTrend = std::make_unique<TTree>();
+  mTrend->SetName(PostProcessingInterface::getName().c_str());
+  mTrend->Branch("runNumber", &mMetaData.runNumber);
+  mTrend->Branch("time", &mTime);
+
+  for (const auto& source : mConfig.dataSources) {
+    std::unique_ptr<Reductor> reductor(root_class_factory::create<Reductor>(source.moduleName, source.reductorName));
+    mTrend->Branch(source.name.c_str(), reductor->getBranchAddress(), reductor->getBranchLeafList());
+    mReductors[source.name] = std::move(reductor);
+  }
+}
+
+// todo: see if OptimizeBaskets() indeed helps after some time
+void TrendingTaskITSFEE::update(Trigger t, framework::ServiceRegistry& services)
+{
+  auto& qcdb = services.get<repository::DatabaseInterface>();
+
+  trendValues(t, qcdb);
+  storePlots(qcdb);
+  storeTrend(qcdb);
+}
+
+void TrendingTaskITSFEE::finalize(Trigger t, framework::ServiceRegistry& services)
+{
+  auto& qcdb = services.get<repository::DatabaseInterface>();
+
+  storePlots(qcdb);
+  storeTrend(qcdb);
+}
+
+void TrendingTaskITSFEE::storeTrend(repository::DatabaseInterface& qcdb)
+{
+  auto mo = std::make_shared<core::MonitorObject>(mTrend.get(), getName(), "o2::quality_control_modules::its::TrendingTaskITSFEE",
+                                                  mConfig.detectorName);
+  mo->setIsOwner(false);
+  qcdb.storeMO(mo);
+}
+
+void TrendingTaskITSFEE::trendValues(const Trigger& t, repository::DatabaseInterface& qcdb)
+{
+  // We use current date and time. This for planned processing (not history). We
+  // still might need to use the objects
+  // timestamps in the end, but this would become ambiguous if there is more
+  // than one data source.
+
+  mTime = TDatime().Convert();
+  mMetaData.runNumber = t.activity.mId;
+  int count = 0;
+
+  for (auto& dataSource : mConfig.dataSources) {
+    // todo: make it agnostic to MOs, QOs or other objects. Let the reductor
+    // cast to whatever it needs.
+    if (dataSource.type == "repository") {
+      auto mo = qcdb.retrieveMO(dataSource.path, dataSource.name, t.timestamp, t.activity);
+      if (!count) {
+        std::map<std::string, std::string> entryMetadata = mo->getMetadataMap();  // full list of metadata as a map
+        mMetaData.runNumber = std::stoi(entryMetadata[metadata_keys::runNumber]); // get and set run number
+        runlist.push_back(std::to_string(mMetaData.runNumber));
+      }
+      TObject* obj = mo ? mo->getObject() : nullptr;
+      if (obj)
+        mReductors[dataSource.name]->update(obj);
+    } else {
+      ILOGE << "Unknown type of data source '" << dataSource.type << "'.";
+    }
+    count++;
+  }
+  mTrend->Fill();
+}
+
+void TrendingTaskITSFEE::storePlots(repository::DatabaseInterface& qcdb)
+{
+  // ILOG(Info, Support) << "Generating and storing " << mConfig.plots.size() << " plots." << ENDM;
+
+  int countplots = 0;
+  int countITSpart = 0;
+  bool isRun = false;
+
+  // Define output canvas and legend
+  TCanvas* canvas;
+  TLegend* legend;
+
+  TMultiGraph* multi_trend;
+  TGraph* trend_plot_Flags[nFlags];
+
+  // Retrieve X-axis for trend plot. Two options: "Time" or "Run"
+  long int numberOfEntries = mTrend->GetEntriesFast();
+  mTrend->Draw("time", "", "goff");
+  std::vector<double> retrieveTime(mTrend->GetV1(), mTrend->GetV1() + mTrend->GetSelectedRows() % mTrend->GetEstimate());
+
+  std::vector<double> retrieveRunNumber;
+  for (int j = 1; j <= numberOfEntries; j++)
+    retrieveRunNumber.push_back((double)j);
+
+  // Lane status summary plots
+  for (const auto& plot : mConfig.plots) {
+
+    if (plot.varexp.find("binContent") == std::string::npos)
+      continue;
+
+    // Checks neeeded to segregate plots from input list
+    if (countplots > nFlags - 1) {
+      countplots = 0;
+      countITSpart++;
+    }
+
+    // Retrieve data for trend plot
+    mTrend->Draw(plot.varexp.c_str(), "", "goff");
+    std::vector<double> dataRetrieve(mTrend->GetV1(), mTrend->GetV1() + mTrend->GetSelectedRows() % mTrend->GetEstimate());
+
+    // Initialize MultiGraph and Legend
+    if (countplots == 0) {
+      multi_trend = new TMultiGraph();
+      SetGraphName(multi_trend, plot.name, Form("Lane status summary trend %s", itsParts[countITSpart].c_str()));
+
+      legend = new TLegend(0.93, 0.55, 1.0, 0.9);
+      SetLegendStyle(legend, Form("LaneStatusSummary_%s_legend", itsParts[countITSpart].c_str()));
+
+      isRun = plot.selection.find("Run") != std::string::npos ? true : false;
+    }
+
+    trend_plot_Flags[countplots] = new TGraph(numberOfEntries, isRun ? &retrieveRunNumber[0] : &retrieveTime[0], &dataRetrieve[0]);
+    trend_plot_Flags[countplots]->SetName(plot.name.c_str());
+    SetGraphStyle(trend_plot_Flags[countplots], colors[countplots], markers[countplots]);
+    multi_trend->Add(trend_plot_Flags[countplots], plot.option.c_str());
+
+    legend->AddEntry(trend_plot_Flags[countplots], Form("%s", trend_titles[countplots].c_str()), "pl");
+
+    // Create and save plots
+    if (countplots == nFlags - 1) {
+
+      SetGraphAxes(multi_trend, Form("%s", isRun ? "Run" : "Time"), "Number of lanes", !isRun);
+
+      // Canvas settings
+      std::string name = "LaneStatusSummary_" + itsParts[countITSpart] + "_Trends";
+      canvas = new TCanvas(Form("%s", name.c_str()), Form("%s", name.c_str()));
+      SetCanvasSettings(canvas);
+
+      // Plot as a function of run number requires a dummy TH1 histogram
+      TH1I* hDummy = nullptr;
+      if (isRun) {
+        hDummy = new TH1I("hDummy", Form("%s; %s; %s", multi_trend->GetTitle(), multi_trend->GetXaxis()->GetTitle(), multi_trend->GetYaxis()->GetTitle()), numberOfEntries, 0.5, numberOfEntries + 0.5);
+        SetHistoAxes(hDummy, runlist, multi_trend->GetYaxis()->GetXmin(), multi_trend->GetYaxis()->GetXmax());
+      }
+
+      // Draw plots
+      if (hDummy)
+        hDummy->Draw();
+      multi_trend->Draw(Form("%s", hDummy ? "" : "a"));
+      legend->Draw("SAME");
+
+      // Upload plots
+      auto mo = std::make_shared<MonitorObject>(canvas, mConfig.taskName, "o2::quality_control_modules::its::TrendingTaskITSFEE", mConfig.detectorName);
+      mo->setIsOwner(false);
+      qcdb.storeMO(mo);
+
+      delete legend;
+      delete canvas;
+      delete multi_trend; // All included plots are deleted automatically
+      delete hDummy;
+    }
+    countplots++;
+    dataRetrieve.clear();
+  } // end loop on plots
+
+  //___________________________________________________________________________
+  // Trigger Count trending plot
+  TGraph* trend_plot_Triggers[nTriggers];
+  countplots = 0;
+
+  for (const auto& plot : mConfig.plots) {
+
+    if (plot.varexp.find("integral") == std::string::npos)
+      continue;
+
+    // Retrieve data for trend plot
+    mTrend->Draw(plot.varexp.c_str(), "", "goff");
+    std::vector<double> dataRetrieve(mTrend->GetV1(), mTrend->GetV1() + mTrend->GetSelectedRows() % mTrend->GetEstimate());
+
+    // Initialize MultiGraph and Legend
+    if (countplots == 0) {
+      multi_trend = new TMultiGraph();
+      SetGraphName(multi_trend, plot.name, "Trigger count trend");
+
+      legend = new TLegend(0.93, 0.15, 1.0, 0.9);
+      SetLegendStyle(legend, "Trigger_count_trend");
+      isRun = plot.selection.find("Run") != std::string::npos ? true : false;
+    }
+
+    trend_plot_Triggers[countplots] = new TGraph(numberOfEntries, isRun ? &retrieveRunNumber[0] : &retrieveTime[0], &dataRetrieve[0]);
+    trend_plot_Triggers[countplots]->SetName(plot.name.c_str());
+    SetGraphStyle(trend_plot_Triggers[countplots], colors[countplots], markers[countplots]);
+    multi_trend->Add(trend_plot_Triggers[countplots], plot.option.c_str());
+
+    legend->AddEntry(trend_plot_Triggers[countplots], Form("%s", mTriggerType[countplots].c_str()), "pl");
+    if (countplots == nTriggers - 1) {
+
+      SetGraphAxes(multi_trend, Form("%s", isRun ? "Run" : "Time"), "Integral over all FEE", !isRun);
+
+      // Canvas settings
+      canvas = new TCanvas("Trigger_count_trend", "Trigger_count_trend");
+      SetCanvasSettings(canvas);
+
+      // Plot as a function of run number requires a dummy TH1 histogram
+      TH1I* hDummy = nullptr;
+      if (isRun) {
+        hDummy = new TH1I("hDummy", Form("%s; %s; %s", multi_trend->GetTitle(), multi_trend->GetXaxis()->GetTitle(), multi_trend->GetYaxis()->GetTitle()), numberOfEntries, 0.5, numberOfEntries + 0.5);
+        SetHistoAxes(hDummy, runlist, multi_trend->GetYaxis()->GetXmin(), multi_trend->GetYaxis()->GetXmax());
+      }
+
+      // Draw plots
+      if (hDummy)
+        hDummy->Draw();
+      multi_trend->Draw(Form("%s", hDummy ? "" : "a"));
+      legend->Draw("SAME");
+
+      // Upload plots
+      auto mo = std::make_shared<MonitorObject>(canvas, mConfig.taskName, "o2::quality_control_modules::its::TrendingTaskITSFEE", mConfig.detectorName);
+      mo->setIsOwner(false);
+      qcdb.storeMO(mo);
+
+      delete legend;
+      delete canvas;
+      delete multi_trend; // All included plots are deleted automatically
+      delete hDummy;
+    }
+    countplots++;
+    dataRetrieve.clear();
+  } // end loop on plots
+  retrieveTime.clear();
+  retrieveRunNumber.clear();
+}
+
+void TrendingTaskITSFEE::SetLegendStyle(TLegend* legend, const std::string& name)
+{
+  legend->SetTextFont(42);
+  legend->SetLineColor(kWhite);
+  legend->SetFillColor(0);
+  legend->SetName(Form("%s_legend", name.c_str()));
+  legend->SetFillStyle(0);
+  legend->SetBorderSize(0);
+}
+
+void TrendingTaskITSFEE::SetCanvasSettings(TCanvas* canvas)
+{
+  canvas->SetTickx();
+  canvas->SetTicky();
+  canvas->SetBottomMargin(0.18);
+  canvas->SetRightMargin(0.07);
+}
+
+void TrendingTaskITSFEE::SetGraphStyle(TGraph* graph, int col, int mkr)
+{
+  graph->SetLineColor(col);
+  graph->SetMarkerStyle(mkr);
+  graph->SetMarkerColor(col);
+  graph->SetMarkerSize(1.0);
+  graph->SetLineWidth(1);
+}
+
+void TrendingTaskITSFEE::SetGraphName(TMultiGraph* graph, std::string name, std::string title)
+{
+  graph->SetTitle(title.c_str());
+  graph->SetName(name.c_str());
+}
+
+void TrendingTaskITSFEE::SetGraphAxes(TMultiGraph* graph, std::string xtitle,
+                                      std::string ytitle, bool isTime)
+{
+  graph->GetXaxis()->SetTitle(xtitle.c_str());
+  graph->GetXaxis()->SetTitleSize(0.045);
+
+  graph->GetYaxis()->SetTitle(ytitle.c_str());
+  graph->GetYaxis()->SetTitleSize(0.045);
+
+  graph->GetXaxis()->SetNdivisions(505);
+  graph->GetXaxis()->SetTimeOffset(0.0);
+  graph->GetXaxis()->SetLabelOffset(0.02);
+  graph->GetXaxis()->SetLabelSize(0.033);
+  graph->GetXaxis()->SetTitleOffset(2.3);
+  if (isTime) {
+    graph->GetXaxis()->SetTimeFormat("#splitline{%d.%m.%y}{%H:%M}");
+    graph->GetXaxis()->SetTimeDisplay(1);
+  }
+}
+
+void TrendingTaskITSFEE::SetHistoAxes(TH1* hist, std::vector<std::string> runlist,
+                                      const double& Ymin, const double& Ymax)
+{
+  hist->SetStats(0);
+  hist->GetXaxis()->SetTitleSize(0.045);
+  hist->GetYaxis()->SetTitleSize(0.045);
+  hist->GetYaxis()->SetRangeUser(Ymin, Ymax);
+
+  hist->GetXaxis()->SetLabelOffset(0.02);
+  hist->GetXaxis()->SetLabelSize(0.033);
+  hist->GetXaxis()->SetTitleOffset(2.3);
+  hist->GetXaxis()->SetNdivisions(-505);
+
+  for (int iX = 0; iX < runlist.size(); iX++) {
+    hist->GetXaxis()->SetBinLabel(iX + 1, runlist[iX].c_str());
+  }
+}
