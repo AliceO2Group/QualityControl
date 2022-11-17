5fd7e53997d4b2938b4704104d6aa3c0d921e671
JianLIUhep <jian@cern.ch>
2022-11-17 14:34:03 +0100

    its decoding error check

--- Modules/ITS/src/ITSDecodingErrorTask.cxx
+++ Modules/ITS/src/ITSDecodingErrorTask.cxx
@@ -36,6 +36,14 @@ ITSDecodingErrorTask::~ITSDecodingErrorTask()
   delete mChipErrorPlots;
   delete mLinkErrorVsFeeid;
   delete mChipErrorVsFeeid;
+  for (int ilayer = 0; ilayer < 7; ilayer++) {
+    delete[] mChipErrorCount[ilayer];
+    delete mChipErrorVsChipid[ilayer];
+    for (int ichip = 0; ichip < ChipBoundary[ilayer + 1] - ChipBoundary[ilayer]; ichip++) {
+      delete[] mChipErrorCount[ilayer][ichip];
+    }
+  }
+  delete[] mChipErrorCount;
 }
 
 void ITSDecodingErrorTask::initialize(o2::framework::InitContext& /*ctx*/)
@@ -44,6 +52,17 @@ void ITSDecodingErrorTask::initialize(o2::framework::InitContext& /*ctx*/)
   getParameters();
   createDecodingPlots();
   setPlotsFormat();
+
+  mChipErrorCount = new int**[7];
+  for (int ilayer = 0; ilayer < 7; ilayer++) {
+    mChipErrorCount[ilayer] = new int*[ChipBoundary[ilayer + 1] - ChipBoundary[ilayer]];
+    for (int ichip = 0; ichip < ChipBoundary[ilayer + 1] - ChipBoundary[ilayer]; ichip++) {
+      mChipErrorCount[ilayer][ichip] = new int[o2::itsmft::ChipStat::NErrorsDefined];
+      for (int ierror = 0; ierror < o2::itsmft::ChipStat::NErrorsDefined; ierror++) {
+        mChipErrorCount[ilayer][ichip][ierror] = 0;
+      }
+    }
+  }
 }
 
 void ITSDecodingErrorTask::createDecodingPlots()
@@ -52,16 +71,24 @@ void ITSDecodingErrorTask::createDecodingPlots()
   mLinkErrorVsFeeid->SetMinimum(0);
   mLinkErrorVsFeeid->SetStats(0);
   getObjectsManager()->startPublishing(mLinkErrorVsFeeid);
+  for (int ilayer = 0; ilayer < 7; ilayer++) {
+    mChipErrorVsChipid[ilayer] = new TH2D(Form("General/Layer%dChipErrorVsChipid", ilayer), Form("Layer%d Chip errors per FeeId", ilayer), ChipBoundary[ilayer + 1] - ChipBoundary[ilayer], 0, ChipBoundary[ilayer + 1] - ChipBoundary[ilayer], o2::itsmft::ChipStat::NErrorsDefined, 0.5, o2::itsmft::ChipStat::NErrorsDefined + 0.5);
+    mChipErrorVsChipid[ilayer]->SetMinimum(0);
+    mChipErrorVsChipid[ilayer]->SetStats(0);
+    getObjectsManager()->startPublishing(mChipErrorVsChipid[ilayer]);
+  }
   mChipErrorVsFeeid = new TH2I("General/ChipErrorVsFeeid", "Chip decoding errors per FeeId", NFees, 0, NFees, o2::itsmft::ChipStat::NErrorsDefined, 0.5, o2::itsmft::ChipStat::NErrorsDefined + 0.5);
   mChipErrorVsFeeid->SetMinimum(0);
   mChipErrorVsFeeid->SetStats(0);
   getObjectsManager()->startPublishing(mChipErrorVsFeeid);
   mLinkErrorPlots = new TH1D("General/LinkErrorPlots", "GBTLink decoding Errors", o2::itsmft::GBTLinkDecodingStat::NErrorsDefined, 0.5, o2::itsmft::GBTLinkDecodingStat::NErrorsDefined + 0.5);
   mLinkErrorPlots->SetMinimum(0);
+  mLinkErrorPlots->SetStats(0);
   mLinkErrorPlots->SetFillColor(kRed);
   getObjectsManager()->startPublishing(mLinkErrorPlots); // mLinkErrorPlots
   mChipErrorPlots = new TH1D("General/ChipErrorPlots", "Chip Decoding Errors", o2::itsmft::ChipStat::NErrorsDefined, 0.5, o2::itsmft::ChipStat::NErrorsDefined + 0.5);
   mChipErrorPlots->SetMinimum(0);
+  mChipErrorPlots->SetStats(0);
   mChipErrorPlots->SetFillColor(kRed);
   getObjectsManager()->startPublishing(mChipErrorPlots); // mChipErrorPlots
 }
@@ -79,6 +106,11 @@ void ITSDecodingErrorTask::setPlotsFormat()
   }
   if (mChipErrorVsFeeid) {
     setAxisTitle(mChipErrorVsFeeid, "FeeID", "Error ID");
+  }
+  for (int ilayer = 0; ilayer < 7; ilayer++) {
+    if (mChipErrorVsChipid[ilayer]) {
+      setAxisTitle(mChipErrorVsChipid[ilayer], "ChipID", "Error ID");
+    }
   }
   if (mLinkErrorPlots) {
     setAxisTitle(mLinkErrorPlots, "LinkError ID", "Counts");
@@ -106,6 +138,13 @@ void ITSDecodingErrorTask::monitorData(o2::framework::ProcessingContext& ctx)
 
   auto linkErrors = ctx.inputs().get<gsl::span<o2::itsmft::GBTLinkDecodingStat>>("linkerrors");
   auto decErrors = ctx.inputs().get<gsl::span<o2::itsmft::ChipError>>("decerrors");
+
+  // multiply Error distributions before re-filling
+  mLinkErrorVsFeeid->Scale((double)mTFCount);
+  mChipErrorVsFeeid->Scale((double)mTFCount);
+  for (int ilayer = 0; ilayer < 7; ilayer++) {
+    mChipErrorVsChipid[ilayer]->Scale((double)mTFCount);
+  }
   for (const auto& le : linkErrors) {
     int istave = (int)(le.feeID & 0x00ff);
     int ilink = (int)((le.feeID & 0x0f00) >> 8);
@@ -123,9 +162,14 @@ void ITSDecodingErrorTask::monitorData(o2::framework::ProcessingContext& ctx)
     int ilink = (int)((de.getFEEID() & 0x0f00) >> 8);
     int ilayer = (int)((de.getFEEID() & 0xf000) >> 12);
     int ifee = 3 * StaveBoundary[ilayer] - (StaveBoundary[ilayer] - StaveBoundary[NLayerIB]) * (ilayer >= NLayerIB) + istave * (3 - (ilayer >= NLayerIB)) + ilink;
+    int ichip = de.getChipID() - ChipBoundary[ilayer];
     for (int ierror = 0; ierror < o2::itsmft::ChipStat::NErrorsDefined; ierror++) {
       if ((de.errors >> ierror) % 2) {
+        if (de.getChipID() == -1) {
+          continue;
+        }
         mChipErrorVsFeeid->Fill(ifee + 1, ierror + 1);
+        mChipErrorCount[ilayer][ichip][ierror]++;
       }
     }
   }
@@ -137,7 +181,25 @@ void ITSDecodingErrorTask::monitorData(o2::framework::ProcessingContext& ctx)
     int feeChipError = mChipErrorVsFeeid->Integral(1, mChipErrorVsFeeid->GetXaxis()->GetNbins(), ierror + 1, ierror + 1);
     mChipErrorPlots->SetBinContent(ierror + 1, feeChipError);
   }
+
+  for (int ilayer = 0; ilayer < 7; ilayer++) {
+    for (int ichip = 0; ichip < ChipBoundary[ilayer + 1] - ChipBoundary[ilayer]; ichip++) {
+      for (int ierror = 0; ierror < o2::itsmft::ChipStat::NErrorsDefined; ierror++) {
+        mChipErrorVsChipid[ilayer]->SetBinContent(ichip + 1, ierror + 1, mChipErrorCount[ilayer][ichip][ierror]);
+      }
+    }
+  }
+
   end = std::chrono::high_resolution_clock::now();
+  mTFCount++; // Number of TF
+  // Scale error distributions by latest number of TF
+  if (mTFCount > 0) {
+    mLinkErrorVsFeeid->Scale(1. / (double)mTFCount);
+    mChipErrorVsFeeid->Scale(1. / (double)mTFCount);
+    for (int ilayer = 0; ilayer < 7; ilayer++) {
+      mChipErrorVsChipid[ilayer]->Scale(1. / (double)mTFCount);
+    }
+  }
 }
 
 void ITSDecodingErrorTask::getParameters()
@@ -160,11 +222,21 @@ void ITSDecodingErrorTask::resetGeneralPlots()
   mChipErrorVsFeeid->Reset();
   mLinkErrorPlots->Reset();
   mChipErrorPlots->Reset();
+  for (int ilayer = 0; ilayer < 7; ilayer++) {
+    mChipErrorVsChipid[ilayer]->Reset();
+  }
 }
 
 void ITSDecodingErrorTask::reset()
 {
   resetGeneralPlots();
+  for (int ilayer = 0; ilayer < 7; ilayer++) {
+    for (int ichip = 0; ichip < ChipBoundary[ilayer + 1] - ChipBoundary[ilayer]; ichip++) {
+      for (int ierror = 0; ierror < o2::itsmft::ChipStat::NErrorsDefined; ierror++) {
+        mChipErrorCount[ilayer][ichip][ierror] = 0;
+      }
+    }
+  }
   ILOG(Info, Support) << "Reset" << ENDM;
 }
 
