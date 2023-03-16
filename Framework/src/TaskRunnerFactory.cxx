27057aa95f63936450a01ba9c878832fac0c80d9
Barthelemy <barthelemy.von.haller@cern.ch>
2023-03-16 10:44:24 +0100

    comment

--- Framework/src/TaskRunnerFactory.cxx
+++ Framework/src/TaskRunnerFactory.cxx
@@ -161,10 +161,12 @@ InputSpec TaskRunnerFactory::createTimerInputSpec(const CommonSpec& globalConfig
 {
   // This is to check that the durations are not below 10 seconds except when using a dummy database
   auto dummyDatabaseUsed = globalConfig.database.count("implementation") > 0 && globalConfig.database.at("implementation") == "Dummy";
-  for (auto& [cycleDuration, validity] : cycleDurations) {
-    if (!dummyDatabaseUsed && cycleDuration < 10) {
-      ILOG(Error, Support) << "Cycle duration is too short (" << cycleDuration << "), replaced by a duration of 10 seconds." << ENDM;
-      cycleDuration = 10;
+  if(!dummyDatabaseUsed) {
+    for (auto& [cycleDuration, validity] : cycleDurations) {
+      if (cycleDuration < 10) {
+        ILOG(Error, Support) << "Cycle duration is too short (" << cycleDuration << "), replaced by a duration of 10 seconds." << ENDM;
+        cycleDuration = 10;
+      }
     }
   }
 
