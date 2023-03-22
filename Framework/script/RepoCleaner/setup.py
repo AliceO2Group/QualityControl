4a9b539918fba8e7f470c44263c57786074ba5d5
Barthélémy von Haller <barthelemy.von.haller@gmail.com>
2023-03-22 14:05:24 +0100

    repocleaner 1.4

--- Framework/script/RepoCleaner/setup.py
+++ Framework/script/RepoCleaner/setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='qcrepocleaner',
-    version='1.3',
+    version='1.4',
     author='Barthelemy von Haller',
     author_email='bvonhall@cern.ch',
     url='https://gitlab.cern.ch/AliceO2Group/QualityControl/Framework/script/RepoCleaner',
