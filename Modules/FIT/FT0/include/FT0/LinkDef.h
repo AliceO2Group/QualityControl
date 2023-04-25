77072a9c39ea8e4bbb842f84d8769b5a83945148
Artur Furs <9881239+afurs@users.noreply.github.com>
2023-04-25 13:13:56 +0300

    FT0: refactoring + new tasks and features (#1743)
    * FT0: refactoring and new features + new check task
    
    * Update PostProcTask.cxx
    
    * Update PostProcTask.cxx
    
    * Update DigitQcTask.cxx

--- Modules/FIT/FT0/include/FT0/LinkDef.h
+++ Modules/FIT/FT0/include/FT0/LinkDef.h
@@ -5,17 +5,12 @@
 
 #pragma link C++ class o2::quality_control_modules::ft0::EventWithChannelData + ;
 
-#pragma link C++ class o2::quality_control_modules::ft0::BasicDigitQcTask + ;
 #pragma link C++ class o2::quality_control_modules::ft0::DigitQcTask + ;
-#pragma link C++ class o2::quality_control_modules::ft0::DigitsCheck + ;
-#pragma link C++ class o2::quality_control_modules::ft0::ChannelsCheck + ;
 #pragma link C++ class o2::quality_control_modules::ft0::MergedTreeCheck + ;
-#pragma link C++ class o2::quality_control_modules::ft0::TreeReaderPostProcessing + ;
-//#pragma link C++ class o2::quality_control_modules::ft0::CalibrationTask + ;
-//#pragma link C++ class o2::quality_control_modules::ft0::ChannelTimeCalibrationCheck + ;
 #pragma link C++ class o2::quality_control_modules::ft0::PostProcTask + ;
-#pragma link C++ class o2::quality_control_modules::ft0::GenericCheck+;
+#pragma link C++ class o2::quality_control_modules::ft0::GenericCheck + ;
 #pragma link C++ class o2::quality_control_modules::ft0::CFDEffCheck + ;
+#pragma link C++ class o2::quality_control_modules::ft0::FractionCheck + ;
 #pragma link C++ class o2::quality_control_modules::ft0::OutOfBunchCollCheck + ;
 #pragma link C++ class o2::quality_control_modules::ft0::RecPointsQcTask + ;
 
