4df43270661623d22c01f65b97b0309da1a1afc4
Barthelemy <barthelemy.von.haller@cern.ch>
2023-04-25 15:07:22 +0200

    making the check expendable

--- Framework/src/CheckRunnerFactory.cxx
+++ Framework/src/CheckRunnerFactory.cxx
@@ -45,6 +45,8 @@ DataProcessorSpec CheckRunnerFactory::create(CheckRunnerConfig checkRunnerConfig
                                     options };
   newCheckRunner.labels.emplace_back(o2::framework::ecs::qcReconfigurable);
   newCheckRunner.labels.emplace_back(CheckRunner::getCheckRunnerLabel());
+  framework::DataProcessorLabel expendableLabel = {"expendable"};
+  newCheckRunner.labels.emplace_back(expendableLabel);
   newCheckRunner.algorithm = adaptFromTask<CheckRunner>(std::move(qcCheckRunner));
   return newCheckRunner;
 }
@@ -61,7 +63,8 @@ DataProcessorSpec CheckRunnerFactory::createSinkDevice(const CheckRunnerConfig& 
                                     checkRunnerConfig.options,
                                     {},
                                     { o2::framework::ecs::qcReconfigurable } };
-
+  framework::DataProcessorLabel expendableLabel = {"expendable"};
+  newCheckRunner.labels.emplace_back(expendableLabel);
   return newCheckRunner;
 }
 
