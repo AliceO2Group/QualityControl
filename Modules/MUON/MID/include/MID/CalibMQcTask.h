0551c1ce96073179cb7ec4268f3fc8d90d454af9
dstocco <diego.stocco@cern.ch>
2023-04-28 17:52:49 +0200

    Use MID digits helper in CalibMQCTask

--- Modules/MUON/MID/include/MID/CalibMQcTask.h
+++ Modules/MUON/MID/include/MID/CalibMQcTask.h
@@ -17,15 +17,15 @@
 #define QC_MODULE_MID_MIDCALIBMQCTASK_H
 
 #include "QualityControl/TaskInterface.h"
-#include "MIDRaw/CrateMasks.h"
-#include "MIDRaw/Decoder.h"
-#include "MIDRaw/ElectronicsDelay.h"
-#include "MIDRaw/FEEIdConfig.h"
-#include "MIDBase/Mapping.h"
+
+#include <array>
+#include <memory>
+#include "MID/DigitsHelper.h"
 
 class TH1F;
 class TH2F;
-class TProfile2D;
+class TH1F;
+class TH2F;
 
 using namespace o2::quality_control::core;
 
@@ -52,67 +52,20 @@ class CalibMQcTask final : public TaskInterface
   void reset() override;
 
  private:
-  ///////////////////////////
-  int mTF = 0;
-  int mNoiseROF = 0;
-  int mDeadROF = 0;
-  int mBadROF = 0;
+  void resetDisplayHistos();
+  DigitsHelper mDigitsHelper; ///! Digits helper
 
-  o2::mid::Mapping mMapping; ///< Mapping
+  std::unique_ptr<TH1F> mNoise{ nullptr };
+  std::array<std::unique_ptr<TH2F>, 4> mBendNoiseMap{};
+  std::array<std::unique_ptr<TH2F>, 4> mNBendNoiseMap{};
 
-  std::shared_ptr<TH1F> mMMultNoiseMT11B{ nullptr };
-  std::shared_ptr<TH1F> mMMultNoiseMT11NB{ nullptr };
-  std::shared_ptr<TH1F> mMMultNoiseMT12B{ nullptr };
-  std::shared_ptr<TH1F> mMMultNoiseMT12NB{ nullptr };
-  std::shared_ptr<TH1F> mMMultNoiseMT21B{ nullptr };
-  std::shared_ptr<TH1F> mMMultNoiseMT21NB{ nullptr };
-  std::shared_ptr<TH1F> mMMultNoiseMT22B{ nullptr };
-  std::shared_ptr<TH1F> mMMultNoiseMT22NB{ nullptr };
+  std::unique_ptr<TH1F> mDead{ nullptr };
+  std::array<std::unique_ptr<TH2F>, 4> mBendDeadMap{};
+  std::array<std::unique_ptr<TH2F>, 4> mNBendDeadMap{};
 
-  std::shared_ptr<TH2I> mMBendNoiseMap11{ nullptr };
-  std::shared_ptr<TH2I> mMBendNoiseMap12{ nullptr };
-  std::shared_ptr<TH2I> mMBendNoiseMap21{ nullptr };
-  std::shared_ptr<TH2I> mMBendNoiseMap22{ nullptr };
-  std::shared_ptr<TH2I> mMNBendNoiseMap11{ nullptr };
-  std::shared_ptr<TH2I> mMNBendNoiseMap12{ nullptr };
-  std::shared_ptr<TH2I> mMNBendNoiseMap21{ nullptr };
-  std::shared_ptr<TH2I> mMNBendNoiseMap22{ nullptr };
-
-  std::shared_ptr<TH1F> mMMultDeadMT11B{ nullptr };
-  std::shared_ptr<TH1F> mMMultDeadMT11NB{ nullptr };
-  std::shared_ptr<TH1F> mMMultDeadMT12B{ nullptr };
-  std::shared_ptr<TH1F> mMMultDeadMT12NB{ nullptr };
-  std::shared_ptr<TH1F> mMMultDeadMT21B{ nullptr };
-  std::shared_ptr<TH1F> mMMultDeadMT21NB{ nullptr };
-  std::shared_ptr<TH1F> mMMultDeadMT22B{ nullptr };
-  std::shared_ptr<TH1F> mMMultDeadMT22NB{ nullptr };
-
-  std::shared_ptr<TH2I> mMBendDeadMap11{ nullptr };
-  std::shared_ptr<TH2I> mMBendDeadMap12{ nullptr };
-  std::shared_ptr<TH2I> mMBendDeadMap21{ nullptr };
-  std::shared_ptr<TH2I> mMBendDeadMap22{ nullptr };
-  std::shared_ptr<TH2I> mMNBendDeadMap11{ nullptr };
-  std::shared_ptr<TH2I> mMNBendDeadMap12{ nullptr };
-  std::shared_ptr<TH2I> mMNBendDeadMap21{ nullptr };
-  std::shared_ptr<TH2I> mMNBendDeadMap22{ nullptr };
-
-  std::shared_ptr<TH1F> mMMultBadMT11B{ nullptr };
-  std::shared_ptr<TH1F> mMMultBadMT11NB{ nullptr };
-  std::shared_ptr<TH1F> mMMultBadMT12B{ nullptr };
-  std::shared_ptr<TH1F> mMMultBadMT12NB{ nullptr };
-  std::shared_ptr<TH1F> mMMultBadMT21B{ nullptr };
-  std::shared_ptr<TH1F> mMMultBadMT21NB{ nullptr };
-  std::shared_ptr<TH1F> mMMultBadMT22B{ nullptr };
-  std::shared_ptr<TH1F> mMMultBadMT22NB{ nullptr };
-
-  std::shared_ptr<TH2I> mMBendBadMap11{ nullptr };
-  std::shared_ptr<TH2I> mMBendBadMap12{ nullptr };
-  std::shared_ptr<TH2I> mMBendBadMap21{ nullptr };
-  std::shared_ptr<TH2I> mMBendBadMap22{ nullptr };
-  std::shared_ptr<TH2I> mMNBendBadMap11{ nullptr };
-  std::shared_ptr<TH2I> mMNBendBadMap12{ nullptr };
-  std::shared_ptr<TH2I> mMNBendBadMap21{ nullptr };
-  std::shared_ptr<TH2I> mMNBendBadMap22{ nullptr };
+  std::unique_ptr<TH1F> mBad{ nullptr };
+  std::array<std::unique_ptr<TH2F>, 4> mBendBadMap{};
+  std::array<std::unique_ptr<TH2F>, 4> mNBendBadMap{};
 };
 
 } // namespace o2::quality_control_modules::mid
