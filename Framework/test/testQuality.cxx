71d62156fe7513fffdfe245ee6a9fc95214a7ac7
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2023-05-24 15:40:06 +0200

    Few more tests moved to Catch2

--- Framework/test/testQuality.cxx
+++ Framework/test/testQuality.cxx
@@ -16,47 +16,43 @@
 
 #include "QualityControl/Quality.h"
 #include "QualityControl/QcInfoLogger.h"
-
-#define BOOST_TEST_MODULE Quality test
-#define BOOST_TEST_MAIN
-#define BOOST_TEST_DYN_LINK
-#include <boost/test/unit_test.hpp>
+#include <catch_amalgamated.hpp>
 
 using namespace std;
 
 namespace o2::quality_control::core
 {
 
-BOOST_AUTO_TEST_CASE(quality_test)
+TEST_CASE("quality_test")
 {
   Quality asdf(123, "asdf");
   Quality myQuality = Quality::Bad;
-  BOOST_CHECK_EQUAL(myQuality.getLevel(), 3);
-  BOOST_CHECK_EQUAL(myQuality.getName(), "Bad");
+  CHECK(myQuality.getLevel() == 3);
+  CHECK(myQuality.getName() == "Bad");
   myQuality = Quality::Good;
-  BOOST_CHECK_EQUAL(myQuality.getLevel(), 1);
-  BOOST_CHECK_EQUAL(myQuality.getName(), "Good");
+  CHECK(myQuality.getLevel() == 1);
+  CHECK(myQuality.getName() == "Good");
   myQuality = Quality::Medium;
-  BOOST_CHECK_EQUAL(myQuality.getLevel(), 2);
-  BOOST_CHECK_EQUAL(myQuality.getName(), "Medium");
+  CHECK(myQuality.getLevel() == 2);
+  CHECK(myQuality.getName() == "Medium");
   myQuality = Quality::Null;
-  BOOST_CHECK_EQUAL(myQuality.getLevel(), Quality::NullLevel);
-  BOOST_CHECK_EQUAL(myQuality.getName(), "Null");
+  CHECK(myQuality.getLevel() == Quality::NullLevel);
+  CHECK(myQuality.getName() == "Null");
 
   ILOG(Info, Support) << "test quality output : " << myQuality << ENDM;
 
-  BOOST_CHECK(Quality::Bad.isWorseThan(Quality::Medium));
-  BOOST_CHECK(Quality::Bad.isWorseThan(Quality::Good));
-  BOOST_CHECK(!Quality::Bad.isWorseThan(Quality::Null));
-  BOOST_CHECK(!Quality::Bad.isWorseThan(Quality::Bad));
+  CHECK(Quality::Bad.isWorseThan(Quality::Medium));
+  CHECK(Quality::Bad.isWorseThan(Quality::Good));
+  CHECK(!Quality::Bad.isWorseThan(Quality::Null));
+  CHECK(!Quality::Bad.isWorseThan(Quality::Bad));
 
-  BOOST_CHECK(Quality::Good.isBetterThan(Quality::Medium));
-  BOOST_CHECK(Quality::Good.isBetterThan(Quality::Bad));
-  BOOST_CHECK(Quality::Good.isBetterThan(Quality::Null));
-  BOOST_CHECK(!Quality::Good.isBetterThan(Quality::Good));
+  CHECK(Quality::Good.isBetterThan(Quality::Medium));
+  CHECK(Quality::Good.isBetterThan(Quality::Bad));
+  CHECK(Quality::Good.isBetterThan(Quality::Null));
+  CHECK(!Quality::Good.isBetterThan(Quality::Good));
 }
 
-BOOST_AUTO_TEST_CASE(quality_reasons)
+TEST_CASE("quality_reasons")
 {
   Quality myQuality = Quality::Bad;
   myQuality.addReason(FlagReasonFactory::BadTracking(), "exception in x");
@@ -64,21 +60,21 @@ BOOST_AUTO_TEST_CASE(quality_reasons)
   myQuality.addReason(FlagReasonFactory::LimitedAcceptance(), "sector C off");
 
   auto myReasons = myQuality.getReasons();
-  BOOST_CHECK_EQUAL(myReasons[0].first, FlagReasonFactory::BadTracking());
-  BOOST_CHECK_EQUAL(myReasons[0].second, "exception in x");
-  BOOST_CHECK_EQUAL(myReasons[1].first, FlagReasonFactory::BadTracking());
-  BOOST_CHECK_EQUAL(myReasons[1].second, "exception in y");
-  BOOST_CHECK_EQUAL(myReasons[2].first, FlagReasonFactory::LimitedAcceptance());
-  BOOST_CHECK_EQUAL(myReasons[2].second, "sector C off");
+  CHECK(myReasons[0].first == FlagReasonFactory::BadTracking());
+  CHECK(myReasons[0].second == "exception in x");
+  CHECK(myReasons[1].first == FlagReasonFactory::BadTracking());
+  CHECK(myReasons[1].second == "exception in y");
+  CHECK(myReasons[2].first == FlagReasonFactory::LimitedAcceptance());
+  CHECK(myReasons[2].second == "sector C off");
 
   auto copyQuality = myQuality;
   auto copyReasons = copyQuality.getReasons();
-  BOOST_CHECK_EQUAL(copyReasons[0].first, FlagReasonFactory::BadTracking());
-  BOOST_CHECK_EQUAL(copyReasons[0].second, "exception in x");
-  BOOST_CHECK_EQUAL(copyReasons[1].first, FlagReasonFactory::BadTracking());
-  BOOST_CHECK_EQUAL(copyReasons[1].second, "exception in y");
-  BOOST_CHECK_EQUAL(copyReasons[2].first, FlagReasonFactory::LimitedAcceptance());
-  BOOST_CHECK_EQUAL(copyReasons[2].second, "sector C off");
+  CHECK(copyReasons[0].first == FlagReasonFactory::BadTracking());
+  CHECK(copyReasons[0].second == "exception in x");
+  CHECK(copyReasons[1].first == FlagReasonFactory::BadTracking());
+  CHECK(copyReasons[1].second == "exception in y");
+  CHECK(copyReasons[2].first == FlagReasonFactory::LimitedAcceptance());
+  CHECK(copyReasons[2].second == "sector C off");
 }
 
 } // namespace o2::quality_control::core
--- Framework/test/testTriggerHelpers.cxx
+++ Framework/test/testTriggerHelpers.cxx
@@ -16,142 +16,137 @@
 
 #include "QualityControl/TriggerHelpers.h"
 #include "QualityControl/PostProcessingConfig.h"
-
-#define BOOST_TEST_MODULE TriggerHelpers test
-#define BOOST_TEST_MAIN
-#define BOOST_TEST_DYN_LINK
-
-#include <boost/test/unit_test.hpp>
+#include <catch_amalgamated.hpp>
 
 using namespace o2::quality_control::postprocessing;
 const std::string CCDB_ENDPOINT = "ccdb-test.cern.ch:8080";
 
-BOOST_AUTO_TEST_CASE(test_factory)
+TEST_CASE("test_factory")
 {
   PostProcessingConfig dummyConfig;
   // check if it ignores letter case
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("once", dummyConfig));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("Once", dummyConfig));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("ONCE", dummyConfig));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("once", dummyConfig));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("Once", dummyConfig));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("ONCE", dummyConfig));
 
   // check if it creates correct triggers
   auto once = trigger_helpers::triggerFactory("once", dummyConfig);
-  BOOST_CHECK_EQUAL(once(), TriggerType::Once);
-  BOOST_CHECK_EQUAL(once(), TriggerType::No);
-  BOOST_CHECK_EQUAL(once(), TriggerType::No);
-  BOOST_CHECK_EQUAL(once(), TriggerType::No);
-  BOOST_CHECK_EQUAL(once(), TriggerType::No);
+  CHECK(once() == TriggerType::Once);
+  CHECK(once() == TriggerType::No);
+  CHECK(once() == TriggerType::No);
+  CHECK(once() == TriggerType::No);
+  CHECK(once() == TriggerType::No);
   auto always = trigger_helpers::triggerFactory("always", dummyConfig);
-  BOOST_CHECK_EQUAL(always(), TriggerType::Always);
-  BOOST_CHECK_EQUAL(always(), TriggerType::Always);
-  BOOST_CHECK_EQUAL(always(), TriggerType::Always);
-  BOOST_CHECK_EQUAL(always(), TriggerType::Always);
-  BOOST_CHECK_EQUAL(always(), TriggerType::Always);
+  CHECK(always() == TriggerType::Always);
+  CHECK(always() == TriggerType::Always);
+  CHECK(always() == TriggerType::Always);
+  CHECK(always() == TriggerType::Always);
+  CHECK(always() == TriggerType::Always);
 
   // unknown trigger
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("adsfzxcvadsf", dummyConfig), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("", dummyConfig), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("adsfzxcvadsf", dummyConfig), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("", dummyConfig), std::invalid_argument);
 
   // generating periodic trigger
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("1sec", dummyConfig));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("1.23sec", dummyConfig));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("123 seconds", dummyConfig));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("2min", dummyConfig));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("2mins", dummyConfig));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("2mins", dummyConfig));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("2minutes", dummyConfig));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("3hour", dummyConfig));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("3hours", dummyConfig));
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("-1sec", dummyConfig), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("sec", dummyConfig), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("asec", dummyConfig), std::invalid_argument);
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("1sec", dummyConfig));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("1.23sec", dummyConfig));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("123 seconds", dummyConfig));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("2min", dummyConfig));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("2mins", dummyConfig));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("2mins", dummyConfig));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("2minutes", dummyConfig));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("3hour", dummyConfig));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("3hours", dummyConfig));
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("-1sec", dummyConfig), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("sec", dummyConfig), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("asec", dummyConfig), std::invalid_argument);
 
   // generating new object trigger
   PostProcessingConfig configWithDBs;
   configWithDBs.qcdbUrl = CCDB_ENDPOINT;
   configWithDBs.ccdbUrl = CCDB_ENDPOINT;
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("newobject:qcdb:qc/asdf/vcxz", configWithDBs));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("newobject:ccdb:qc/asdf/vcxz", configWithDBs));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("newobject:QCDB:qc/asdf/vcxz", configWithDBs));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("newobject:CCDB:qc/asdf/vcxz", configWithDBs));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("NewObject:QcDb:qc/asdf/vcxz", configWithDBs));
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("newobject", configWithDBs), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("newobject:", configWithDBs), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("newobject::", configWithDBs), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("newobject::qc/no/db/specified", configWithDBs), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("newobject:nodb:qc/incorrect/db/speficied", configWithDBs), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("newobject:ccdb:qc/too:many tokens", configWithDBs), std::invalid_argument);
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("newobject:qcdb:qc/asdf/vcxz", configWithDBs));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("newobject:ccdb:qc/asdf/vcxz", configWithDBs));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("newobject:QCDB:qc/asdf/vcxz", configWithDBs));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("newobject:CCDB:qc/asdf/vcxz", configWithDBs));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("NewObject:QcDb:qc/asdf/vcxz", configWithDBs));
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("newobject", configWithDBs), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("newobject:", configWithDBs), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("newobject::", configWithDBs), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("newobject::qc/no/db/specified", configWithDBs), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("newobject:nodb:qc/incorrect/db/speficied", configWithDBs), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("newobject:ccdb:qc/too:many tokens", configWithDBs), std::invalid_argument);
 
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("foreachobject:qcdb:qc/asdf/vcxz", configWithDBs));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("foreachobject:ccdb:qc/asdf/vcxz", configWithDBs));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("foreachobject:QCDB:qc/asdf/vcxz", configWithDBs));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("foreachobject:CCDB:qc/asdf/vcxz", configWithDBs));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("ForEachObject:QcDb:qc/asdf/vcxz", configWithDBs));
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("foreachobject", configWithDBs), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("foreachobject:", configWithDBs), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("foreachobject::", configWithDBs), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("foreachobject::qc/no/db/specified", configWithDBs), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("foreachobject:nodb:qc/incorrect/db/speficied", configWithDBs), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("foreachobject:ccdb:qc/too:many tokens", configWithDBs), std::invalid_argument);
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("foreachobject:qcdb:qc/asdf/vcxz", configWithDBs));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("foreachobject:ccdb:qc/asdf/vcxz", configWithDBs));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("foreachobject:QCDB:qc/asdf/vcxz", configWithDBs));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("foreachobject:CCDB:qc/asdf/vcxz", configWithDBs));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("ForEachObject:QcDb:qc/asdf/vcxz", configWithDBs));
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("foreachobject", configWithDBs), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("foreachobject:", configWithDBs), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("foreachobject::", configWithDBs), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("foreachobject::qc/no/db/specified", configWithDBs), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("foreachobject:nodb:qc/incorrect/db/speficied", configWithDBs), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("foreachobject:ccdb:qc/too:many tokens", configWithDBs), std::invalid_argument);
 
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("foreachlatest:qcdb:qc/asdf/vcxz", configWithDBs));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("foreachlatest:ccdb:qc/asdf/vcxz", configWithDBs));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("foreachlatest:QCDB:qc/asdf/vcxz", configWithDBs));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("foreachlatest:CCDB:qc/asdf/vcxz", configWithDBs));
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("ForEachLatest:QcDb:qc/asdf/vcxz", configWithDBs));
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("foreachlatest", configWithDBs), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("foreachlatest:", configWithDBs), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("foreachlatest::", configWithDBs), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("foreachlatest::qc/no/db/specified", configWithDBs), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("foreachlatest:nodb:qc/incorrect/db/speficied", configWithDBs), std::invalid_argument);
-  BOOST_CHECK_THROW(trigger_helpers::triggerFactory("foreachlatest:ccdb:qc/too:many tokens", configWithDBs), std::invalid_argument);
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("foreachlatest:qcdb:qc/asdf/vcxz", configWithDBs));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("foreachlatest:ccdb:qc/asdf/vcxz", configWithDBs));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("foreachlatest:QCDB:qc/asdf/vcxz", configWithDBs));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("foreachlatest:CCDB:qc/asdf/vcxz", configWithDBs));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("ForEachLatest:QcDb:qc/asdf/vcxz", configWithDBs));
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("foreachlatest", configWithDBs), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("foreachlatest:", configWithDBs), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("foreachlatest::", configWithDBs), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("foreachlatest::qc/no/db/specified", configWithDBs), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("foreachlatest:nodb:qc/incorrect/db/speficied", configWithDBs), std::invalid_argument);
+  CHECK_THROWS_AS(trigger_helpers::triggerFactory("foreachlatest:ccdb:qc/too:many tokens", configWithDBs), std::invalid_argument);
 
   // check if config string is propagated
-  BOOST_CHECK_EQUAL(trigger_helpers::triggerFactory("10sec", dummyConfig)().config, "10sec");
-  BOOST_CHECK_EQUAL(trigger_helpers::triggerFactory("newobject:qcdb:qc/asdf/vcxz", configWithDBs)().config, "newobject:qcdb:qc/asdf/vcxz");
-  BOOST_CHECK_EQUAL(trigger_helpers::triggerFactory("foreachobject:qcdb:qc/asdf/vcxz", configWithDBs)().config, "foreachobject:qcdb:qc/asdf/vcxz");
-  BOOST_CHECK_EQUAL(trigger_helpers::triggerFactory("foreachlatest:qcdb:qc/asdf/vcxz", configWithDBs)().config, "foreachlatest:qcdb:qc/asdf/vcxz");
+  CHECK(trigger_helpers::triggerFactory("10sec", dummyConfig)().config == "10sec");
+  CHECK(trigger_helpers::triggerFactory("newobject:qcdb:qc/asdf/vcxz", configWithDBs)().config == "newobject:qcdb:qc/asdf/vcxz");
+  CHECK(trigger_helpers::triggerFactory("foreachobject:qcdb:qc/asdf/vcxz", configWithDBs)().config == "foreachobject:qcdb:qc/asdf/vcxz");
+  CHECK(trigger_helpers::triggerFactory("foreachlatest:qcdb:qc/asdf/vcxz", configWithDBs)().config == "foreachlatest:qcdb:qc/asdf/vcxz");
 
   // fixme: this is treated as "123 seconds", do we want to be so defensive?
-  BOOST_CHECK_NO_THROW(trigger_helpers::triggerFactory("123 secure code", dummyConfig));
+  CHECK_NOTHROW(trigger_helpers::triggerFactory("123 secure code", dummyConfig));
 }
 
-BOOST_AUTO_TEST_CASE(test_create_trigger)
+TEST_CASE("test_create_trigger")
 {
   PostProcessingConfig dummyConfig;
 
-  BOOST_CHECK_EQUAL(trigger_helpers::createTriggers({}, dummyConfig).size(), 0);
-  BOOST_CHECK_EQUAL(trigger_helpers::createTriggers({ "once", "always" }, dummyConfig).size(), 2);
+  CHECK(trigger_helpers::createTriggers({}, dummyConfig).size() == 0);
+  CHECK(trigger_helpers::createTriggers({ "once", "always" }, dummyConfig).size() == 2);
 }
 
-BOOST_AUTO_TEST_CASE(test_try_triggers)
+TEST_CASE("test_try_triggers")
 {
   PostProcessingConfig dummyConfig;
   {
     auto triggers = trigger_helpers::createTriggers({}, dummyConfig);
-    BOOST_CHECK(!trigger_helpers::tryTrigger(triggers));
-    BOOST_CHECK(!trigger_helpers::tryTrigger(triggers));
-    BOOST_CHECK(!trigger_helpers::tryTrigger(triggers));
-    BOOST_CHECK(!trigger_helpers::tryTrigger(triggers));
-    BOOST_CHECK(!trigger_helpers::tryTrigger(triggers));
+    CHECK(!trigger_helpers::tryTrigger(triggers));
+    CHECK(!trigger_helpers::tryTrigger(triggers));
+    CHECK(!trigger_helpers::tryTrigger(triggers));
+    CHECK(!trigger_helpers::tryTrigger(triggers));
+    CHECK(!trigger_helpers::tryTrigger(triggers));
   }
 
   {
     auto triggers = trigger_helpers::createTriggers({ "once" }, dummyConfig);
-    BOOST_CHECK(trigger_helpers::tryTrigger(triggers));
-    BOOST_CHECK(!trigger_helpers::tryTrigger(triggers));
-    BOOST_CHECK(!trigger_helpers::tryTrigger(triggers));
-    BOOST_CHECK(!trigger_helpers::tryTrigger(triggers));
-    BOOST_CHECK(!trigger_helpers::tryTrigger(triggers));
+    CHECK(trigger_helpers::tryTrigger(triggers));
+    CHECK(!trigger_helpers::tryTrigger(triggers));
+    CHECK(!trigger_helpers::tryTrigger(triggers));
+    CHECK(!trigger_helpers::tryTrigger(triggers));
+    CHECK(!trigger_helpers::tryTrigger(triggers));
   }
 
   {
     auto triggers = trigger_helpers::createTriggers({ "once", "once", "once" }, dummyConfig);
-    BOOST_CHECK(trigger_helpers::tryTrigger(triggers));
-    BOOST_CHECK(trigger_helpers::tryTrigger(triggers));
-    BOOST_CHECK(trigger_helpers::tryTrigger(triggers));
-    BOOST_CHECK(!trigger_helpers::tryTrigger(triggers));
-    BOOST_CHECK(!trigger_helpers::tryTrigger(triggers));
-    BOOST_CHECK(!trigger_helpers::tryTrigger(triggers));
+    CHECK(trigger_helpers::tryTrigger(triggers));
+    CHECK(trigger_helpers::tryTrigger(triggers));
+    CHECK(trigger_helpers::tryTrigger(triggers));
+    CHECK(!trigger_helpers::tryTrigger(triggers));
+    CHECK(!trigger_helpers::tryTrigger(triggers));
+    CHECK(!trigger_helpers::tryTrigger(triggers));
   }
 }
