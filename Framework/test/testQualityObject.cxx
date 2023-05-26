8d34dde9a1a7462b66185a8bf036d2990dc9fca4
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2023-05-26 10:53:27 +0200

    More Catch2 tests

--- Framework/test/testQualityObject.cxx
+++ Framework/test/testQualityObject.cxx
@@ -20,17 +20,14 @@
 
 #include <DataFormatsQualityControl/FlagReasons.h>
 
-#define BOOST_TEST_MODULE QualityObject test
-#define BOOST_TEST_MAIN
-#define BOOST_TEST_DYN_LINK
-#include <boost/test/unit_test.hpp>
+#include <catch_amalgamated.hpp>
 
 using namespace std;
 using namespace o2::quality_control::test;
 using namespace o2::quality_control::core;
 using namespace o2::quality_control;
 
-BOOST_AUTO_TEST_CASE(quality_object_test_constructors)
+TEST_CASE("quality_object_test_constructors")
 {
   QualityObject qo(Quality::Medium,
                    "xyzCheck",
@@ -40,40 +37,40 @@ BOOST_AUTO_TEST_CASE(quality_object_test_constructors)
                    {},
                    { { "probability", "0.45" }, { "threshold_medium", "0.42" } });
 
-  BOOST_CHECK_EQUAL(qo.getName(), "xyzCheck");
-  BOOST_CHECK(strcmp(qo.GetName(), "xyzCheck") == 0);
-  BOOST_CHECK_EQUAL(qo.getDetectorName(), "TST");
-  BOOST_CHECK_EQUAL(qo.getQuality(), Quality::Medium);
-  BOOST_REQUIRE_EQUAL(qo.getInputs().size(), 2);
-  BOOST_CHECK_EQUAL(qo.getInputs()[0], "qc/TST/testTask/mo1");
-  BOOST_CHECK_EQUAL(qo.getInputs()[1], "qc/TST/testTask/mo2");
-  BOOST_REQUIRE_EQUAL(qo.getMetadataMap().count("probability"), 1);
-  BOOST_CHECK_EQUAL(qo.getMetadataMap().at("probability"), "0.45");
-  BOOST_REQUIRE_EQUAL(qo.getMetadataMap().count("threshold_medium"), 1);
-  BOOST_CHECK_EQUAL(qo.getMetadataMap().at("threshold_medium"), "0.42");
+  CHECK(qo.getName() == "xyzCheck");
+  CHECK(strcmp(qo.GetName(), "xyzCheck") == 0);
+  CHECK(qo.getDetectorName() == "TST");
+  CHECK(qo.getQuality() == Quality::Medium);
+  REQUIRE(qo.getInputs().size() == 2);
+  CHECK(qo.getInputs()[0] == "qc/TST/testTask/mo1");
+  CHECK(qo.getInputs()[1] == "qc/TST/testTask/mo2");
+  REQUIRE(qo.getMetadataMap().count("probability") == 1);
+  CHECK(qo.getMetadataMap().at("probability") == "0.45");
+  REQUIRE(qo.getMetadataMap().count("threshold_medium") == 1);
+  CHECK(qo.getMetadataMap().at("threshold_medium") == "0.42");
 
   auto qo2 = qo;
 
-  BOOST_CHECK_EQUAL(qo2.getName(), "xyzCheck");
-  BOOST_CHECK(strcmp(qo2.GetName(), "xyzCheck") == 0);
-  BOOST_CHECK_EQUAL(qo2.getDetectorName(), "TST");
-  BOOST_CHECK_EQUAL(qo2.getQuality(), Quality::Medium);
-  BOOST_REQUIRE_EQUAL(qo2.getInputs().size(), 2);
-  BOOST_CHECK_EQUAL(qo2.getInputs()[0], "qc/TST/testTask/mo1");
-  BOOST_CHECK_EQUAL(qo2.getInputs()[1], "qc/TST/testTask/mo2");
-  BOOST_REQUIRE_EQUAL(qo2.getMetadataMap().count("probability"), 1);
-  BOOST_CHECK_EQUAL(qo2.getMetadataMap().at("probability"), "0.45");
-  BOOST_REQUIRE_EQUAL(qo2.getMetadataMap().count("threshold_medium"), 1);
-  BOOST_CHECK_EQUAL(qo2.getMetadataMap().at("threshold_medium"), "0.42");
+  CHECK(qo2.getName() == "xyzCheck");
+  CHECK(strcmp(qo2.GetName(), "xyzCheck") == 0);
+  CHECK(qo2.getDetectorName() == "TST");
+  CHECK(qo2.getQuality() == Quality::Medium);
+  REQUIRE(qo2.getInputs().size() == 2);
+  CHECK(qo2.getInputs()[0] == "qc/TST/testTask/mo1");
+  CHECK(qo2.getInputs()[1] == "qc/TST/testTask/mo2");
+  REQUIRE(qo2.getMetadataMap().count("probability") == 1);
+  CHECK(qo2.getMetadataMap().at("probability") == "0.45");
+  REQUIRE(qo2.getMetadataMap().count("threshold_medium") == 1);
+  CHECK(qo2.getMetadataMap().at("threshold_medium") == "0.42");
 
   Quality q(123, "defCheck");
   q.addMetadata("mykey", "myvalue");
   QualityObject qo3{ q, "defCheck" };
-  BOOST_REQUIRE_EQUAL(qo3.getMetadataMap().count("mykey"), 1);
-  BOOST_CHECK_EQUAL(qo3.getMetadataMap().at("mykey"), "myvalue");
+  REQUIRE(qo3.getMetadataMap().count("mykey") == 1);
+  CHECK(qo3.getMetadataMap().at("mykey") == "myvalue");
 }
 
-BOOST_AUTO_TEST_CASE(quality_object_test_setters)
+TEST_CASE("quality_object_test_setters")
 {
   QualityObject qo(Quality::Null, "xyzCheck");
   qo.setDetectorName("INVALID");
@@ -85,42 +82,42 @@ BOOST_AUTO_TEST_CASE(quality_object_test_setters)
   qo.addMetadata("probability", "0.45");
   qo.addMetadata("threshold_medium", "0.42");
 
-  BOOST_CHECK_EQUAL(qo.getName(), "xyzCheck");
-  BOOST_CHECK(strcmp(qo.GetName(), "xyzCheck") == 0);
-  BOOST_CHECK_EQUAL(qo.getDetectorName(), "TST");
-  BOOST_CHECK_EQUAL(qo.getQuality(), Quality::Medium);
-  BOOST_REQUIRE_EQUAL(qo.getInputs().size(), 2);
-  BOOST_CHECK_EQUAL(qo.getInputs()[0], "qc/TST/testTask/mo1");
-  BOOST_CHECK_EQUAL(qo.getInputs()[1], "qc/TST/testTask/mo2");
-  BOOST_REQUIRE_EQUAL(qo.getMetadataMap().count("probability"), 1);
-  BOOST_CHECK_EQUAL(qo.getMetadataMap().at("probability"), "0.45");
-  BOOST_REQUIRE_EQUAL(qo.getMetadataMap().count("threshold_medium"), 1);
-  BOOST_CHECK_EQUAL(qo.getMetadataMap().at("threshold_medium"), "0.42");
+  CHECK(qo.getName() == "xyzCheck");
+  CHECK(strcmp(qo.GetName(), "xyzCheck") == 0);
+  CHECK(qo.getDetectorName() == "TST");
+  CHECK(qo.getQuality() == Quality::Medium);
+  REQUIRE(qo.getInputs().size() == 2);
+  CHECK(qo.getInputs()[0] == "qc/TST/testTask/mo1");
+  CHECK(qo.getInputs()[1] == "qc/TST/testTask/mo2");
+  REQUIRE(qo.getMetadataMap().count("probability") == 1);
+  CHECK(qo.getMetadataMap().at("probability") == "0.45");
+  REQUIRE(qo.getMetadataMap().count("threshold_medium") == 1);
+  CHECK(qo.getMetadataMap().at("threshold_medium") == "0.42");
 }
 
-BOOST_AUTO_TEST_CASE(qopath)
+TEST_CASE("qopath")
 {
   // no policy
   QualityObject qo(Quality::Null, "xyzCheck", "DET");
   string path = qo.getPath();
-  BOOST_CHECK_EQUAL(path, "qc/DET/QO/xyzCheck");
+  CHECK(path == "qc/DET/QO/xyzCheck");
 
   // a policy which is not OnEachSeparately
   QualityObject qo2(Quality::Null, "xyzCheck", "DET", "OnAnyNonZero");
   string path2 = qo2.getPath();
-  BOOST_CHECK_EQUAL(path2, "qc/DET/QO/xyzCheck");
+  CHECK(path2 == "qc/DET/QO/xyzCheck");
 
   // policy is OnEachSeparately
   QualityObject qo3(Quality::Null, "xyzCheck", "DET", "OnEachSeparately", {}, { "objectABC" });
   string path3 = qo3.getPath();
-  BOOST_CHECK_EQUAL(path3, "qc/DET/QO/xyzCheck/objectABC");
+  CHECK(path3 == "qc/DET/QO/xyzCheck/objectABC");
 
   // policy is OnEachSeparately and the vector is empty
   QualityObject qo4(Quality::Null, "xyzCheck", "DET", "OnEachSeparately", {}, {});
-  BOOST_CHECK_EXCEPTION(qo4.getPath(), AliceO2::Common::FatalException, do_nothing);
+  CHECK_THROWS_AS(qo4.getPath(), AliceO2::Common::FatalException);
 }
 
-BOOST_AUTO_TEST_CASE(qo_reasons)
+TEST_CASE("qo_reasons")
 {
   QualityObject qo1(Quality::Bad, "xyzCheck", "DET");
   qo1.addReason(FlagReasonFactory::BadTracking(), "exception in x");
@@ -128,28 +125,28 @@ BOOST_AUTO_TEST_CASE(qo_reasons)
   qo1.addReason(FlagReasonFactory::LimitedAcceptance(), "sector C off");
 
   auto reasons1 = qo1.getReasons();
-  BOOST_CHECK_EQUAL(reasons1[0].first, FlagReasonFactory::BadTracking());
-  BOOST_CHECK_EQUAL(reasons1[0].second, "exception in x");
-  BOOST_CHECK_EQUAL(reasons1[1].first, FlagReasonFactory::BadTracking());
-  BOOST_CHECK_EQUAL(reasons1[1].second, "exception in y");
-  BOOST_CHECK_EQUAL(reasons1[2].first, FlagReasonFactory::LimitedAcceptance());
-  BOOST_CHECK_EQUAL(reasons1[2].second, "sector C off");
+  CHECK(reasons1[0].first == FlagReasonFactory::BadTracking());
+  CHECK(reasons1[0].second == "exception in x");
+  CHECK(reasons1[1].first == FlagReasonFactory::BadTracking());
+  CHECK(reasons1[1].second == "exception in y");
+  CHECK(reasons1[2].first == FlagReasonFactory::LimitedAcceptance());
+  CHECK(reasons1[2].second == "sector C off");
 
   auto qo2 = qo1;
   auto reasons2 = qo2.getReasons();
-  BOOST_CHECK_EQUAL(reasons2[0].first, FlagReasonFactory::BadTracking());
-  BOOST_CHECK_EQUAL(reasons2[0].second, "exception in x");
-  BOOST_CHECK_EQUAL(reasons2[1].first, FlagReasonFactory::BadTracking());
-  BOOST_CHECK_EQUAL(reasons2[1].second, "exception in y");
-  BOOST_CHECK_EQUAL(reasons2[2].first, FlagReasonFactory::LimitedAcceptance());
-  BOOST_CHECK_EQUAL(reasons2[2].second, "sector C off");
+  CHECK(reasons2[0].first == FlagReasonFactory::BadTracking());
+  CHECK(reasons2[0].second == "exception in x");
+  CHECK(reasons2[1].first == FlagReasonFactory::BadTracking());
+  CHECK(reasons2[1].second == "exception in y");
+  CHECK(reasons2[2].first == FlagReasonFactory::LimitedAcceptance());
+  CHECK(reasons2[2].second == "sector C off");
 
   auto quality = qo1.getQuality();
   auto reasons3 = quality.getReasons();
-  BOOST_CHECK_EQUAL(reasons3[0].first, FlagReasonFactory::BadTracking());
-  BOOST_CHECK_EQUAL(reasons3[0].second, "exception in x");
-  BOOST_CHECK_EQUAL(reasons3[1].first, FlagReasonFactory::BadTracking());
-  BOOST_CHECK_EQUAL(reasons3[1].second, "exception in y");
-  BOOST_CHECK_EQUAL(reasons3[2].first, FlagReasonFactory::LimitedAcceptance());
-  BOOST_CHECK_EQUAL(reasons3[2].second, "sector C off");
+  CHECK(reasons3[0].first == FlagReasonFactory::BadTracking());
+  CHECK(reasons3[0].second == "exception in x");
+  CHECK(reasons3[1].first == FlagReasonFactory::BadTracking());
+  CHECK(reasons3[1].second == "exception in y");
+  CHECK(reasons3[2].first == FlagReasonFactory::LimitedAcceptance());
+  CHECK(reasons3[2].second == "sector C off");
 }
