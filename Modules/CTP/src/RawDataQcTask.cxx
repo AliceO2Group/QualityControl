f5d4f30cf8bfaa533f817f326cc84f1a099b5c73
mbombara <Marek.Bombara@cern.ch>
2023-06-09 15:01:38 +0200

    use decodeRaw from CTPReconstruction for retrieving the digits from CTP readout

--- Modules/CTP/src/RawDataQcTask.cxx
+++ Modules/CTP/src/RawDataQcTask.cxx
@@ -65,29 +65,12 @@ void CTPRawDataReaderTask::startOfCycle()
 
 void CTPRawDataReaderTask::monitorData(o2::framework::ProcessingContext& ctx)
 {
-  LOG(info) << "============  Starting monitoring ================== ";
+  //LOG(info) << "============  Starting monitoring ================== ";
   // get the input
-
-  //std::map<o2::InteractionRecord, o2::ctp::CTPDigit> digits;
-  //std::vector<o2::ctp::CTPDigit> digits;
   std::vector<o2::framework::InputSpec> filter;
-  //std::map<o2::InteractionRecord, o2::ctp::CTPDigit> digits;
-  //std::map<o2::InteractionRecord, CTPDigit> digits;
   std::vector<o2::ctp::LumiInfo> lumiPointsHBF1;
   std::vector<o2::ctp::CTPDigit> outputDigits;
-  //LOG(info) << "before decoder -----------";
   mDecoder.decodeRaw(ctx.inputs(), filter, outputDigits, lumiPointsHBF1);
-  
-  LOG(info) << "after decoder ------------- ";
-  //LOG(info) << "size of the map: " << digits.size();
-  //LOG(info) << "is map empty? " << digits.empty();
-
-  
-  //for (auto const digmap : digits) {
-    //  LOG(info) << "before outputDigits -----------";
-    //  outputDigits.push_back(digmap.second);
-    //  LOG(info) << "after outputDigits -----------";
-  //  }
   
   for (auto const digit : outputDigits) {
     uint16_t bcid = digit.intRecord.bc;
@@ -112,139 +95,6 @@ void CTPRawDataReaderTask::monitorData(o2::framework::ProcessingContext& ctx)
   }
 
 }
-
-// old code
-/*
-void CTPRawDataReaderTask::monitorData(o2::framework::ProcessingContext& ctx)
-{
-  // get the input
-  o2::framework::DPLRawParser parser(ctx.inputs());
-  o2::ctp::gbtword80_t remnant = 0;
-  uint32_t sizegbt = 0;
-  uint32_t orbit0 = 0;
-  bool first = true;
-  const o2::ctp::gbtword80_t bcidmask = 0xfff;
-  o2::ctp::gbtword80_t pldmask = 0;
-
-  // loop over input
-  for (auto it = parser.begin(), end = parser.end(); it != end; ++it) {
-    // get the header
-    LOG(info) << "before getting header";
-    auto const* rdh = it.get_if<o2::header::RAWDataHeader>();
-    LOG(info) << "before getting orbit";
-    auto triggerOrbit = o2::raw::RDHUtils::getTriggerOrbit(rdh);
-    LOG(info) << "after getting orbit";
-    if (first) {
-      orbit0 = triggerOrbit;
-      first = false;
-    }
-
-    // LOG(info) << "trigger orbit = " << triggerOrbit;
-    // mHistogram->Fill(triggerOrbit);
-    // mHistogram2->Fill(triggerBC);
-
-    uint32_t payloadCTP;
-    LOG(info) << "before FEEID";
-    auto feeID = o2::raw::RDHUtils::getFEEID(rdh); // 0 = IR, 1 = TCR
-    auto linkCRU = (feeID & 0xf00) >> 8; 
-    if (linkCRU == o2::ctp::GBTLinkIDIntRec) {
-      payloadCTP = o2::ctp::NIntRecPayload;
-    } else if (linkCRU == o2::ctp::GBTLinkIDClassRec) {
-      payloadCTP = o2::ctp::NClassPayload;
-    } else {
-      LOG(error) << "Unxpected CTP CRU link:" << linkCRU;
-    }
-    LOG(info) << "RDH FEEid: " << feeID << " CTP CRU link:" << linkCRU << " Orbit:" << triggerOrbit << " payloadCTP = " << payloadCTP;
-    pldmask = 0;
-    for (uint32_t i = 0; i < payloadCTP; i++) {
-      pldmask[12 + i] = 1;
-    }
-    LOG(info) << "before payload";
-    gsl::span<const uint8_t> payload(it.data(), it.size());
-    LOG(info) << "after payload";
-    o2::ctp::gbtword80_t gbtWord = 0;
-    int wordCount = 0;
-    std::vector<o2::ctp::gbtword80_t> diglets;
-
-    // === according to O2: Detectors/CTP/workflow/src/RawToDigitConverterSpec.cxx ========
-    for (auto payloadWord : payload) {
-      if (wordCount == 15) {
-        wordCount = 0;
-      } else if (wordCount > 9) {
-        wordCount++;
-      } else if (wordCount == 9) {
-        for (int i = 0; i < 8; i++) {
-          gbtWord[wordCount * 8 + i] = bool(int(payloadWord) & (1 << i));
-        }
-        wordCount++;
-        diglets.clear();
-        o2::ctp::gbtword80_t diglet = remnant;
-        uint32_t k = 0;
-        const uint32_t nGBT = o2::ctp::NGBT;
-        while (k < (nGBT - payloadCTP)) {
-          std::bitset<nGBT> masksize = 0;
-          for (uint32_t j = 0; j < (payloadCTP - sizegbt); j++) {
-            masksize[j] = 1;
-          }
-          diglet |= (gbtWord & masksize) << (sizegbt);
-          diglets.push_back(diglet);
-          diglet = 0;
-          k += payloadCTP - sizegbt;
-          gbtWord = gbtWord >> (payloadCTP - sizegbt);
-          sizegbt = 0;
-        }
-        sizegbt = nGBT - k;
-        remnant = gbtWord;
-        // ==========================================
-        // LOG(info) << " gbtword after:" << gbtWord;
-        for (auto diglet : diglets) {
-          // LOG(info) << " diglet:" << diglet;
-          // LOG(info) << " pldmask:" << pldmask;
-          o2::ctp::gbtword80_t pld = (diglet & pldmask);
-          if (pld.count() == 0) {
-            continue;
-          }
-          // LOG(info) << "    pld:" << pld;
-          pld >>= 12;
-          uint32_t bcid = (diglet & bcidmask).to_ulong();
-          // LOG(info) << " diglet:" << diglet;
-          // LOG(info) << " bcid:" << bcid;
-          mHistoBC->Fill(bcid);
-
-          o2::ctp::gbtword80_t inputMask = 0;
-          if (linkCRU == o2::ctp::GBTLinkIDIntRec) {
-            inputMask = pld;
-            // LOG(info) << " InputMask:" << inputMask;
-            for (Int_t i = 0; i < payloadCTP; i++) {
-              if (inputMask[i] != 0) {
-                mHistoInputs->Fill(i);
-              }
-            }
-          }
-
-          o2::ctp::gbtword80_t classMask = 0;
-          if (linkCRU == o2::ctp::GBTLinkIDClassRec) {
-            classMask = pld;
-            // LOG(info) << " ClassMask:" << classMask;
-            for (Int_t i = 0; i < payloadCTP; i++) {
-              if (classMask[i] != 0) {
-                mHistoClasses->Fill(i);
-              }
-            }
-          }
-        }
-        gbtWord = 0;
-      } else {
-        for (int i = 0; i < 8; i++) {
-          gbtWord[wordCount * 8 + i] = bool(int(payloadWord) & (1 << i));
-        }
-        wordCount++;
-      }
-    }
-  }
-}
-
-*/
 
 void CTPRawDataReaderTask::endOfCycle()
 {
