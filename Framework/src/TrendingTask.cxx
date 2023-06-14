8201fe8bca3aa3d105279c58e380583956ae5acc
Barthélémy von Haller <barthelemy.von.haller@cern.ch>
2023-06-14 10:42:17 +0200

    [QC-984] Fix pp finalize no init (#1834)
    * [QC-984] nullptr if finalize is called before initialize
    
    * format
    
    * do not finalize if it is not initialized.
    
    * actually fix
    
    * format

--- Framework/src/TrendingTask.cxx
+++ Framework/src/TrendingTask.cxx
@@ -199,6 +199,11 @@ void TrendingTask::setUserYAxisRange(TH1* hist, const std::string& graphYAxisRan
 
 void TrendingTask::generatePlots()
 {
+  if (mTrend == nullptr) {
+    ILOG(Info, Support) << "The trend object is not there, won't generate any plots." << ENDM;
+    return;
+  }
+
   if (mTrend->GetEntries() < 1) {
     ILOG(Info, Support) << "No entries in the trend so far, won't generate any plots." << ENDM;
     return;
