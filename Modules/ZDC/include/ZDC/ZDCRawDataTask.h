1a31aa3cb76e50c221df8c46cb113b2c97e93b30
cpuggion84 <90757182+cpuggion84@users.noreply.github.com>
2023-07-31 12:14:31 +0200

    ZDC: Add and check  new plots (#1894)
    * ZDC: Add and check  new plots
    
    * Update ZDCRawDataTask.h
    
    * Update ZDCRawDataTask.cxx
    
    * ZDC: Add and check  new plots

--- Modules/ZDC/include/ZDC/ZDCRawDataTask.h
+++ Modules/ZDC/include/ZDC/ZDCRawDataTask.h
@@ -153,6 +153,7 @@ class ZDCRawDataTask final : public TaskInterface
   TH1* fSummaryRate;
   TH2* fSummaryAlign;
   TH2* fSummaryAlignShift;
+  TH2* fSummaryError;
   TH1* fOverBc;
 
   std::vector<std::string> fNameHisto;
@@ -167,8 +168,11 @@ class ZDCRawDataTask final : public TaskInterface
   double fMinBinY = 0;
   double fMaxBinY = 0;
   int fNumCycle = 0;
+  int fNumCycleErr = 0;
   int fAlignCycle = 1;
+  int fErrorCycle = 1;
   int fAlignNumEntries = 2000;
+
   sAlignment fMatrixAlign[o2::zdc::NModules][o2::zdc::NChPerModule];
 };
 
