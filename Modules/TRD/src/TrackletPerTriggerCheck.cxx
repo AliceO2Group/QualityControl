e1df078ce5e6eceaae8754e0fb3c6d93262daa03
Barthelemy <barthelemy.von.haller@cern.ch>
2023-08-29 16:36:38 +0200

    Fix some warnings in TRD

--- Modules/TRD/src/TrackletPerTriggerCheck.cxx
+++ Modules/TRD/src/TrackletPerTriggerCheck.cxx
@@ -93,13 +93,13 @@ Quality TrackletPerTriggerCheck::check(std::map<std::string, std::shared_ptr<Mon
       msg1->SetTextSize(10);
       int Entries = h->GetEntries();
       if (Entries > StatThreshold) {
-        msg1->AddText(TString::Format("Hist Can't be ignored. Stat is enough. Entries: %d > Threshold: %d", Entries, StatThreshold));
+        msg1->AddText(TString::Format("Hist Can't be ignored. Stat is enough. Entries: %d > Threshold: %ld", Entries, StatThreshold));
         // msg1->SetTextColor(kGreen);
       } else if (Entries > 0) {
-        msg1->AddText(TString::Format("Hist Can be ignored. Stat is low. Entries: %d < Threshold: %d", Entries, StatThreshold));
+        msg1->AddText(TString::Format("Hist Can be ignored. Stat is low. Entries: %d < Threshold: %ld", Entries, StatThreshold));
         // msg1->SetTextColor(kYellow);
       } else if (Entries == 0) {
-        msg1->AddText(TString::Format("Hist is empty. Entries: %d < Threshold: %d", Entries, StatThreshold));
+        msg1->AddText(TString::Format("Hist is empty. Entries: %d < Threshold: %ld", Entries, StatThreshold));
         msg1->SetTextColor(kRed);
       }
 
