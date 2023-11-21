bdeafe0164c6cca78d8cd034d3e8d12f2a013da6
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2023-11-21 18:58:42 +0100

    Add raw checkers

---
+++ Modules/TOF/include/TOF/CheckRaw.h
@@ -0,0 +1,52 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file   CheckRaw.h
+/// \author Nicolo' Jacazio
+/// \brief  Checker for the raw compressed data for TOF
+///
+
+#ifndef QC_MODULE_TOF_CHECKCOMPRESSEDDATA_H
+#define QC_MODULE_TOF_CHECKCOMPRESSEDDATA_H
+
+#include "QualityControl/CheckInterface.h"
+#include "QualityControl/MonitorObject.h"
+#include "QualityControl/Quality.h"
+
+namespace o2::quality_control_modules::tof
+{
+
+/// \brief  Checker for the data produced by the TOF compressor (i.e. checking raw data)
+///
+/// \author Nicolo' Jacazio
+class CheckRaw : public o2::quality_control::checker::CheckInterface
+{
+ public:
+  /// Default constructor
+  CheckRaw() = default;
+  /// Destructor
+  ~CheckRaw() override = default;
+
+  // Override interface
+  void configure(std::string name) override;
+  Quality check(std::map<std::string, std::shared_ptr<MonitorObject>>* moMap) override;
+  void beautify(std::shared_ptr<MonitorObject> mo, Quality checkResult) override;
+  std::string getAcceptedType() override;
+
+ private:
+  float mDiagnosticThresholdPerSlot = 0;
+
+  ClassDefOverride(CheckRaw, 1);
+};
+
+} // namespace o2::quality_control_modules::tof
+
+#endif // QC_MODULE_TOF_CHECKCOMPRESSEDDATA_H
