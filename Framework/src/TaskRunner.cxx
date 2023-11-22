75c0186aaf91c2e5a6f91cbb14e11de1a5ba015c
Barthelemy <barthelemy.von.haller@cern.ch>
2023-11-22 15:38:25 +0100

    fix case

--- Framework/src/TaskRunner.cxx
+++ Framework/src/TaskRunner.cxx
@@ -478,9 +478,9 @@ void TaskRunner::finishCycle(DataAllocator& outputs)
     ILOG(Debug, Devel) << "Registering taskRunner to BookKeeping" << ENDM;
     try {
       Bookkeeping::getInstance().registerProcess(mActivity.mId, mTaskConfig.taskName, mTaskConfig.detectorName, bookkeeping::DPL_PROCESS_TYPE_QC_TASK, "");
-      if (gSystem->Getenv("O2_QC_REGISTER_IN_BK_X_times")) {
-        ILOG(Debug, Devel) << "O2_QC_REGISTER_IN_BK_X_times set to " << gSystem->Getenv("O2_QC_REGISTER_IN_BK_X_times") << ENDM;
-        int iterations = std::stoi(gSystem->Getenv("O2_QC_REGISTER_IN_BK_X_times"));
+      if (gSystem->Getenv("O2_QC_REGISTER_IN_BK_X_TIMES")) {
+        ILOG(Debug, Devel) << "O2_QC_REGISTER_IN_BK_X_TIMES set to " << gSystem->Getenv("O2_QC_REGISTER_IN_BK_X_TIMES") << ENDM;
+        int iterations = std::stoi(gSystem->Getenv("O2_QC_REGISTER_IN_BK_X_TIMES"));
         for (int i = 1; i < iterations; i++) { // start at 1 because we already did it once
           Bookkeeping::getInstance().registerProcess(mActivity.mId, mTaskConfig.taskName, mTaskConfig.detectorName, bookkeeping::DPL_PROCESS_TYPE_QC_TASK, "");
         }
