81e963e0cb5449f11696ffcedc572fad66a9131c
Ole Schmidt <ole.schmidt@cern.ch>
2024-03-18 15:32:32 +0100

    Fix example for customParameters in SkeletonCheck

--- Modules/Skeleton/src/SkeletonCheck.cxx
+++ Modules/Skeleton/src/SkeletonCheck.cxx
@@ -38,7 +38,7 @@ Quality SkeletonCheck::check(std::map<std::string, std::shared_ptr<MonitorObject
   // you can get details about the activity via the object mActivity:
   ILOG(Debug, Devel) << "Run " << mActivity->mId << ", type: " << mActivity->mType << ", beam: " << mActivity->mBeamType << ENDM;
   // and you can get your custom parameters:
-  ILOG(Debug, Devel) << "custom param physics.pp.myOwnKey1 : " << mCustomParameters.atOrDefaultValue("myOwnKey1", "physics", "pp", "default_value") << ENDM;
+  ILOG(Debug, Devel) << "custom param physics.pp.myOwnKey1 : " << mCustomParameters.atOrDefaultValue("myOwnKey1", "default_value", "physics", "pp") << ENDM;
 
   for (auto& [moName, mo] : *moMap) {
 
