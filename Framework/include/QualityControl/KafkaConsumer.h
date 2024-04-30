a0c00c6ba50e2b77c13f2114b62a7ee8a619474b
Michal Tichák <michal.tichak@cern.ch>
2024-04-30 09:57:50 +0200

    midwork

---
+++ Framework/include/QualityControl/KafkaConsumer.h
@@ -0,0 +1,30 @@
+// Copyright 2019-2024 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// @file     KafkaConsumer.h
+/// @author   Michal Tichak
+
+#ifndef QC_CORE_KAFKA_CONSUMER_H
+#define QC_CORE_KAFKA_CONSUMER_H
+
+#include <kafka/KafkaConsumer.h>
+
+namespace o2::quality_control::core
+{
+
+class KafkaConsumer
+{
+};
+
+} // namespace o2::quality_control::core
+
+#endif
---
+++ Framework/src/KafkaConsumer.cxx
@@ -0,0 +1,1 @@
+#include "QualityControl/KafkaConsumer.h"
