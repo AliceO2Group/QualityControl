f74d02cd8b32fbe8b687f771f2998d17dbb6cc06
Barthélémy von Haller <barthelemy.von.haller@cern.ch>
2024-05-02 10:18:45 +0200

    [QC-1079] Run type as a string (#2077)
    * [QC-1079] run type as string
    
    * doc
    
    * config files
    
    * format
    
    * clearer method name
    
    * 1. extract isOnlyDigits to stringUtils
    2. handle the case when the run type is a number and convert it to the string representation
    3. tests
    
    * fix broken test
    
    * Test the translation of an integer run type into a string run type. Also warn the user of the translation.
    
    * format
    
    * forgot the test
    
    * fix (one line went missing0
    
    * format
    
    * rebase master
    unify isOnlyDigits and is_unsigned_integer into isUnsignedInteger

--- Framework/test/testCustomParameters.cxx
+++ Framework/test/testCustomParameters.cxx
@@ -139,7 +139,7 @@ TEST_CASE("test_at_optional_activity")
 {
   Activity activity;
   activity.mBeamType = "PROTON-PROTON";
-  activity.mType = 1;
+  activity.mType = "PHYSICS";
 
   CustomParameters cp;
   cp.set("aaa", "AAA");
@@ -155,7 +155,7 @@ TEST_CASE("test_at_optional_activity")
 
   Activity activity2;
   activity.mBeamType = "Pb-Pb";
-  activity.mType = 1;
+  activity.mType = "PHYSICS";
   CHECK(cp.atOptional("aaa", activity).value() == "DDD");
 }
 
