823e91c36a78cd166d9027725ef73bfd9de90818
anabananana98 <ananya.rai@yale.edu>
2024-05-21 16:21:55 -0400

    [EMCAL-527] Made changes to Payload Per Event DDL Check

--- Modules/EMCAL/src/PayloadPerEventDDLCheck.cxx
+++ Modules/EMCAL/src/PayloadPerEventDDLCheck.cxx
@@ -26,7 +26,7 @@ if (nChiSqMedPayloadThresh != mCustomParameters.end()) {
   try {
     mChiSqMedPayloadThresh = std::stod(nChiSqMedPayloadThresh->second);
   } catch (std::exception& e) {
-    ILOG(Error, Support) << fmt::format("Value {} not a double", nChiSqMedPayloadThresh->second.data()) << ENDM;
+    ILOG(Error, Support) << ("Value {} not a double", nChiSqMedPayloadThresh->second.data()) << ENDM;
   }
 }
 
@@ -35,7 +35,7 @@ if (nBadChiSqLowPayloadThresh != mCustomParameters.end()) {
   try {
     mChiSqBadLowPayloadThresh = std::stod(nBadChiSqLowPayloadThresh->second);
   } catch (std::exception& e) {
-    ILOG(Error, Support) << fmt::format("Value {} not a double", nBadChiSqLowPayloadThresh->second.data()) << ENDM;
+    ILOG(Error, Support) << ("Value {} not a double", nBadChiSqLowPayloadThresh->second.data()) << ENDM;
   }
 }
 }
@@ -55,11 +55,11 @@ Quality PayloadPerEventDDLCheck::check(std::map<std::string, std::shared_ptr<Mon
     auto* h = dynamic_cast<TH2*>(mo->getObject());
 
     for (int i = 1; i <= h->GetNbinsX(); i++) {
-      TH1* h_y = (TH1*)h->ProjectionY(Form("h_y_%d", i), i, i);
+      std::unique_ptr<TH1> h_y = (TH1*)h->ProjectionY(Form("h_y_%d", i), i, i);
       h_y->Scale(1. / h_y->Integral());
-      TH1* h_y_ref = (TH1*)mCalibReference->ProjectionY(Form("h_y_ref_%d", i), i, i);
+      std::unique_ptr<TH1> h_y_ref = (TH1*)mCalibReference->ProjectionY(Form("h_y_ref_%d", i), i, i);
       h_y_ref->Scale(1. / h_y_ref->Integral());
-      Double_t chisq_val = h_y->Chi2Test(h_y_ref, "UU NORM CHI2/NDF");
+      Double_t chisq_val = h_y->Chi2Test(h_y_ref.get(), "UU NORM CHI2/NDF");
 
       if (chisq_val > mChiSqMedPayloadThresh) {
         result = Quality::Medium;
@@ -78,31 +78,32 @@ std::string PayloadPerEventDDLCheck::getAcceptedType() { return "TH2"; }
 
 void PayloadPerEventDDLCheck::beautify(std::shared_ptr<MonitorObject> mo, Quality checkResult)
 {
-  //    if (mo->getName() == “NPhysicsTriggers”)
-  //    {
-  //        auto* h = dynamic_cast<TH1*>(mo->getObject());
-  //        TPaveText* msg = new TPaveText(0.5, 0.5, 0.9, 0.75, "NDC");
-  //        h->GetListOfFunctions()->Add(msg);
-  //        msg->SetName(Form("%s_msg", mo->GetName()));
-  //        if (checkResult == Quality::Good) {
-  //            //
-  //            msg->Clear();
-  //            msg->AddText("NPhysicsTriggs increases with time: OK!!!");
-  //            msg->SetFillColor(kGreen);
-  //            //
-  //            h->SetFillColor(kGreen);
-  //        } else if (checkResult == Quality::Bad) {
-  //            ILOG(Debug, Devel) << "Quality::Bad, setting to red";
-  //            msg->Clear();
-  //            msg->AddText("NPhysicsTriggs EMPTY");
-  //            msg->AddText("If NOT a technical run,");
-  //            msg->AddText("call EMCAL on-call.");
-  //            h->SetFillColor(kRed);
-  //        } else if (checkResult == Quality::Medium) {
-  //            ILOG(Debug, Devel) << "Quality::medium, setting to orange";
-  //            h->SetFillColor(kOrange);
-  //        }
-  //        h->SetLineColor(kBlack);
-  //    }
+  if (mo->getName() == "PayloadSizePerDDL") {
+    auto* h = dynamic_cast<TH1*>(mo->getObject());
+    TPaveText* msg = new TPaveText(0.5, 0.5, 0.9, 0.75, "NDC");
+    h->GetListOfFunctions()->Add(msg);
+    msg->SetName(Form("%s_msg", mo->GetName()));
+    if (checkResult == Quality::Good) {
+      //
+      msg->Clear();
+      msg->AddText("Agrees with reference histogram: OK!!!");
+      msg->SetFillColor(kGreen);
+      //
+      h->SetFillColor(kGreen);
+    } else if (checkResult == Quality::Bad) {
+      ILOG(Debug, Devel) << "Quality::Bad, setting to red";
+      msg->Clear();
+      msg->AddText("ERROR: Disagrees with reference histogram, high Chi-Sq");
+      msg->AddText("If NOT a technical run,");
+      msg->AddText("call EMCAL on-call.");
+      h->SetFillColor(kRed);
+    } else if (checkResult == Quality::Medium) {
+      msg->Clear();
+      msg->AddText("WARNING: Chi-Sq greater than medium threshold");
+      ILOG(Debug, Devel) << "Quality::medium, setting to orange";
+      h->SetFillColor(kOrange);
+    }
+    h->SetLineColor(kBlack);
+  }
 }
 } // namespace o2::quality_control_modules::emcal
