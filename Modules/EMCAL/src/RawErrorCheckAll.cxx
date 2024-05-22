98b59c0a6bf8596b75125cf546364a70202c00dc
anabananana98 <ananya.rai@yale.edu>
2024-05-22 08:22:59 -0400

    [EMCAL-792] Fixed ILOG errors

--- Modules/EMCAL/src/RawErrorCheckAll.cxx
+++ Modules/EMCAL/src/RawErrorCheckAll.cxx
@@ -29,7 +29,7 @@ void RawErrorCheckAll::configure()
     try {
       mBadThreshold = std::stod(nBadThreshold->second);
     } catch (std::exception& e) {
-      ILOG(Error, Support) << ("Value {} not a double", nBadThreshold->second.data()) << ENDM;
+      ILOG(Error, Support) << "Value " <<  nBadThreshold->second << " not a double" << ENDM;
     }
   }
 
@@ -38,7 +38,7 @@ void RawErrorCheckAll::configure()
     try {
       mPeriodMovAvg = std::stoi(nPeriodMovAvg->second);
     } catch (std::exception& e) {
-      ILOG(Error, Support) << ("Value {} not an int", nPeriodMovAvg->second.data()) << ENDM;
+      ILOG(Error, Support) << "Value " <<  nPeriodMovAvg->second << " not an int" << ENDM;
     }
   }
 }
