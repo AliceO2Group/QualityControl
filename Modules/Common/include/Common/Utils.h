c51316d7d7c25bbf93eb912e4b5ffc5e225d044c
Felix Schlepper <felix.schlepper@cern.ch>
2024-05-23 11:25:54 +0200

    GLO:  ITS-TPC matching efficiency to th1 (#2293)
    * Framework: Add missing const for CustomParameters
    
    Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>
    
    * Common: Add getFromExtendedConfig
    
    Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>
    
    * GLO: Use common param parsing
    
    Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>
    
    * GLO: Change TEfficiency to TH1
    
    Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>
    
    * Framework: Conform to suggested naming
    
    Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>
    
    ---------
    
    Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>

--- Modules/Common/include/Common/Utils.h
+++ Modules/Common/include/Common/Utils.h
@@ -25,6 +25,38 @@
 namespace o2::quality_control_modules::common
 {
 
+namespace internal
+{
+/// \brief Parses a string to a type
+/// \param params String to be cast
+/// \return Returns requested type from param
+template <typename T>
+T stringToType(const std::string& param)
+{
+  T retVal{};
+  if constexpr (std::is_same<int, T>::value) {
+    retVal = std::stoi(param);
+  } else if constexpr (std::is_same<std::string, T>::value) {
+    retVal = param;
+  } else if constexpr (std::is_same<float, T>::value) {
+    retVal = std::stof(param);
+  } else if constexpr (std::is_same<double, T>::value) {
+    retVal = std::stod(param);
+  } else if constexpr (std::is_same<bool, T>::value) {
+    if ((param == "true") || (param == "True") || (param == "TRUE") || (param == "1")) {
+      retVal = true;
+    } else if ((param == "false") || (param == "False") || (param == "FALSE") || (param == "0")) {
+      retVal = false;
+    } else {
+      LOG(error) << "Cannot parse boolean.";
+    }
+  } else {
+    LOG(error) << "Template type not supported";
+  }
+  return retVal;
+}
+} // namespace internal
+
 /// \brief Gets a task parameter from the config file
 /// Convenience function to return a value for a taskParameter given in the config file
 /// \param params mCustomParameters from within the QualityControl/TaskInterface
@@ -38,27 +70,28 @@ T getFromConfig(const quality_control::core::CustomParameters& params, const std
     LOGP(warning, "Missing parameter. Please add '{}': '<value>' to the 'taskParameters'. Using default value {}.", name.data(), retVal);
   } else {
     const auto& param = itParam->second;
-    if constexpr (std::is_same<int, T>::value) {
-      retVal = std::stoi(param);
-    } else if constexpr (std::is_same<std::string, T>::value) {
-      retVal = param;
-    } else if constexpr (std::is_same<float, T>::value) {
-      retVal = std::stof(param);
-    } else if constexpr (std::is_same<double, T>::value) {
-      retVal = std::stod(param);
-    } else if constexpr (std::is_same<bool, T>::value) {
-      if ((param == "true") || (param == "True") || (param == "TRUE") || (param == "1")) {
-        retVal = true;
-      } else if ((param == "false") || (param == "False") || (param == "FALSE") || (param == "0")) {
-        retVal = false;
-      } else {
-        LOG(error) << fmt::format("Please provide a valid boolean value for {}.", name.data()).data();
-      }
-    } else {
-      LOG(error) << "Template type not supported";
-    }
+    return internal::stringToType<T>(param);
   }
   return retVal;
+}
+
+/// \brief Gets a extended task parameter from the config file
+/// Convenience function to return a value for a extended taskParameter given in the config file
+/// \param activity mActivity from within the QualityControl/TaskInterface
+/// \param params mCustomParameters from within the QualityControl/TaskInterface
+/// \param name Name of the taskParameter
+/// \param retVal Default value if not specified in config, must be stringyfiable
+/// \return taskParameter converted to bool, int, float, double or std::string depending on the template type
+template <typename T>
+T getFromExtendedConfig(const quality_control::core::Activity& activity, const quality_control::core::CustomParameters& params, const std::string& name, T retVal = T{})
+{
+  std::string parameter;
+  if (auto param = params.atOptional(name, activity)) {
+    parameter = param.value();
+  } else {
+    parameter = params.atOrDefaultValue(name, std::to_string(retVal));
+  }
+  return internal::stringToType<T>(parameter);
 }
 
 } // namespace o2::quality_control_modules::common
