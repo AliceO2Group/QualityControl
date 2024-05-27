1393bd7a8d6e2568438b5902993af43115951990
Piotr Konopka <piotr.jan.konopka@cern.ch>
2024-05-27 14:23:34 +0200

    QC-1179 Fix TrendingTask test and allow it to be ran with CI
    The test could be extended of course, but this already brings back what was tested before.

--- Framework/include/QualityControl/CcdbDatabase.h
+++ Framework/include/QualityControl/CcdbDatabase.h
@@ -91,7 +91,7 @@ class CcdbDatabase : public DatabaseInterface
   void disconnect() override;
   void prepareTaskDataContainer(std::string taskName) override;
   std::vector<std::string> getPublishedObjectNames(std::string taskName) override;
-  void truncate(std::string taskName, std::string objectName) override;
+  void truncate(std::string path, std::string objectName) override;
   static long getCurrentTimestamp();
   static long getFutureTimestamp(int secondsInFuture);
   /**
