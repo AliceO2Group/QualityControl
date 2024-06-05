d5368a734b082b7046792050b1cd043d6fda422d
Isakov Artem <isakov@ujf.cas.cz>
2024-06-05 16:52:52 +0200

    Applied comments

--- Modules/ITS/include/ITS/ITSChipStatusTask.h
+++ Modules/ITS/include/ITS/ITSChipStatusTask.h
@@ -19,6 +19,7 @@
 
 #include "QualityControl/TaskInterface.h"
 #include "Common/Utils.h"
+#include "Common/TH2Ratio.h"
 
 #include <TH1D.h>
 #include <TH2D.h>
@@ -38,9 +39,9 @@ class Stack
  public:
   std::vector<std::vector<int>> stack;
   Stack() : size_max(0), current_element_id(0) {}
-  Stack(int nsizey, int nsizex) : size_max(nsizey), current_element_id(0)
+  Stack(int nsizex, int nsizey) : size_max(nsizex), current_element_id(0)
   {
-    stack.resize(nsizey, std::vector<int>(nsizex, 0));
+    stack.resize(nsizex, std::vector<int>(nsizey, 0));
   }
   void push(const std::vector<int>& element)
   {
@@ -73,19 +74,23 @@ class ITSChipStatusTask final : public TaskInterface
  private:
   void getParameters(); // get Task parameters from json file
   void setAxisTitle(TH1* object, const char* xTitle, const char* yTitle);
+  void Beautify();
   static constexpr int NLayer = 7;
   static constexpr int NLayerIB = 3;
   const int nChipsPerLayer[NLayer] = { 108, 144, 180, 2688, 3360, 8232, 9408 };
   const int StaveBoundary[NLayer + 1] = { 0, 12, 28, 48, 72, 102, 144, 192 };
   const int ChipBoundary[NLayer + 1] = { 0, 108, 252, 432, 3120, 6480, 14712, 24120 };
+  const int ChipsBoundaryLayers[8] = { 0, 108, 252, 432, 3120 - 432, 6480 - 432, 14712 - 6480, 24120 - 6480 }; // needed for drawing labels and lines for histogram
   const int ChipsBoundaryBarrels[4] = { 0, 432, 6480, 24120 };
   static constexpr int NFees = 48 * 3 + 144 * 2;
   TString LayerBinLabels[11] = { "L0", "L1", "L2", "L3B", "L3T", "L4B", "L4T", "L5B", "L5T", "L6B", "L6T" };
-  TH2D* DeadChips[3];
+  std::shared_ptr<TH2FRatio> DeadChips[3];
   Stack* ChipsStack[3];
+  Stack* TFsStack[3];
   int nQCCycleToMonitor = 50;
   TString BarrelNames[3] = { "IB", "ML", "OB" };
   std::vector<int> CurrentDeadChips[3]; // Vectors of Dead Chips in current QC cycle
+  std::vector<int> CurrentTFs[3];       // Vectors of Dead Chips in current QC cycle
 };
 
 } // namespace o2::quality_control_modules::its
--- Modules/ITS/src/ITSChipStatusTask.cxx
+++ Modules/ITS/src/ITSChipStatusTask.cxx
@@ -35,7 +35,6 @@ ITSChipStatusTask::~ITSChipStatusTask()
 {
   for (int i = 0; i < 3; i++) {
     delete ChipsStack[i];
-    delete DeadChips[i];
   }
 }
 
@@ -44,62 +43,15 @@ void ITSChipStatusTask::initialize(o2::framework::InitContext& /*ctx*/)
   ILOG(Debug, Devel) << "initializing the ITSChipStatusTask" << ENDM;
   getParameters();
 
-  int Layer_Draw = 0;
   for (int i = 0; i < 3; i++) {
 
     ChipsStack[i] = new Stack(nQCCycleToMonitor, ChipsBoundaryBarrels[i + 1] - ChipsBoundaryBarrels[i]);
+    TFsStack[i] = new Stack(nQCCycleToMonitor, ChipsBoundaryBarrels[i + 1] - ChipsBoundaryBarrels[i]);
 
-    DeadChips[i] = new TH2D(Form("DeadChips%s", BarrelNames[i].Data()), Form("Dead Chips in %s", BarrelNames[i].Data()), nQCCycleToMonitor, 0, nQCCycleToMonitor, ChipsBoundaryBarrels[i + 1] - ChipsBoundaryBarrels[i], 0, ChipsBoundaryBarrels[i + 1] - ChipsBoundaryBarrels[i]);
-    setAxisTitle(DeadChips[i], "QC Cycle ID", "");
+    DeadChips[i] = std::make_shared<TH2FRatio>(Form("DeadChips%s", BarrelNames[i].Data()), Form("Time fraction without data from %s chips", BarrelNames[i].Data()), nQCCycleToMonitor, 0, nQCCycleToMonitor, ChipsBoundaryBarrels[i + 1] - ChipsBoundaryBarrels[i], 0, ChipsBoundaryBarrels[i + 1] - ChipsBoundaryBarrels[i], false);
+    setAxisTitle(DeadChips[i].get(), Form("Last %d QC Cycles", nQCCycleToMonitor), "Chip ID");
 
-    int iLayerBegin, iLayerEnd;
-    if (i == 0) {
-      iLayerBegin = 0;
-      iLayerEnd = 2;
-    } else if (i == 1) {
-      iLayerBegin = 3;
-      iLayerEnd = 4;
-    } else {
-      iLayerBegin = 5;
-      iLayerEnd = 6;
-    }
-    const int ChipBoundary[8] = { 0, 108, 252, 432, 3120 - 432, 6480 - 432, 14712 - 6480, 24120 - 6480 }; // needed for drawing labels and lines for histogram
-    const int StaveBoundary[NLayer] = { 0, 12, 28, 0, 24, 0, 42 };
-    for (Int_t iLayer = iLayerBegin; iLayer <= iLayerEnd; iLayer++) {
-
-      TLatex* msg = new TLatex(5, ChipBoundary[iLayer + 1] - 15 * (1 + i * 30), Form("#bf{L%d}", iLayer));
-      msg->SetTextSize(20);
-      msg->SetTextFont(43);
-      DeadChips[i]->GetListOfFunctions()->Add(msg);
-      if (iLayer < iLayerEnd) {
-        auto l = new TLine(0, ChipBoundary[iLayer + 1] - 0.5, nQCCycleToMonitor, ChipBoundary[iLayer + 1] - 0.5);
-        DeadChips[i]->GetListOfFunctions()->Add(l);
-      }
-    }
-    int nChipsPerStave = i == 0 ? 9 : i == 1 ? 112
-                                             : 196;
-    for (int iy = 0; iy < DeadChips[i]->GetNbinsY(); iy++) {
-      if (iy % nChipsPerStave == 0) {
-        auto l = new TLine(0, iy - 0.5, nQCCycleToMonitor, iy - 0.5);
-        l->SetLineColor(40);
-        l->SetLineWidth(1);
-
-        DeadChips[i]->GetListOfFunctions()->Add(l);
-
-        if (iy >= ChipBoundary[Layer_Draw + 1]) {
-          Layer_Draw++;
-        }
-        TLatex* msg = new TLatex(-3, iy, Form("%d", (int)(iy) / nChipsPerStave - StaveBoundary[Layer_Draw]));
-        msg->SetTextSize(12);
-        msg->SetTextFont(43);
-        DeadChips[i]->GetListOfFunctions()->Add(msg);
-      }
-    }
-    Layer_Draw++;
-
-    DeadChips[i]->GetYaxis()->SetLabelSize(0);
-    DeadChips[i]->GetYaxis()->SetAxisColor(0);
-    getObjectsManager()->startPublishing(DeadChips[i]);
+    getObjectsManager()->startPublishing(DeadChips[i].get());
   }
 }
 
@@ -121,6 +73,8 @@ void ITSChipStatusTask::startOfCycle()
   for (int i = 0; i < 3; i++) {
     CurrentDeadChips[i].clear();
     CurrentDeadChips[i].resize(ChipsBoundaryBarrels[i + 1] - ChipsBoundaryBarrels[i], 0);
+    CurrentTFs[i].clear();
+    CurrentTFs[i].resize(ChipsBoundaryBarrels[i + 1] - ChipsBoundaryBarrels[i], 0);
   }
 }
 
@@ -139,6 +93,7 @@ void ITSChipStatusTask::monitorData(o2::framework::ProcessingContext& ctx)
       } else {
         CurrentDeadChips[iBarrel][ChipID_fill]++;
       }
+      CurrentTFs[iBarrel][ChipID_fill]++;
     }
     id++;
   }
@@ -156,13 +111,49 @@ void ITSChipStatusTask::endOfCycle()
   ILOG(Debug, Devel) << "endOfCycle" << ENDM;
   for (int iBarrel = 0; iBarrel < 3; iBarrel++) {
     ChipsStack[iBarrel]->push(CurrentDeadChips[iBarrel]);
-
+    TFsStack[iBarrel]->push(CurrentTFs[iBarrel]);
     for (int ix = 1; ix <= DeadChips[iBarrel]->GetNbinsX(); ix++)
-      for (int iy = 1; iy <= DeadChips[iBarrel]->GetNbinsY(); iy++)
-        DeadChips[iBarrel]->SetBinContent(ix, iy, ChipsStack[iBarrel]->stack[ix - 1][iy - 1]);
+      for (int iy = 1; iy <= DeadChips[iBarrel]->GetNbinsY(); iy++) {
+        DeadChips[iBarrel]->getNum()->SetBinContent(ix, iy, ChipsStack[iBarrel]->stack[ix - 1][iy - 1]);
+        DeadChips[iBarrel]->getDen()->SetBinContent(ix, iy, TFsStack[iBarrel]->stack[ix - 1][iy - 1]);
+      }
+
+    DeadChips[iBarrel]->update();
   }
 
-  ILOG(Debug, Devel) << "endOfCycle" << ENDM;
+  Beautify();
+}
+
+void ITSChipStatusTask::Beautify()
+{
+
+  int Layer_Draw = 0;
+  for (int i = 0; i < 3; i++) {
+
+    int iLayerBegin, iLayerEnd;
+    if (i == 0) {
+      iLayerBegin = 0;
+      iLayerEnd = 2;
+    } else if (i == 1) {
+      iLayerBegin = 3;
+      iLayerEnd = 4;
+    } else {
+      iLayerBegin = 5;
+      iLayerEnd = 6;
+    }
+
+    for (Int_t iLayer = iLayerBegin; iLayer <= iLayerEnd; iLayer++) {
+
+      TLatex* msg = new TLatex(5, ChipsBoundaryLayers[iLayer + 1] - 15 * (1 + i * 30), Form("#bf{L%d}", iLayer));
+      msg->SetTextSize(20);
+      msg->SetTextFont(43);
+      DeadChips[i]->GetListOfFunctions()->Add(msg);
+      if (iLayer < iLayerEnd) {
+        auto l = new TLine(0, ChipsBoundaryLayers[iLayer + 1] - 0.5, nQCCycleToMonitor, ChipsBoundaryLayers[iLayer + 1] - 0.5);
+        DeadChips[i]->GetListOfFunctions()->Add(l);
+      }
+    }
+  }
 }
 
 void ITSChipStatusTask::endOfActivity(const Activity& /*activity*/)
