1df96a5445bcfbbb4e25f1dc482d883f041f2266
Maximilian Horst <maximilian.horst@frm2.tum.de>
2024-06-14 09:17:31 +0200

    Make Mergable

--- Modules/TPC/src/PID.cxx
+++ Modules/TPC/src/PID.cxx
@@ -97,7 +97,72 @@ void PID::monitorData(o2::framework::ProcessingContext& ctx)
 
 void PID::endOfCycle()
 {
+  // ===| Fitting Histogram for separation Power |============================================================
+  std::unique_ptr<TF1> fitFunc = std::make_unique<TF1>("fitFunc", "[0]*exp(-0.5*((x-[1])/[2])^2) + [3]*exp(-0.5*((x-[4])/[5])^2)", 0, 100);
+  //To-Do: put this right after your declaration of the TF1 fitFunc
+  const int nPars = 8; //6 fit parameters + 2 external (seperation power + chi²/ndf)
+
+  for (auto const& pair : mQCPID.getMapOfHisto()) {
+    for (auto& hist : pair.second) {
+      //      if (std::static_cast<string>(pair.first).compare("hdEdxMaxMIP") == 0) {
+      if (pair.first.compare("hdEdxMaxMIP") == 0) {
+        mTrendingParameters.clear();
+        // Define fitting function: sum of two Gaussians with an offset
+        // Set initial parameters for the fit
+        fitFunc->SetParameter(0, 3000); // Amplitude of the first Gaussian
+        fitFunc->SetParameter(1, 50);   // Mean of the first Gaussian
+        fitFunc->SetParLimits(1, 45, 55);
+        fitFunc->SetParameter(2, 2);   // Sigma of the first Gaussian
+        fitFunc->SetParameter(3, 100); // Amplitude of the second Gaussian
+        fitFunc->SetParameter(4, 75);  // Mean of the second Gaussian
+        fitFunc->SetParLimits(4, 60, 90);
+        fitFunc->SetParameter(5, 10); // Sigma of the second Gaussian
+
+        // Fit the histogram with the fitting function
+        hist->Fit(fitFunc.get(), "QR");
+        for (int iPar = 0; iPar < nPars - 2; iPar++) {
+          mTrendingParameters.emplace_back(fitFunc->GetParameter(iPar)); //save fit parameters
+        }
+        // Retrieve parameters of the fitted function
+
+        const double sepPow = (mTrendingParameters[4] - mTrendingParameters[1]) / (mTrendingParameters[2] / 2. + mTrendingParameters[5] / 2.); //separation power
+        mTrendingParameters.emplace_back(sepPow);
+        mTrendingParameters.emplace_back(fitFunc->GetChisquare() / fitFunc->GetNDF()); //chi²/ndf of fit
+
+        //        // Retrieve parameters of the fitted function
+        //        mTrendingParameters.emplace_back(fitFunc->GetParameter(1)); // Mean pion
+        //        mTrendingParameters.emplace_back(fitFunc->GetParameter(2)); // sigma pion
+        //        mTrendingParameters.emplace_back(fitFunc->GetParameter(4)); // Mean electron
+        //        mTrendingParameters.emplace_back(fitFunc->GetParameter(5)); // sigma electro
+      }
+    }
+  }
+
+  //To-Do: Change this after you call hist->Fit(fitFunc.get(), "QR"); + Don't draw the fit
+
+  //To-Do: Change your TPaveText to this:
+
+  mSeparationPower->cd();
+  pSeparationPower->Draw();
+
   ILOG(Debug, Devel) << "endOfCycle" << ENDM;
+
+  //TPaveText* pSeparationPower = new TPaveText(.05, .05, .95, .95);
+  //pSeparationPower->AddText(fmt::format("Mean Pi: {:.3}", mTrendingParameters[0]).c_str());
+  //pSeparationPower->AddText(fmt::format("Sigma Pi: {:.3}", mTrendingParameters[1]).c_str());
+  //pSeparationPower->AddText(fmt::format("Mean El: {:.3}", mTrendingParameters[2]).c_str());
+  //pSeparationPower->AddText(fmt::format("Sigma El: {:.3}", mTrendingParameters[3]).c_str());
+  //pSeparationPower->AddText(fmt::format("separationPower: {:.3}", (mTrendingParameters[2] - mTrendingParameters[0]) / (mTrendingParameters[1] / 2. + mTrendingParameters[3] / 2.)).c_str());
+  //mSeparationPower->cd();
+  //pSeparationPower->Draw();
+
+  TProfile* pSeparationPower = new TProfile("pSeparationPower", "pSeparationPower", nPars, 0., (float)nPars);
+  const TString binLabels[nPars] = { "Amplitude Pi", "Mean Pi", "Sigma Pi", "Amplitude El", "Mean El", "Sigma El", "Separation Power", "chiSquare/ndf" };
+
+  for (int iPar = 0; iPar < nPars; iPar++) {
+    pSeparationPower->GetXaxis()->SetBinLabel(iPar + 1, binLabels[iPar]);
+    pSeparationPower->Fill((float)iPar + 0.5, mTrendingParameters[iPar]);
+  }
 }
 
 void PID::endOfActivity(const Activity& /*activity*/)
