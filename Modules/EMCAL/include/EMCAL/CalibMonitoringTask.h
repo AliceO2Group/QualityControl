7758337cccc9ff30143d976d6f0533782da4c0d0
Sierra Weyhmiller <sierra.lisa.weyhmiller@cern.ch>
2024-08-06 10:02:23 -0400

    [EMCAL-767] removed the unused TCanvas

--- Modules/EMCAL/include/EMCAL/CalibMonitoringTask.h
+++ Modules/EMCAL/include/EMCAL/CalibMonitoringTask.h
@@ -94,7 +94,6 @@ class CalibMonitoringTask final : public quality_control::postprocessing::PostPr
   TH1* mTRUThresholds = nullptr;                      ///< The L0 threshold vs TRU ID PHYS
   TH1* mL0Algorithm = nullptr;                        ///< The L0 algorithm vs TRU ID
   TH1* mRollbackSTU = nullptr;                        ///< The Rollback buffer vs TRU ID
-  TCanvas* mTRUMaskPositionCanvas = nullptr;          ///< The FastOR Mask Position in Eta, Phi Canvas
   TH2* mTRUMaskPositionHisto = nullptr;               ///< The FastOR Mask Position in Eta, Phi Histogram
   std::unique_ptr<o2::emcal::CalibDB> mCalibDB;       ///< EMCAL calibration DB handler
   std::unique_ptr<o2::emcal::MappingHandler> mMapper; ///< EMCAL mapper
