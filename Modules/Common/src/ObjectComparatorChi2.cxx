da4b56fcead7718fd15c90fbc2a27cf50583f638
aferrero2707 <aferrero1975@gmail.com>
2024-09-22 18:18:52 +0200

    [Common] enable ranges in object comparisons
    The "average deviation" and "Chi2 test"  comparison methods are improved
    to take into account the X and/or Y ranges in the comparison.

--- Modules/Common/src/ObjectComparatorChi2.cxx
+++ Modules/Common/src/ObjectComparatorChi2.cxx
@@ -37,10 +37,37 @@ Quality ObjectComparatorChi2::compare(TObject* object, TObject* referenceObject,
   auto* histogram = std::get<0>(checkResult);
   auto* referenceHistogram = std::get<1>(checkResult);
 
+  // if X and/or Y ranges are specified, set the appropriate bin range in the corresponding axis
+  // to restrict the histogram area where the test is applied
+  const double epsilon = 1.0e-6;
+  if( getXRange().has_value()) {
+    int binMin = histogram->GetXaxis()->FindBin(getXRange()->first);
+    // subtract a small amount to the upper edge to avoid getting the next bin
+    int binMax = histogram->GetXaxis()->FindBin(getXRange()->second - epsilon);
+
+    histogram->GetXaxis()->SetRange(binMin, binMax);
+    referenceHistogram->GetXaxis()->SetRange(binMin, binMax);
+  }
+
+  if( getYRange().has_value()) {
+    int binMin = histogram->GetYaxis()->FindBin(getYRange()->first);
+    // subtract a small amount to the upper edge to avoid getting the next bin
+    int binMax = histogram->GetYaxis()->FindBin(getYRange()->second - epsilon);
+
+    histogram->GetYaxis()->SetRange(binMin, binMax);
+    referenceHistogram->GetYaxis()->SetRange(binMin, binMax);
+  }
+
   // perform a chi2 compatibility test between the two histograms
   // it assumes that both histigrams represent counts, but the reference might
   // have been rescaled to match the integral of the current histogram
   double testProbability = histogram->Chi2Test(referenceHistogram, "UU NORM");
+
+  // reset the axis ranges
+  histogram->GetXaxis()->SetRange(0, 0);
+  histogram->GetYaxis()->SetRange(0, 0);
+  referenceHistogram->GetXaxis()->SetRange(0, 0);
+  referenceHistogram->GetYaxis()->SetRange(0, 0);
 
   // compare the chi2 probability with the minimum allowed value
   if (testProbability < getThreshold()) {
--- Modules/Common/src/ObjectComparatorDeviation.cxx
+++ Modules/Common/src/ObjectComparatorDeviation.cxx
@@ -37,15 +37,41 @@ Quality ObjectComparatorDeviation::compare(TObject* object, TObject* referenceOb
   auto* histogram = std::get<0>(checkResult);
   auto* referenceHistogram = std::get<1>(checkResult);
 
+  const double epsilon = 1.0e-6;
+  int binRangeX[2] = { 1, histogram->GetXaxis()->GetNbins()};
+  int binRangeY[2] = { 1, histogram->GetYaxis()->GetNbins()};
+  int binRangeZ[2] = { 1, histogram->GetZaxis()->GetNbins()};
+
+  if( getXRange().has_value()) {
+    binRangeX[0] = histogram->GetXaxis()->FindBin(getXRange()->first);
+    // subtract a small amount to the upper edge to avoid getting the next bin
+    binRangeX[1] = histogram->GetXaxis()->FindBin(getXRange()->second - epsilon);
+  }
+
+  if( getYRange().has_value()) {
+    binRangeY[0] = histogram->GetYaxis()->FindBin(getYRange()->first);
+    // subtract a small amount to the upper edge to avoid getting the next bin
+    binRangeY[1] = histogram->GetYaxis()->FindBin(getYRange()->second - epsilon);
+  }
+
   // compute the average relative deviation between the bins
   double averageDeviation = 0;
-  int numberOfBins = histogram->GetNcells() - 2;
-  for (int bin = 1; bin <= numberOfBins; bin++) {
-    double val = histogram->GetBinContent(bin);
-    double refVal = referenceHistogram->GetBinContent(bin);
-    averageDeviation += (refVal == 0) ? 0 : std::abs((val - refVal) / refVal);
+  int numberOfBins = 0;
+  for (int binX = binRangeX[0]; binX <= binRangeX[1]; binX++) {
+    for (int binY = binRangeY[0]; binY <= binRangeY[1]; binY++) {
+      for (int binZ = binRangeZ[0]; binZ <= binRangeZ[1]; binZ++) {
+        int bin = histogram->GetBin(binX, binY, binZ);
+        double val = histogram->GetBinContent(bin);
+        double refVal = referenceHistogram->GetBinContent(bin);
+        double deviation = (refVal == 0) ? 0 : std::abs((val - refVal) / refVal);
+        averageDeviation += deviation;
+        numberOfBins += 1;
+      }
+    }
   }
-  averageDeviation /= numberOfBins;
+  if (numberOfBins > 0) {
+    averageDeviation /= numberOfBins;
+  }
 
   // compare the average deviation with the maximum allowed value
   if (averageDeviation > getThreshold()) {
