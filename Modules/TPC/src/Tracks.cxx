8082ebbe415e7715e66cbb56e179b1732f46369d
Maximilian Korwieser <maximilian.korwieser@cern.ch>
2024-10-04 12:11:36 +0200

    [TPC-QC] Add configurables for the DCAr selection in Tracks.cxx

--- Modules/TPC/src/Tracks.cxx
+++ Modules/TPC/src/Tracks.cxx
@@ -43,9 +43,11 @@ void Tracks::initialize(o2::framework::InitContext& /*ctx*/)
   const float cutPtForDCAr = o2::quality_control_modules::common::getFromConfig<float>(mCustomParameters, "cutPtForDCAr", 1.5); // min pt cut only for the DCA
   const float samplingFractionDCAr = o2::quality_control_modules::common::getFromConfig<float>(mCustomParameters, "samplingFractionDCAr", 0.1);
   const bool runAsyncAndTurnOffSomeHistos = o2::quality_control_modules::common::getFromConfig<bool>(mCustomParameters, "turnOffHistosForAsync");
+  const float cutMaxAbsDCAr = o2::quality_control_modules::common::getFromConfig<float>(mCustomParameters, "cutMaxAbsDCAr", 0.1);
+  const bool useCutMaxAbsDCArOnHistos = o2::quality_control_modules::common::getFromConfig<bool>(mCustomParameters, "useCutMaxAbsDCArOnHistos");
 
   // set track cuts defaults are (AbsEta = 1.0, nCluster = 60, MindEdxTot  = 20)
-  mQCTracks.setTrackCuts(cutAbsEta, cutMinNCluster, cutMindEdxTot, cutPtForDCAr, samplingFractionDCAr, runAsyncAndTurnOffSomeHistos);
+  mQCTracks.setTrackCuts(cutAbsEta, cutMinNCluster, cutMindEdxTot, cutPtForDCAr, samplingFractionDCAr, runAsyncAndTurnOffSomeHistos, cutMaxAbsDCAr, useCutMaxAbsDCArOnHistos);
 
   mQCTracks.initializeHistograms();
   // pass map of vectors of histograms to be beutified!
