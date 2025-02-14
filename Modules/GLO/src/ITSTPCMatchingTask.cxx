f65076f4a7360465e192348cf589b2ab14b0846a
Felix Schlepper <felix.schlepper@cern.ch>
2025-02-14 15:30:01 +0100

    GLO: use getFromConfig
    Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>

--- Modules/GLO/src/ITSTPCMatchingTask.cxx
+++ Modules/GLO/src/ITSTPCMatchingTask.cxx
@@ -20,11 +20,12 @@
 #include "QualityControl/QcInfoLogger.h"
 #include "GLO/ITSTPCMatchingTask.h"
 #include "Common/Utils.h"
+
 #include <Framework/InputRecord.h>
 #include <Framework/InputRecordWalker.h>
-#include <QualityControl/stringUtils.h>
 
 using matchType = o2::gloqc::MatchITSTPCQC::matchType;
+using namespace o2::quality_control_modules::common;
 
 namespace o2::quality_control_modules::glo
 {
@@ -33,92 +34,33 @@ void ITSTPCMatchingTask::initialize(o2::framework::InitContext& /*ctx*/)
 {
   ILOG(Debug, Devel) << "initialize ITSTPCMatchingTask" << ENDM; // QcInfoLogger is used. FairMQ logs will go to there as well.
 
-  if (auto param = mCustomParameters.find("isMC"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - isMC (= use of MC info): " << param->second << ENDM;
-    mMatchITSTPCQC.setUseMC(o2::quality_control::core::decodeBool(param->second));
-  }
+  // MC
+  mMatchITSTPCQC.setUseMC(getFromConfig(mCustomParameters, "isMC", false));
+  mMatchITSTPCQC.setUseTrkPID(getFromConfig(mCustomParameters, "useTrkPID", false));
 
-  if (auto param = mCustomParameters.find("useTrkPID"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - useTrkPID (= add plots for tracking PID): " << param->second << ENDM;
-    mMatchITSTPCQC.setUseTrkPID(o2::quality_control::core::decodeBool(param->second));
-  }
-
-  ///////////////////////////////   Track selections for MatchITSTPCQC    ////////////////////////////////
   // ITS track
-  if (auto param = mCustomParameters.find("minPtITSCut"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - minPtITSCut (for track selection): " << param->second << ENDM;
-    mMatchITSTPCQC.setMinPtITSCut(atof(param->second.c_str()));
-  }
-  if (auto param = mCustomParameters.find("etaITSCut"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - etaITSCut (for track selection): " << param->second << ENDM;
-    mMatchITSTPCQC.setEtaITSCut(atof(param->second.c_str()));
-  }
-  if (auto param = mCustomParameters.find("minNITSClustersCut"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - minNITSClustersCut (for track selection): " << param->second << ENDM;
-    mMatchITSTPCQC.setMinNClustersITS(atoi(param->second.c_str()));
-  }
-  if (auto param = mCustomParameters.find("maxChi2PerClusterITS"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - maxChi2PerClusterITS (for track selection): " << param->second << ENDM;
-    mMatchITSTPCQC.setMaxChi2PerClusterITS(atof(param->second.c_str()));
-  }
-  // TO DO: define an agreed way to implement the setter for ITS matching (min. # layers, which layers)
-  // [...] --> exploit the method TrackCuts::setRequireHitsInITSLayers(...)
+  mMatchITSTPCQC.setMinPtITSCut(getFromConfig(mCustomParameters, "minPtITSCut", 0.1f));
+  mMatchITSTPCQC.setEtaITSCut(getFromConfig(mCustomParameters, "minEtaITSCut", 1.4f));
+  mMatchITSTPCQC.setEtaITSCut(getFromConfig(mCustomParameters, "etaITSCut", 1.4f));
+  mMatchITSTPCQC.setMinNClustersITS(getFromConfig(mCustomParameters, "minNITSClustersCut", 0));
+  mMatchITSTPCQC.setMaxChi2PerClusterITS(getFromConfig(mCustomParameters, "maxChi2PerClusterITS", 1e10f));
   // TPC track
-  if (auto param = mCustomParameters.find("minPtTPCCut"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - minPtTPCCut (for track selection): " << param->second << ENDM;
-    mMatchITSTPCQC.setMinPtTPCCut(atof(param->second.c_str()));
-  }
-  if (auto param = mCustomParameters.find("etaTPCCut"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - etaTPCCut (for track selection): " << param->second << ENDM;
-    mMatchITSTPCQC.setEtaTPCCut(atof(param->second.c_str()));
-  }
-  if (auto param = mCustomParameters.find("minNTPCClustersCut"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - minNTPCClustersCut (for track selection): " << param->second << ENDM;
-    mMatchITSTPCQC.setMinNTPCClustersCut(atoi(param->second.c_str()));
-  }
-  if (auto param = mCustomParameters.find("minDCACut"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - minDCACut (for track selection): " << param->second << ENDM;
-    mMatchITSTPCQC.setMinDCAtoBeamPipeDistanceCut(atof(param->second.c_str()));
-  }
-  if (auto param = mCustomParameters.find("minDCACutY"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - minDCACutY (for track selection): " << param->second << ENDM;
-    mMatchITSTPCQC.setMinDCAtoBeamPipeYCut(atof(param->second.c_str()));
-  }
+  mMatchITSTPCQC.setMinPtTPCCut(getFromConfig(mCustomParameters, "minPtTPCCut", 0.1f));
+  mMatchITSTPCQC.setEtaTPCCut(getFromConfig(mCustomParameters, "etaTPCCut", 1.4f));
+  mMatchITSTPCQC.setMinNTPCClustersCut(getFromConfig(mCustomParameters, "minNTPCClustersCut", 60));
+  mMatchITSTPCQC.setMinDCAtoBeamPipeDistanceCut(getFromConfig(mCustomParameters, "minDCACut", 100.f));
+  mMatchITSTPCQC.setMinDCAtoBeamPipeYCut(getFromConfig(mCustomParameters, "minDCACutY", 10.f));
   // ITS-TPC kinematics
-  if (auto param = mCustomParameters.find("minPtCut"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - minPtCut (for track selection): " << param->second << ENDM;
-    mMatchITSTPCQC.setPtCut(atof(param->second.c_str()));
-  }
-  if (auto param = mCustomParameters.find("maxPtCut"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - maxPtCut (for track selection): " << param->second << ENDM;
-    mMatchITSTPCQC.setMaxPtCut(atof(param->second.c_str()));
-  }
-  if (auto param = mCustomParameters.find("etaCut"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - EtaCut (for track selection): " << param->second << ENDM;
-    mMatchITSTPCQC.setEtaCut(atof(param->second.c_str()));
-  }
-
-  ///////////////////////////////   Options for K0    ////////////////////////////////
-  if (auto param = mCustomParameters.find("doK0QC"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - doK0QC (= do K0 QC): " << param->second << ENDM;
-    mMatchITSTPCQC.setDoK0QC(o2::quality_control::core::decodeBool(param->second));
-  }
-
+  mMatchITSTPCQC.setPtCut(getFromConfig(mCustomParameters, "minPtCut", 0.1f));
+  mMatchITSTPCQC.setMaxPtCut(getFromConfig(mCustomParameters, "maxPtCut", 20.f));
+  mMatchITSTPCQC.setEtaCut(getFromConfig(mCustomParameters, "etaCut", 1.4f));
+  // K0s
+  mMatchITSTPCQC.setDoK0QC(getFromConfig(mCustomParameters, "doK0QC", true));
+  mMatchITSTPCQC.setMaxK0Eta(getFromConfig(mCustomParameters, "maxK0Eta", 0.8f));
+  mMatchITSTPCQC.setRefitK0(getFromConfig(mCustomParameters, "refitK0", true));
+  mMatchITSTPCQC.setCutK0Mass(getFromConfig(mCustomParameters, "cutK0Mass", 0.05f));
   if (auto param = mCustomParameters.find("trackSourcesK0"); param != mCustomParameters.end()) {
     mMatchITSTPCQC.setTrkSources(o2::dataformats::GlobalTrackID::getSourcesMask(param->second));
-  }
-
-  if (auto param = mCustomParameters.find("maxK0Eta"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - maxK0Eta (for K0 selection): " << param->second << ENDM;
-    mMatchITSTPCQC.setMaxK0Eta(atof(param->second.c_str()));
-  }
-  if (auto param = mCustomParameters.find("refitK0"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - refitK0 ( enable refit K0s): " << param->second << ENDM;
-    mMatchITSTPCQC.setRefitK0(o2::quality_control::core::decodeBool(param->second));
-  }
-  if (auto param = mCustomParameters.find("cutK0Mass"); param != mCustomParameters.end()) {
-    ILOG(Debug, Devel) << "Custom parameter - cutK0Mass (cut on the distance to the PDG mass): " << param->second << ENDM;
-    mMatchITSTPCQC.setCutK0Mass(atof(param->second.c_str()));
   }
 
   mMatchITSTPCQC.initDataRequest();
