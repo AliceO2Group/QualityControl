c890a18e12fdcec06d82b7bc54cf430d5e3d430c
Felix Schlepper <felix.schlepper@cern.ch>
2025-03-07 15:14:39 +0100

    GLO: generalize reductor
    Signed-off-by: Felix Schlepper <felix.schlepper@cern.ch>

--- Modules/GLO/src/Reductors.cxx
+++ Modules/GLO/src/Reductors.cxx
@@ -9,7 +9,7 @@
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
 
-#include "GLO/K0sFitReductor.h"
+#include "GLO/Reductors.h"
 #include "GLO/Helpers.h"
 
 #include <TF1.h>
