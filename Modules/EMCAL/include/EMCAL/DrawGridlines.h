AliceO2Group/QualityControl

commit edcd7b9a572c621a71027e3b65ae228f4c5e9207

[EMCAL-519] Optimize QC Messages for oncall related histograms (#2637)
- Add online supermodule indices to multiple histograms
- Add grid lines with SM borders to mw histograms

Co-authored-by: jokonig <jokonig@cern.ch>

Modules/EMCAL/include/EMCAL/DrawGridlines.h
@@ -14,6 +14,7 @@
 
 #include "TLine.h"
 #include "TH2D.h"
+#include "QualityControl/QcInfoLogger.h"
 
 namespace o2::quality_control_modules::emcal
 {
@@ -36,6 +37,24 @@ class DrawGridlines : public quality_control::postprocessing::PostProcessingInte
     if (histo == nullptr) {
       return;
     }
+
+    // check if the gridlines are already drawn by looking for a line at the first SM boundary
+    auto* funcs = histo->GetListOfFunctions();
+    if (!funcs) {
+      return;
+    }
+
+    // Remove previously added grid lines
+    TIter it(funcs);
+    TObject* obj = nullptr;
+    while ((obj = it())) {
+      if (obj->InheritsFrom(TLine::Class())) {
+        ILOG(Debug, Support) << "Removing existing grid line from histogram " << histo->GetName() << ENDM;
+        funcs->Remove(obj);
+        delete obj;
+      }
+    }
+
     // EMCAL
     for (int iside = 0; iside <= 48; iside += 24) {
       auto smline = new TLine(static_cast<double>(iside) - 0.5, -0.5, static_cast<double>(iside) - 0.5, 63.5);
@@ -112,6 +131,63 @@ class DrawGridlines : public quality_control::postprocessing::PostProcessingInte
       truseparator->SetLineWidth(3);
 
       histo->GetListOfFunctions()->Add(truseparator);
+    }
+  };
+
+  /// \brief Draw the gridlines in the standard cell geometry
+  static void DrawSMGridInStdGeo(TH2* histo = nullptr)
+  {
+    if (histo == nullptr) {
+      return;
+    }
+    // EMCAL
+    for (int iside = 0; iside <= 96; iside += 48) {
+      auto smline = new TLine(static_cast<double>(iside) - 0.5, -0.5, static_cast<double>(iside) - 0.5, 127.5);
+      smline->SetLineWidth(6);
+
+      histo->GetListOfFunctions()->Add(smline);
+    }
+    for (int iphi = 0; iphi < 120; iphi += 24) {
+      auto smline = new TLine(-0.5, static_cast<double>(iphi) - 0.5, 95.5, static_cast<double>(iphi) - 0.5);
+      smline->SetLineWidth(6);
+
+      histo->GetListOfFunctions()->Add(smline);
+    }
+    for (auto iphi = 120; iphi <= 128; iphi += 8) {
+      auto smline = new TLine(-0.5, static_cast<double>(iphi) - 0.5, 95.5, static_cast<double>(iphi) - 0.5);
+      smline->SetLineWidth(6);
+
+      histo->GetListOfFunctions()->Add(smline);
+    }
+
+    // DCAL
+    for (int side = 0; side < 2; side++) {
+      int sideoffset = (side == 0) ? 0 : 64;
+      for (int isepeta = 0; isepeta < 2; isepeta++) {
+        int etaoffset = sideoffset + isepeta * 32;
+        auto smline = new TLine(static_cast<double>(etaoffset) - 0.5, 127.5, static_cast<double>(etaoffset) - 0.5, 199.5);
+        smline->SetLineWidth(6);
+
+        histo->GetListOfFunctions()->Add(smline);
+      }
+      for (auto iphi = 152; iphi <= 176; iphi += 24) {
+        auto smline = new TLine(static_cast<double>(sideoffset) - 0.5, static_cast<double>(iphi) - 0.5, static_cast<double>(sideoffset + 32) - 0.5, static_cast<double>(iphi) - 0.5);
+        smline->SetLineWidth(6);
+
+        histo->GetListOfFunctions()->Add(smline);
+      }
+    }
+    for (auto iphi = 200; iphi <= 208; iphi += 8) {
+      auto smline = new TLine(-0.5, static_cast<double>(iphi) - 0.5, 95.5, static_cast<int>(iphi) - 0.5);
+      smline->SetLineWidth(6);
+
+      histo->GetListOfFunctions()->Add(smline);
+    }
+    for (auto ieta = 0; ieta <= 96; ieta += 48) {
+      auto smline = new TLine(static_cast<double>(ieta) - 0.5, 199.5, static_cast<double>(ieta) - 0.5, 207.5);
+      smline->SetLineWidth(6);
+
+      histo->GetListOfFunctions()->Add(smline);
     }
   };
 

Modules/EMCAL/src/CellTask.cxx
@@ -1067,6 +1067,13 @@ void CellTask::CellHistograms::startPublishing(o2::quality_control::core::Object
     }
   };
 
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancy);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyThr);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyThrBelow);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyGood);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyBad);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mIntegratedOccupancy);
+
   publishOptional(mCellTime);
   publishOptional(mCellTimeCalib);
   publishOptional(mCellAmplitude);
@@ -1091,8 +1098,6 @@ void CellTask::CellHistograms::startPublishing(o2::quality_control::core::Object
   publishOptional(mCellAmplitudeCalib_EMCAL);
   publishOptional(mCellAmplitudeCalib_DCAL);
   publishOptional(mCellOccupancy);
-  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyThr);
-  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyThrBelow);
   publishOptional(mCellOccupancyThr);
   publishOptional(mCellOccupancyThrBelow);
   publishOptional(mCellOccupancyGood);
@@ -1169,6 +1174,14 @@ void CellTask::CellHistograms::reset()
   for (auto histos : mCellTimeBC) {
     resetOptional(histos);
   }
+
+  // Draw Grid Lines
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancy);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyThr);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyThrBelow);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyGood);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyBad);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mIntegratedOccupancy);
 }
 
 void CellTask::CellHistograms::clean()

Modules/EMCAL/CMakeLists.txt
@@ -45,6 +45,8 @@ add_root_dictionary(O2QcEMCAL
   include/EMCAL/DrawGridlines.h
   include/EMCAL/FECRateVisualization.h
   include/EMCAL/NumPhysTriggCheck.h
+  include/EMCAL/NumPhysTriggCheck.h
+  include/EMCAL/IndicesConverter.h
   LINKDEF include/EMCAL/LinkDef.h)
 
 install(TARGETS O2QcEMCAL

Modules/EMCAL/include/EMCAL/FECRateVisualization.h
@@ -21,6 +21,7 @@
 #include <array>
 #include <memory>
 #include <TCanvas.h>
+#include <EMCAL/IndicesConverter.h>
 
 namespace o2::quality_control_modules::emcal
 {
@@ -60,6 +61,7 @@ class FECRateVisualization final : public quality_control::postprocessing::PostP
 
  private:
   std::array<std::unique_ptr<TCanvas>, 20> mSupermoduleCanvas; /// Canvas with FEC Rates per supermodule
+  o2::emcal::IndicesConverter mIndicesConverter;               ///< Converter for online-offline supermodule indices
   double mMaxRate = 2000.;
 };
 

Modules/EMCAL/include/EMCAL/IndicesConverter.h
@@ -1,0 +1,65 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+#ifndef QUALITYCONTROL_INDICESCONVERTER_H
+#define QUALITYCONTROL_INDICESCONVERTER_H
+#include <map>
+#include <string>
+
+namespace o2::emcal
+{
+class IndicesConverter
+{
+ public:
+  IndicesConverter()
+  {
+    Initialize();
+  }
+  ~IndicesConverter() = default;
+
+  void Initialize()
+  {
+    // Initialize the map with online and offline supermodule indices
+    for (int i = 0; i < 20; i++) {
+      std::string SMSide = "A";
+      if (i % 2 != 0) {
+        SMSide = "C";
+      }
+      int SMRowIndex = (i / 2) + (i >= 12 ? 3 : 0); // Adjust row index for DCAL
+      mOnlineToOfflineSMMap[i] = "SM" + SMSide + std::to_string(SMRowIndex);
+    }
+  }
+
+  int GetOfflineSMIndex(const std::string& onlineSMIndex) const
+  {
+    for (const auto& pair : mOnlineToOfflineSMMap) {
+      if (pair.second == onlineSMIndex) {
+        return pair.first;
+      }
+    }
+    return -1; // Return -1 if no matching index is found
+  }
+  std::string GetOnlineSMIndex(const int offlineSMIndex) const
+  {
+    if (mOnlineToOfflineSMMap.find(offlineSMIndex) != mOnlineToOfflineSMMap.end()) {
+      return mOnlineToOfflineSMMap.at(offlineSMIndex);
+    } else {
+      return "Invalid SM Index";
+    }
+  }
+
+ private:
+  std::map<int, std::string> mOnlineToOfflineSMMap; ///< Map for conversion between online and offline supermodule indices
+};
+
+} // namespace o2::emcal
+
+#endif // QUALITYCONTROL_INDICESCONVERTER_H

Modules/EMCAL/include/EMCAL/LinkDef.h
@@ -40,6 +40,6 @@
 #pragma link C++ class o2::quality_control_modules::emcal::RawErrorCheckAll + ;
 #pragma link C++ class o2::quality_control_modules::emcal::DrawGridlines + ;
 
-      #pragma link C++ class o2::quality_control_modules::emcal::FECRateVisualization+;
-      
-      #endif
+#pragma link C++ class o2::quality_control_modules::emcal::FECRateVisualization+;
+
+#endif

Modules/EMCAL/include/EMCAL/RawCheck.h
@@ -13,6 +13,7 @@
 #define QC_MODULE_EMCAL_EMCALRAWCHECK_H
 
 #include "QualityControl/CheckInterface.h"
+#include "EMCAL/IndicesConverter.h"
 #include <array>
 #include <tuple>
 #include <vector>
@@ -63,6 +64,11 @@ class RawCheck final : public o2::quality_control::checker::CheckInterface
   bool mILMessageRawErrorCheck = false;            ///< Switch for IL message for error code
   bool mILMessageNoisyFECCheck = false;            ///< Switch for IL message for noisy FEC
   bool mILMessagePayloadSizeCheck = false;         ///< Switch for IL message for large payload size
+
+  /************************************************
+   * Conversion between online and offline indices *
+   ************************************************/
+  o2::emcal::IndicesConverter mIndicesConverter; ///< Converter for online and offline supermodule indices
 
   /************************************************
    * sigma cuts                                   *

Modules/EMCAL/include/EMCAL/SupermoduleProjectorTask.h
@@ -20,6 +20,7 @@
 // QC includes
 #include "QualityControl/QualityObject.h"
 #include "QualityControl/PostProcessingInterface.h"
+#include "EMCAL/IndicesConverter.h"
 #include <map>
 #include <memory>
 #include <vector>
@@ -98,6 +99,7 @@ class SupermoduleProjectorTask final : public quality_control::postprocessing::P
   std::vector<DataSource> mDataSources;                    ///< Data sources to be projected
   std::map<std::string, TCanvas*> mCanvasHandler;          ///< Mapping between data source and output canvas
   std::map<std::string, PlotAttributes> mAttributeHandler; ///< Customizations for canvases (i.e. axis titles)
+  o2::emcal::IndicesConverter mIndicesConverter;           ///< Converter for online-offline supermodule indices
 };
 
 } // namespace o2::quality_control_modules::emcal

commit f337cb2d052965bbe73c612e5e55d81fc1d0eb03

[EMCAL-519] Fix drawing lines for SM geom instead of trigger geom
- Wrong geometry used, introduced in https://github.com/AliceO2Group/QualityControl/pull/2637
- Make line width smaller
- factor out function to remove existing lines

Modules/EMCAL/include/EMCAL/DrawGridlines.h
@@ -39,38 +39,24 @@ class DrawGridlines : public quality_control::postprocessing::PostProcessingInte
     }
 
     // check if the gridlines are already drawn by looking for a line at the first SM boundary
-    auto* funcs = histo->GetListOfFunctions();
-    if (!funcs) {
-      return;
-    }
-
-    // Remove previously added grid lines
-    TIter it(funcs);
-    TObject* obj = nullptr;
-    while ((obj = it())) {
-      if (obj->InheritsFrom(TLine::Class())) {
-        ILOG(Debug, Support) << "Removing existing grid line from histogram " << histo->GetName() << ENDM;
-        funcs->Remove(obj);
-        delete obj;
-      }
-    }
+    ResetLines(histo);
 
     // EMCAL
     for (int iside = 0; iside <= 48; iside += 24) {
       auto smline = new TLine(static_cast<double>(iside) - 0.5, -0.5, static_cast<double>(iside) - 0.5, 63.5);
-      smline->SetLineWidth(6);
+      smline->SetLineWidth(2);
 
       histo->GetListOfFunctions()->Add(smline);
     }
     for (int iphi = 0; iphi < 60; iphi += 12) {
       auto smline = new TLine(-0.5, static_cast<double>(iphi) - 0.5, 47.5, static_cast<double>(iphi) - 0.5);
-      smline->SetLineWidth(6);
+      smline->SetLineWidth(2);
 
       histo->GetListOfFunctions()->Add(smline);
     }
     for (auto iphi = 60; iphi <= 64; iphi += 4) {
       auto smline = new TLine(-0.5, static_cast<double>(iphi) - 0.5, 47.5, static_cast<double>(iphi) - 0.5);
-      smline->SetLineWidth(6);
+      smline->SetLineWidth(2);
 
       histo->GetListOfFunctions()->Add(smline);
     }
@@ -81,26 +67,26 @@ class DrawGridlines : public quality_control::postprocessing::PostProcessingInte
       for (int isepeta = 0; isepeta < 2; isepeta++) {
         int etaoffset = sideoffset + isepeta * 16;
         auto smline = new TLine(static_cast<double>(etaoffset) - 0.5, 63.5, static_cast<double>(etaoffset) - 0.5, 99.5);
-        smline->SetLineWidth(6);
+        smline->SetLineWidth(2);
 
         histo->GetListOfFunctions()->Add(smline);
       }
       for (auto iphi = 76; iphi <= 88; iphi += 12) {
         auto smline = new TLine(static_cast<double>(sideoffset) - 0.5, static_cast<double>(iphi) - 0.5, static_cast<double>(sideoffset + 16) - 0.5, static_cast<double>(iphi) - 0.5);
-        smline->SetLineWidth(6);
+        smline->SetLineWidth(2);
 
         histo->GetListOfFunctions()->Add(smline);
       }
     }
     for (auto iphi = 100; iphi <= 104; iphi += 4) {
       auto smline = new TLine(-0.5, static_cast<double>(iphi) - 0.5, 47.5, static_cast<int>(iphi) - 0.5);
-      smline->SetLineWidth(6);
+      smline->SetLineWidth(2);
 
       histo->GetListOfFunctions()->Add(smline);
     }
     for (auto ieta = 0; ieta <= 48; ieta += 24) {
       auto smline = new TLine(static_cast<double>(ieta) - 0.5, 99.5, static_cast<double>(ieta) - 0.5, 103.5);
-      smline->SetLineWidth(6);
+      smline->SetLineWidth(2);
 
       histo->GetListOfFunctions()->Add(smline);
     }
@@ -112,13 +98,17 @@ class DrawGridlines : public quality_control::postprocessing::PostProcessingInte
     if (histo == nullptr) {
       return;
     }
+
+    // check if the gridlines are already drawn by looking for a line at the first SM boundary
+    ResetLines(histo);
+
     // EMCAL
     for (int side = 0; side < 2; side++) {
       int sideoffset = 24 * side;
       for (int itru = 0; itru < 2; itru++) {
         int truoffset = sideoffset + (itru + 1) * 8;
         auto truline = new TLine(static_cast<int>(truoffset) - 0.5, -0.5, static_cast<int>(truoffset) - 0.5, 59.5);
-        truline->SetLineWidth(3);
+        truline->SetLineWidth(2);
 
         histo->GetListOfFunctions()->Add(truline);
       }
@@ -128,7 +118,7 @@ class DrawGridlines : public quality_control::postprocessing::PostProcessingInte
     for (int side = 0; side < 2; side++) {
       int sideoffset = (side == 0) ? 0 : 32;
       auto truseparator = new TLine(static_cast<double>(sideoffset + 8) - 0.5, 63.5, static_cast<double>(sideoffset + 8) - 0.5, 99.5);
-      truseparator->SetLineWidth(3);
+      truseparator->SetLineWidth(2);
 
       histo->GetListOfFunctions()->Add(truseparator);
     }
@@ -140,22 +130,26 @@ class DrawGridlines : public quality_control::postprocessing::PostProcessingInte
     if (histo == nullptr) {
       return;
     }
+
+    // check if the gridlines are already drawn by looking for a line at the first SM boundary
+    ResetLines(histo);
+
     // EMCAL
     for (int iside = 0; iside <= 96; iside += 48) {
       auto smline = new TLine(static_cast<double>(iside) - 0.5, -0.5, static_cast<double>(iside) - 0.5, 127.5);
-      smline->SetLineWidth(6);
+      smline->SetLineWidth(2);
 
       histo->GetListOfFunctions()->Add(smline);
     }
     for (int iphi = 0; iphi < 120; iphi += 24) {
       auto smline = new TLine(-0.5, static_cast<double>(iphi) - 0.5, 95.5, static_cast<double>(iphi) - 0.5);
-      smline->SetLineWidth(6);
+      smline->SetLineWidth(2);
 
       histo->GetListOfFunctions()->Add(smline);
     }
     for (auto iphi = 120; iphi <= 128; iphi += 8) {
       auto smline = new TLine(-0.5, static_cast<double>(iphi) - 0.5, 95.5, static_cast<double>(iphi) - 0.5);
-      smline->SetLineWidth(6);
+      smline->SetLineWidth(2);
 
       histo->GetListOfFunctions()->Add(smline);
     }
@@ -166,26 +160,26 @@ class DrawGridlines : public quality_control::postprocessing::PostProcessingInte
       for (int isepeta = 0; isepeta < 2; isepeta++) {
         int etaoffset = sideoffset + isepeta * 32;
         auto smline = new TLine(static_cast<double>(etaoffset) - 0.5, 127.5, static_cast<double>(etaoffset) - 0.5, 199.5);
-        smline->SetLineWidth(6);
+        smline->SetLineWidth(2);
 
         histo->GetListOfFunctions()->Add(smline);
       }
       for (auto iphi = 152; iphi <= 176; iphi += 24) {
         auto smline = new TLine(static_cast<double>(sideoffset) - 0.5, static_cast<double>(iphi) - 0.5, static_cast<double>(sideoffset + 32) - 0.5, static_cast<double>(iphi) - 0.5);
-        smline->SetLineWidth(6);
+        smline->SetLineWidth(2);
 
         histo->GetListOfFunctions()->Add(smline);
       }
     }
     for (auto iphi = 200; iphi <= 208; iphi += 8) {
       auto smline = new TLine(-0.5, static_cast<double>(iphi) - 0.5, 95.5, static_cast<int>(iphi) - 0.5);
-      smline->SetLineWidth(6);
+      smline->SetLineWidth(2);
 
       histo->GetListOfFunctions()->Add(smline);
     }
     for (auto ieta = 0; ieta <= 96; ieta += 48) {
       auto smline = new TLine(static_cast<double>(ieta) - 0.5, 199.5, static_cast<double>(ieta) - 0.5, 207.5);
-      smline->SetLineWidth(6);
+      smline->SetLineWidth(2);
 
       histo->GetListOfFunctions()->Add(smline);
     }
@@ -197,6 +191,10 @@ class DrawGridlines : public quality_control::postprocessing::PostProcessingInte
     if (histo == nullptr) {
       return;
     }
+
+    // check if the gridlines are already drawn by looking for a line at the first SM boundary
+    ResetLines(histo);
+
     // EMCAL
     for (int iphi = 1; iphi < 64; iphi++) {
       auto fastorLine = new TLine(-0.5, static_cast<double>(iphi) - 0.5, 47.5, static_cast<double>(iphi) - 0.5);
@@ -228,6 +226,29 @@ class DrawGridlines : public quality_control::postprocessing::PostProcessingInte
     for (auto iphi = 101; iphi <= 103; iphi++) {
       auto philine = new TLine(-0.5, static_cast<double>(iphi) - 0.5, 47.5, static_cast<int>(iphi) - 0.5);
       histo->GetListOfFunctions()->Add(philine);
+    }
+  };
+
+  static void ResetLines(TH1* histo = nullptr)
+  {
+    if (histo == nullptr) {
+      return;
+    }
+
+    auto* funcs = histo->GetListOfFunctions();
+    if (!funcs) {
+      return;
+    }
+
+    // Remove previously added grid lines
+    TIter it(funcs);
+    TObject* obj = nullptr;
+    while ((obj = it())) {
+      if (obj->InheritsFrom(TLine::Class())) {
+        ILOG(Debug, Support) << "Removing existing grid line from histogram " << histo->GetName() << ENDM;
+        funcs->Remove(obj);
+        delete obj;
+      }
     }
   };
 };

Modules/EMCAL/src/CellTask.cxx
@@ -1067,12 +1067,12 @@ void CellTask::CellHistograms::startPublishing(o2::quality_control::core::Object
     }
   };
 
-  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancy);
-  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyThr);
-  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyThrBelow);
-  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyGood);
-  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyBad);
-  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mIntegratedOccupancy);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInStdGeo(mCellOccupancy);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInStdGeo(mCellOccupancyThr);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInStdGeo(mCellOccupancyThrBelow);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInStdGeo(mCellOccupancyGood);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInStdGeo(mCellOccupancyBad);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInStdGeo(mIntegratedOccupancy);
 
   publishOptional(mCellTime);
   publishOptional(mCellTimeCalib);
@@ -1176,12 +1176,12 @@ void CellTask::CellHistograms::reset()
   }
 
   // Draw Grid Lines
-  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancy);
-  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyThr);
-  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyThrBelow);
-  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyGood);
-  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mCellOccupancyBad);
-  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInTriggerGeo(mIntegratedOccupancy);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInStdGeo(mCellOccupancy);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInStdGeo(mCellOccupancyThr);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInStdGeo(mCellOccupancyThrBelow);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInStdGeo(mCellOccupancyGood);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInStdGeo(mCellOccupancyBad);
+  o2::quality_control_modules::emcal::DrawGridlines::DrawSMGridInStdGeo(mIntegratedOccupancy);
 }
 
 void CellTask::CellHistograms::clean()