af79245f5fe5eb6520aba936c40ec5305e0af369
Piotr Konopka <piotr.jan.konopka@cern.ch>
2026-06-02 08:56:11 +0200

    add missing file header

--- Framework/include/QualityControl/ServicesConfig.h
+++ Framework/include/QualityControl/ServicesConfig.h
@@ -1,6 +1,18 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
 //
-// Created by pkonopka on 04/12/2025.
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
 //
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// \file   ServicesConfig.h
+/// \author Piotr Konopka
+///
 
 #ifndef QUALITYCONTROL_SERVICESCONFIG_H
 #define QUALITYCONTROL_SERVICESCONFIG_H
